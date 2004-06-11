a58571a87ecfbbd03afd6a01cf9b628a7086d4d7
João Prado Maia <jpm@pessoal.org>
2004-06-11 04:48:49 +0000

    (Logical change 1.95)

---
+++ eventum/misc/upgrade/v1.1_to_v1.2/database_changes.php
@@ -0,0 +1,34 @@
+<?php
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+
+$changes = array();
+$changes[] = 'CREATE TABLE eventum_support_email_body (
+  seb_sup_id int(11) unsigned NOT NULL,
+  seb_body longtext NOT NULL,
+  seb_full_email longtext NOT NULL,
+  PRIMARY KEY (seb_sup_id)
+);
+';
+$changes[] = 'INSERT INTO eventum_support_email_body (SELECT sup_id, sup_body, sup_full_email FROM eventum_support_email);'
+$changes[] = 'ALTER TABLE eventum_support_email DROP COLUMN sup_body;';
+$changes[] = 'ALTER TABLE eventum_support_email DROP COLUMN sup_full_email;';
+$changes[] = 'ALTER TABLE eventum_support_email ADD COLUMN sup_usr_id int(11) unsigned DEFAULT NULL AFTER sup_iss_id;';
+$changes[] = 'ALTER TABLE eventum_support_email ADD KEY sup_usr_id(sup_usr_id);';
+$changes[] = 'ALTER TABLE eventum_email_account ADD COLUMN ema_issue_auto_creation varchar(8) NOT NULL DEFAULT \'disabled\';';
+$changes[] = 'ALTER TABLE eventum_email_account ADD COLUMN ema_issue_auto_creation_options text;';
+
+foreach ($changes as $stmt) {
+    $update = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($update)) {
+        echo "<pre>";var_dump($update);echo "</pre>";
+        exit;
+    }
+}
+echo "complete<br />\n\n";
+
+
+
+?>
+now please run /misc/upgrade/v1.1_to_v1.2/set_support_email_usr_id.php
---
+++ eventum/misc/upgrade/v1.1_to_v1.2/set_support_email_usr_id.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Runonce script to set the sup_usr_id field in support_email
+ */
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+
+$stmt = "SELECT
+            sup_id,
+            sup_from
+         FROM
+            " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "support_email
+         WHERE
+            sup_usr_id IS NULL AND
+            sup_iss_id != 0";
+$res = $GLOBALS["db_api"]->dbh->getAssoc($stmt);
+foreach ($res as $sup_id => $email) {
+    $usr_id = User::getUserIDByEmail(Mail_API::getEmailAddress($email));
+    if (!empty($usr_id)) {
+        $stmt = "UPDATE
+                    " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "support_email
+                 SET
+                    sup_usr_id = $usr_id
+                 WHERE
+                    sup_id = $sup_id";
+        $update = $GLOBALS["db_api"]->dbh->query($stmt);
+        if (PEAR::isError($update)) {
+            echo "<pre>";var_dump($update);echo "</pre>";
+            exit;
+        }
+    }
+}
+echo "complete";
+
+?>
