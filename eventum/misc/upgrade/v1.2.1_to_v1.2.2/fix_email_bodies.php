f27e5402d00196a79212a867bb907382173bbbfc
João Prado Maia <jpm@pessoal.org>
2004-06-17 14:16:59 +0000

    (Logical change 1.114)

---
+++ eventum/misc/upgrade/v1.2.1_to_v1.2.2/fix_email_bodies.php
@@ -0,0 +1,36 @@
+<?php
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+include_once(APP_INC_PATH . "class.mime_helper.php");
+
+ini_set("memory_limit", "512M");
+
+$stmt = "SELECT
+            seb_sup_id,
+            seb_full_email
+         FROM
+            " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "support_email_body
+         ORDER BY
+            seb_sup_id";
+$res = $GLOBALS["db_api"]->dbh->getAssoc($stmt);
+foreach ($res as $sup_id => $full_message) {
+    $structure = Mime_Helper::decode($full_message, true, true);
+    $body = Mime_Helper::getMessageBody($structure);
+
+    $stmt = "UPDATE
+                " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "support_email_body
+             SET
+                seb_body='" . Misc::escapeString($body) . "'
+             WHERE
+                seb_sup_id=$sup_id";
+    $update = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($update)) {
+        echo "<pre>";var_dump($update);echo "</pre>";
+        exit;
+    }
+    echo "fixed email #$sup_id<br />";
+    flush();
+}
+echo "complete";
+
+?>
