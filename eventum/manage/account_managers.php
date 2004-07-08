4d5bd5dd021037a55e78777d00e14b54fb1aa8ab
João Prado Maia <jpm@pessoal.org>
2004-07-08 21:07:06 +0000

    (Logical change 1.137)

---
+++ eventum/manage/account_managers.php
@@ -0,0 +1,78 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003, 2004 MySQL AB                                    |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: João Prado Maia <jpm@mysql.com>                             |
+// +----------------------------------------------------------------------+
+//
+// @(#) $Id$
+//
+include_once("../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+include_once(APP_INC_PATH . "class.template.php");
+include_once(APP_INC_PATH . "class.auth.php");
+include_once(APP_INC_PATH . "class.user.php");
+include_once(APP_INC_PATH . "class.customer.php");
+
+$tpl = new Template_API();
+$tpl->setTemplate("manage/index.tpl.html");
+
+Auth::checkAuthentication(APP_COOKIE);
+
+$tpl->assign("type", "account_managers");
+
+$role_id = User::getRoleByUser(Auth::getUserID());
+if (($role_id == User::getRoleID('administrator')) || ($role_id == User::getRoleID('manager'))) {
+    if ($role_id == User::getRoleID('administrator')) {
+        $tpl->assign("show_setup_links", true);
+    }
+
+    if (@$HTTP_POST_VARS["cat"] == "new") {
+        $tpl->assign("result", Customer::insertAccountManager());
+    } elseif (@$HTTP_POST_VARS["cat"] == "update") {
+        $tpl->assign("result", Customer::updateAccountManager());
+    } elseif (@$HTTP_POST_VARS["cat"] == "delete") {
+        Customer::removeAccountManager();
+    } elseif (!empty($HTTP_GET_VARS['prj_id'])) {
+        $tpl->assign("info", array('cam_prj_id' => $HTTP_GET_VARS['prj_id']));
+        $tpl->assign('customers', Customer::getAssocList($HTTP_GET_VARS['prj_id']));
+    }
+
+    if (@$HTTP_GET_VARS["cat"] == "edit") {
+        $info = Customer::getAccountManagerDetails($HTTP_GET_VARS["id"]);
+        if (!empty($HTTP_GET_VARS['prj_id'])) {
+            $info['cam_prj_id'] = $HTTP_GET_VARS['prj_id'];
+        }
+        $tpl->assign('customers', Customer::getAssocList($info['cam_prj_id']));
+        $tpl->assign("info", $info);
+    }
+
+    $tpl->assign("list", Customer::getAccountManagerList());
+    $tpl->assign("user_options", User::getActiveAssocList(User::getRoleID('Customer')));
+    $tpl->assign("project_list", Project::getAll());
+} else {
+    $tpl->assign("show_not_allowed_msg", true);
+}
+
+$tpl->displayTemplate();
+?>
---
+++ eventum/manage/customer_notes.php
@@ -0,0 +1,79 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003, 2004 MySQL AB                                    |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Bryan Alsdorf <bryan@mysql.com>                             |
+// +----------------------------------------------------------------------+
+//
+// @(#) $Id$
+//
+include_once("../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+include_once(APP_INC_PATH . "class.template.php");
+include_once(APP_INC_PATH . "class.auth.php");
+include_once(APP_INC_PATH . "class.user.php");
+include_once(APP_INC_PATH . "class.project.php");
+include_once(APP_INC_PATH . "class.customer.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+$tpl = new Template_API();
+$tpl->setTemplate("manage/index.tpl.html");
+
+Auth::checkAuthentication(APP_COOKIE);
+
+$tpl->assign("type", "customer_notes");
+
+$role_id = User::getRoleByUser(Auth::getUserID());
+if (($role_id == User::getRoleID('administrator')) || ($role_id == User::getRoleID('manager'))) {
+    if ($role_id == User::getRoleID('administrator')) {
+        $tpl->assign("show_setup_links", true);
+    }
+
+    if (@$HTTP_POST_VARS["cat"] == "new") {
+        $tpl->assign("result", Customer::insertNote($HTTP_POST_VARS["project"], $HTTP_POST_VARS["customer"], $HTTP_POST_VARS["note"]));
+    } else if (@$HTTP_POST_VARS["cat"] == "update") {
+        $tpl->assign("result", Customer::updateNote($HTTP_POST_VARS["id"], $HTTP_POST_VARS["project"], $HTTP_POST_VARS["customer"], $HTTP_POST_VARS["note"])); 
+    } elseif (@$HTTP_POST_VARS["cat"] == "delete") {
+        $tpl->assign("result", Customer::removeNotes($HTTP_POST_VARS['items']));
+    } elseif (!empty($HTTP_GET_VARS['prj_id'])) {
+        $tpl->assign("info", array('cno_prj_id' => $HTTP_GET_VARS['prj_id']));
+        $tpl->assign('customers', Customer::getAssocList($HTTP_GET_VARS['prj_id']));
+    }
+
+    if (@$HTTP_GET_VARS["cat"] == "edit") {
+        $info = Customer::getNoteDetailsByID($HTTP_GET_VARS["id"]);
+        if (!empty($HTTP_GET_VARS['prj_id'])) {
+            $info['cno_prj_id'] = $HTTP_GET_VARS['prj_id'];
+        }
+        $tpl->assign('customers', Customer::getAssocList($info['cno_prj_id']));
+        $tpl->assign("info", $info);
+    }
+
+    $tpl->assign("list", Customer::getNoteList());
+    $tpl->assign("project_list", Project::getAll());
+} else {
+    $tpl->assign("show_not_allowed_msg", true);
+}
+
+$tpl->displayTemplate();
+?>
