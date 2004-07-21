8734c5f77a01b5da6e7eaa6abc68cf7aab29aedf
Bryan Alsdorf <balsdorf@gmail.com>
2004-07-21 20:50:11 +0000

    Added option to set custom workflow for a project.
    (Logical change 1.176)

--- eventum/manage/projects.php
+++ eventum/manage/projects.php
@@ -34,6 +34,7 @@ include_once(APP_INC_PATH . "class.auth.php");
 include_once(APP_INC_PATH . "class.project.php");
 include_once(APP_INC_PATH . "class.user.php");
 include_once(APP_INC_PATH . "class.status.php");
+include_once(APP_INC_PATH . "class.workflow.php");
 include_once(APP_INC_PATH . "db_access.php");
 
 $tpl = new Template_API();
@@ -65,6 +66,7 @@ if (($role_id == User::getRoleID('administrator')) || ($role_id == User::getRole
     $tpl->assign("user_options", User::getActiveAssocList());
     $tpl->assign("status_options", Status::getAssocList());
     $tpl->assign("customer_backends", Customer::getBackendList());
+    $tpl->assign("workflow_backends", Workflow::getBackendList());
 } else {
     $tpl->assign("show_not_allowed_msg", true);
 }
