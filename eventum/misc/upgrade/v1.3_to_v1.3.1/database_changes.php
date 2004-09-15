ec82e272d0d9570d05f94ebebe8cda2951ba2892
João Prado Maia <jpm@pessoal.org>
2004-09-15 20:07:12 +0000

    (Logical change 1.289)

---
+++ eventum/misc/upgrade/v1.3_to_v1.3.1/database_changes.php
@@ -0,0 +1,41 @@
+<?php
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+
+$stmt = "desc eventum_mail_queue";
+$stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+$columns = $GLOBALS["db_api"]->dbh->getCol($stmt);
+if (PEAR::isError($columns)) {
+    echo "<pre>";var_dump($columns);echo "</pre>";
+    exit;
+}
+
+if (!in_array('maq_iss_id', $columns)) {
+    $stmt = "ALTER TABLE eventum_mail_queue ADD COLUMN maq_iss_id int(11) unsigned AFTER maq_id";
+    $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+    $update = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($update)) {
+        echo "<pre>";var_dump($update);echo "</pre>";
+        exit;
+    }
+    $stmt = "ALTER TABLE eventum_mail_queue ADD INDEX maq_iss_id (maq_iss_id)";
+    $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+    $update = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($update)) {
+        echo "<pre>";var_dump($update);echo "</pre>";
+        exit;
+    }
+}
+
+if (!in_array('maq_subject', $columns)) {
+    $stmt = "ALTER TABLE eventum_mail_queue ADD COLUMN maq_subject varchar(255) NOT NULL AFTER maq_recipient";
+    $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+    $update = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($update)) {
+        echo "<pre>";var_dump($update);echo "</pre>";
+        exit;
+    }
+}
+?>
+done
