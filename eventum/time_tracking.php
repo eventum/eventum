ce3a3072e33e21497a93312d92b7d365835dc009
Bryan Alsdorf <balsdorf@gmail.com>
2004-09-25 13:07:11 +0000

    (Logical change 1.324)

---
+++ eventum/time_tracking.php
@@ -0,0 +1,56 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003, 2004 MySQL AB                                    |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: João Prado Maia <jpm@mysql.com>                             |
+// +----------------------------------------------------------------------+
+//
+// @(#) $Id$
+//
+include_once("config.inc.php");
+include_once(APP_INC_PATH . "class.template.php");
+include_once(APP_INC_PATH . "class.auth.php");
+include_once(APP_INC_PATH . "class.time_tracking.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+$tpl = new Template_API();
+$tpl->setTemplate("add_time_tracking.tpl.html");
+
+Auth::checkAuthentication(APP_COOKIE, 'index.php?err=5', true);
+
+if (@$HTTP_POST_VARS["cat"] == "add_time") {
+    $res = Time_Tracking::insertEntry();
+    $tpl->assign("time_add_result", $res);
+    $issue_id = $HTTP_POST_VARS["issue_id"];
+} else {
+    $issue_id = $HTTP_GET_VARS["iss_id"];
+}
+
+$tpl->assign(array(
+            "issue_id"         => $issue_id,
+            "time_categories"  => Time_Tracking::getAssocCategories()
+));
+$tpl->assign("current_user_prefs", Prefs::get(Auth::getUserID()));
+
+$tpl->displayTemplate();
+?>
