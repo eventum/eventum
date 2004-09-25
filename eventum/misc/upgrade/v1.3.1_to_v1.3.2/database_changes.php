eb0c9b800102d0ec86d51ca1e5617693507c74c0
João Prado Maia <jpm@pessoal.org>
2004-09-25 07:52:41 +0000

    Fix the customer note messup when people upgrade to the latest 1.3.2
    (Logical change 1.319)

--- eventum/misc/upgrade/v1.3.1_to_v1.3.2/database_changes.php
+++ eventum/misc/upgrade/v1.3.1_to_v1.3.2/database_changes.php
@@ -10,7 +10,6 @@ if (PEAR::isError($columns)) {
     echo "<pre>";var_dump($columns);echo "</pre>";
     exit;
 }
-
 $stmts = array();
 // need to handle problems where the auto_increment key was not added to pri_id
 if (!strstr($columns[0]['Extra'], 'auto_increment')) {
@@ -20,6 +19,19 @@ if (!strstr($columns[0]['Key'], 'PRI')) {
     $stmts[] = "ALTER TABLE eventum_project_priority DROP PRIMARY KEY";
     $stmts[] = "ALTER TABLE eventum_project_priority ADD PRIMARY KEY(pri_id)";
 }
+
+$stmt = "desc eventum_customer_note";
+$stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+$columns = $GLOBALS["db_api"]->dbh->getCol($stmt);
+if (PEAR::isError($columns)) {
+    echo "<pre>";var_dump($columns);echo "</pre>";
+    exit;
+}
+// need to handle the problem in which upgrades from 1.2.2 to 1.3 didn't get the cno_prj_id field
+if (!in_array('cno_prj_id', $columns)) {
+    $stmts[] = "ALTER TABLE eventum_customer_note ADD COLUMN cno_prj_id int(11) unsigned NOT NULL";
+}
+
 foreach ($stmts as $stmt) {
     $res = $GLOBALS["db_api"]->dbh->query($stmt);
     if (PEAR::isError($res)) {
