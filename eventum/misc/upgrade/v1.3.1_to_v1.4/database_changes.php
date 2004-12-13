60f5a842e7f9c7d21a1c21cab90b1a5fabd2cbcb
Bryan Alsdorf <balsdorf@gmail.com>
2004-12-13 06:17:35 +0000

    fixed typo.
    (Logical change 1.434)

--- eventum/misc/upgrade/v1.3.1_to_v1.4/database_changes.php
+++ eventum/misc/upgrade/v1.3.1_to_v1.4/database_changes.php
@@ -75,7 +75,7 @@ $stmts[] = "CREATE TABLE eventum_columns_to_display (
 
 // add filtering by events in past x hours to advanced search
 $stmts[] = "ALTER TABLE eventum_custom_filter ADD COLUMN cst_created_date_time_period smallint(4) AFTER cst_created_date_filter_type";
-$stmts[] = "ALTER TABLE eventum_custom_filter ADD COLUMN cst_updated_date_time_period smallint(4) AFTER cst_updated_date_filter_type;"
+$stmts[] = "ALTER TABLE eventum_custom_filter ADD COLUMN cst_updated_date_time_period smallint(4) AFTER cst_updated_date_filter_type";
 $stmts[] = "ALTER TABLE eventum_custom_filter ADD COLUMN cst_last_response_date_time_period smallint(4) AFTER cst_last_response_date_filter_type";
 $stmts[] = "ALTER TABLE eventum_custom_filter ADD COLUMN cst_first_response_date_time_period smallint(4) AFTER cst_first_response_date_filter_type";
 $stmts[] = "ALTER TABLE eventum_custom_filter ADD COLUMN cst_closed_date_time_period smallint(4) AFTER cst_closed_date_filter_type";
