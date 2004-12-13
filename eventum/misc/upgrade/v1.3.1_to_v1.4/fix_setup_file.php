c69ee44962a4a0240d28a761ad23fb84b94e02f2
Bryan Alsdorf <balsdorf@gmail.com>
2004-12-13 06:17:35 +0000

    (Logical change 1.434)

---
+++ eventum/misc/upgrade/v1.3.1_to_v1.4/fix_setup_file.php
@@ -0,0 +1,19 @@
+<?php
+// changes the setup file from just containing the setup string, to be a php file with an array.
+
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "class.setup.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+$contents = file_get_contents(APP_SETUP_FILE);
+
+$array = unserialize(base64_decode($contents));
+
+$res = Setup::save($array);
+
+if ($res != 1) {
+    echo "Unable to write to file '" . APP_SETUP_FILE . "'.<br />\nPlease verify this file is writeable and try again.";
+    exit;
+}
+?>
+Done. 
