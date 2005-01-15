ccc8fbd44a981e82165c992e854c1beaf9046756
João Prado Maia <jpm@pessoal.org>
2005-01-15 17:03:29 +0000

    Prevent PHP warning from being displayed when running the process_mail_queue.php script
    (Logical change 1.462)

--- eventum/include/class.error_handler.php
+++ eventum/include/class.error_handler.php
@@ -98,7 +98,7 @@ class Error_Handler
             $msg .= "'$error_msg'\n\n";
         }
         $msg .= "That happened on page '" . $HTTP_SERVER_VARS["PHP_SELF"] . "' from IP Address '" . getenv("REMOTE_ADDR") . "' coming from the page (referrer) '" . getenv("HTTP_REFERER") . "'.\n\n";
-        $msg .= "The user agent given was '" . $HTTP_SERVER_VARS['HTTP_USER_AGENT'] . "'.\n\n";
+        $msg .= "The user agent given was '" . @$HTTP_SERVER_VARS['HTTP_USER_AGENT'] . "'.\n\n";
         $msg .= "Sincerely yours,\nAutomated Error_Handler Class";
         // only try to include the backtrace if we are on PHP 4.3.0 or later
         if (version_compare(phpversion(), "4.3.0", ">=")) {
