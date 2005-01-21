c626dde25b4c0ed5362661d425667614f9bac7a4
Bryan Alsdorf <balsdorf@gmail.com>
2005-01-21 02:13:28 +0000

    (Logical change 1.468)

---
+++ eventum/misc/upgrade/v1.4_to_1.5/database_changes.php
@@ -0,0 +1,34 @@
+<?php
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+
+$stmts = array();
+
+// see if this category already exists and if not, add it
+$sql = "SELECT
+            count(*)
+        FROM
+            " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "time_tracking_category
+        WHERE
+            ttc_title = 'Email Discussion'";
+$res = $GLOBALS["db_api"]->dbh->getOne($sql);
+if ($res == 0) {
+    // test if this works
+    $stmts[] = "INSERT INTO eventum_time_tracking_category (ttc_id, ttc_title, ttc_created_date) VALUES (9, 'Email Discussion', NOW())";
+}
+
+$stmts[] = "ALTER TABLE eventum_project_user ADD COLUMN pru_role tinyint(1) unsigned default 1";
+
+
+foreach ($stmts as $stmt) {
+    $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+    $res = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($res)) {
+        echo "<pre>";var_dump($res);echo "</pre>";
+        exit;
+    }
+}
+
+?>
+done
---
+++ eventum/misc/upgrade/v1.4_to_1.5/set_user_roles.php
@@ -0,0 +1,72 @@
+<?php
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "class.prefs.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+$sql = "SELECT
+            usr_id,
+            usr_role
+        FROM
+            " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "user
+        WHERE
+            usr_role != ''";
+$res = $GLOBALS["db_api"]->dbh->getAssoc($sql);
+if (PEAR::isError($res)) {
+    echo "<pre>";var_dump($res);echo "</pre>";
+    exit;
+}
+
+foreach ($res as $usr_id => $role) {
+    $sql = "UPDATE
+                " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "project_user
+            SET
+                pru_role = $role
+            WHERE
+                pru_usr_id = $usr_id";
+    $res = $GLOBALS["db_api"]->dbh->query($sql);
+    if (PEAR::isError($res)) {
+        echo "<pre>";var_dump($res);echo "</pre>";
+        exit;
+    }
+    
+    // handle preferences
+    $prefs = Prefs::get($usr_id);
+    echo "<pre>$usr_id";print_r($prefs);echo "</pre>";
+    $receive_assigned_emails = @$prefs['receive_assigned_emails'];
+    if (empty($receive_assigned_emails)) {
+        $receive_assigned_emails = 0;
+    }
+    $receive_new_emails = @$prefs['receive_new_emails'];
+    if (empty($receive_new_emails)) {
+        $receive_new_emails = 0;
+    }
+    echo "$receive_assigned_emails - $receive_new_emails<br />";
+    
+    $projects = Project::getAssocList($usr_id);
+    $prefs['receive_new_emails'] = array();
+    $prefs['receive_assigned_emails'] = array();
+    foreach ($projects as $prj_id => $project_title) {
+        $prefs['receive_assigned_emails'][$prj_id] = $receive_assigned_emails;
+        $prefs['receive_new_emails'][$prj_id] = $receive_new_emails;
+    }
+    echo "<pre>";print_r($prefs);echo "</pre><hr>";
+    $sql = "UPDATE
+                " . APP_DEFAULT_DB . "." . APP_TABLE_PREFIX . "user
+            SET
+                usr_preferences='" . Misc::escapeString(serialize($prefs)) . "'
+            WHERE
+                usr_id=$usr_id";
+    $res = $GLOBALS["db_api"]->dbh->query($sql);
+    if (PEAR::isError($res)) {
+        echo "<pre>";print_r($res);exit;
+    }
+}
+
+?>
+done.<br />
+
+Please check that you can successfully login. If everything looks in order you can go ahead and
+run the following statement to drop a now redundant column.<br />
+<pre>
+ALTER TABLE <?php echo APP_TABLE_PREFIX; ?>user DROP column usr_role;
+</pre>
