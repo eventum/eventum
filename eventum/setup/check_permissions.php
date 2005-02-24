6f3629b71cc65738178c5f41bc6bde1b7575ed97
João Prado Maia <jpm@pessoal.org>
2005-02-24 18:27:20 +0000

    Some wording fixes
    (Logical change 1.527)

--- eventum/setup/check_permissions.php
+++ eventum/setup/check_permissions.php
@@ -10,32 +10,32 @@ echo "<html>
 <body>";
 
 echo "<p class=\"default\">This script checks your eventum directory for permission problems. Since different hosts will have
-permissions setup differently this script cannot automatically correct fix permission problems.</p>
-<p class=\"default\">As a general rule, your webserver should be running as 'nobody', a user with few permissions
+permissions setup differently this script cannot automatically fix permission problems.</p>
+<p class=\"default\">As a general rule, your webserver should be running as 'nobody' (a user with few permissions)
 and your files should not be writable from the web. Only your logs (" . APP_LOG_PATH . ") and setup (" . 
-APP_SETUP_FILE . ") files need to be writeable by the web server.";
+APP_SETUP_FILE . ") files need to be writable by the web server.</p>";
 
-echo "<table border=\"1\">
+echo "<table cellpadding=\"3\">
         <tr class=\"default\">
-          <th>Title</th>
-          <th>Path</th>
-          <th>Is Readable?</th>
-          <th>Is Writeable?</th>
-          <th>Comments</th>
+          <th style=\"border: 1px solid black;\">Title</th>
+          <th style=\"border: 1px solid black;\">Path</th>
+          <th style=\"border: 1px solid black;\">Is Readable?</th>
+          <th style=\"border: 1px solid black;\">Is Writeable?</th>
+          <th style=\"border: 1px solid black;\">Comments</th>
         </tr>";
 
 // check if base directory is writeable
-check_file("Base Directory", APP_PATH, "Base directory should be read only by your web server", "r");
+check_file("Base Directory", APP_PATH, "Base directory should be read only by your web server.", "r");
 
-check_file("Log Directory", APP_LOG_PATH, "Log directory should be writeable by your webserver. However, your web
+check_file("Log Directory", APP_LOG_PATH, "Log directory should be writable by your webserver. However, your web
     server should <b>NOT</b> be able to read this directory to prevent outsiders from viewing your logs.", "w");
 
-check_file("Setup File", APP_SETUP_FILE, "The setup file should be both readable and writeable from your web server.
-    The setup file is used to store general settings in.<br /><b>Note:</b> Once you have eventum configured, you can 
+check_file("Setup File", APP_SETUP_FILE, "The setup file should be both readable and writable from your web server.
+    The setup file is used to store general settings.<br /><b>Note:</b> Once you have eventum configured, you can 
     mark this file as 'read only' if you want.", "rw");
 
 echo "</table>
-<p class=\"default\">Once you are done setting permissions, you should remove or restrict access to this setup directory (" . APP_PATH . "setup/).
+<p class=\"default\">Once you are done setting permissions, you should remove or restrict access to this setup directory (" . APP_PATH . "setup/).</p>
 </body>
 </html>";
 
@@ -61,11 +61,11 @@ function check_file($title, $path, $comment, $desired_permission)
     }
     
     echo "<tr class=\"default\">
-            <td>$title</td>
-            <td>$path</td>
-            <td align=\"center\">" . ($readable == true ? "yes" : "no") . "</td>
-            <td align=\"center\">" . ($writeable == true ? "yes" : "no") . "</td>
-            <td><span style=\"color: $color\">$comment</span></td>
+            <td style=\"border: 1px solid black;\">$title</td>
+            <td style=\"border: 1px solid black;\">$path</td>
+            <td align=\"center\" style=\"border: 1px solid black;\">" . ($readable == true ? "yes" : "no") . "</td>
+            <td align=\"center\" style=\"border: 1px solid black;\">" . ($writeable == true ? "yes" : "no") . "</td>
+            <td style=\"border: 1px solid black;\"><span style=\"color: $color\">$comment</span></td>
           </tr>";
 }
 ?>
