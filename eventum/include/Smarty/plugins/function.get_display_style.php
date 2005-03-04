2d05f85b57082752442032ec6a5ae060a26280ab
João Prado Maia <jpm@pessoal.org>
2005-03-04 20:26:30 +0000

    Changed the behavior of the view issue screen to automatically hide tables without any data
    (Logical change 1.588)

--- eventum/include/Smarty/plugins/function.get_display_style.php
+++ eventum/include/Smarty/plugins/function.get_display_style.php
@@ -9,6 +9,12 @@ function smarty_function_get_display_style($params, &$smarty)
     if (!empty($HTTP_COOKIE_VARS["visibility_" . $element_name])) {
         $html_result = 'style="display:' . $HTTP_COOKIE_VARS["visibility_" . $element_name] . ';"';
     }
+    // automatically hide the table if there is nothing to be displayed
+    if (isset($total)) {
+        if ($total < 1) {
+            $html_result = 'style="display: none;"';
+        }
+    }
     
     if ($print_result) {
         print $html_result;
