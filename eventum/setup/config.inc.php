ecf038b408d943c7d0e112cdee0b5ff17f440e34
Bryan Alsdorf <balsdorf@gmail.com>
2005-04-20 23:18:42 +0000

    merge
    2005/04/20 18:13:06-05:00 mysql.com!bryan
    Dispel magic quotes on $_REQUEST array to fix bug #9915
    
    (Logical change 1.697)

--- eventum/setup/config.inc.php
+++ eventum/setup/config.inc.php
@@ -142,6 +142,7 @@ if (isset($_GET)) {
 // fix magic_quote_gpc'ed values (i wish i knew who is the person behind this)
 $HTTP_GET_VARS =& Misc::dispelMagicQuotes($HTTP_GET_VARS);
 $HTTP_POST_VARS =& Misc::dispelMagicQuotes($HTTP_POST_VARS);
+$_REQUEST =& Misc::dispelMagicQuotes($_REQUEST);
 
 // handle the language preferences now
 @include_once(APP_INC_PATH . "class.language.php");
