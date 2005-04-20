04519c87aa72fcf4906c2fd9468a92ca4dc7c708
João Prado Maia <jpm@pessoal.org>
2005-04-20 15:48:06 +0000

    (Logical change 1.692)

---
+++ eventum/misc/upgrade/v1.5.2_to_v1.5.3/database_changes.php
@@ -0,0 +1,20 @@
+<?php
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+
+$stmts = array();
+
+$stmts[] = "UPDATE eventum_columns_to_display SET ctd_field='sta_rank' WHERE ctd_field='iss_sta_id'";
+
+foreach ($stmts as $stmt) {
+    $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+    $res = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($res)) {
+        echo "<pre>";var_dump($res);echo "</pre>";
+        exit;
+    }
+}
+
+?>
+done
