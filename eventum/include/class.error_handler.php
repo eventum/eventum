55349b530e4687cee4c61ecd3a98d58fd9c537f4
João Prado Maia <jpm@pessoal.org>
2005-05-04 17:42:13 +0000

    It's always the same error message, so just stop execution
    (Logical change 1.709)

--- eventum/include/class.error_handler.php
+++ eventum/include/class.error_handler.php
@@ -114,13 +114,13 @@ class Error_Handler
             $msg .= $contents;
             ob_end_clean();
         }
+        // avoid triggering an email notification about a query that 
+        // was bigger than max_allowed_packet (usually 16 megs on 3.23 
+        // client libraries)
+        if (strlen($msg) > 16777216) {
+            return false;
+        }
         foreach ($notify_list as $notify_email) {
-            // avoid triggering an email notification about a query that 
-            // was bigger than max_allowed_packet (usually 16 megs on 3.23 
-            // client libraries)
-            if (strlen($msg) > 16777216) {
-                continue;
-            }
             $mail = new Mail_API;
             $mail->setTextBody($msg);
             $mail->send($setup['smtp']['from'], $notify_email, $subject);
