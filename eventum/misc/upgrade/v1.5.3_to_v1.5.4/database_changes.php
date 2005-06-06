00aeb662343bfaa33569c7a81b07734bdb4782bb
João Prado Maia <jpm@pessoal.org>
2005-06-06 15:34:46 +0000

    (Logical change 1.752)

---
+++ eventum/misc/upgrade/v1.5.3_to_v1.5.4/database_changes.php
@@ -0,0 +1,26 @@
+<?php
+include_once("../../../config.inc.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+
+$stmts = array();
+
+$stmts[] = "CREATE FULLTEXT INDEX ft_issue ON eventum_issue (iss_summary, iss_description)";
+$stmts[] = "CREATE FULLTEXT INDEX ft_support_email ON eventum_support_email_body (seb_body)";
+$stmts[] = "CREATE FULLTEXT INDEX ft_note ON eventum_note (not_title,not_note)";
+$stmts[] = "CREATE FULLTEXT INDEX ft_time_tracking ON eventum_time_tracking (ttr_summary)";
+$stmts[] = "CREATE FULLTEXT INDEX ft_phone_support ON eventum_phone_support (phs_description)";
+$stmts[] = "ALTER TABLE eventum_time_tracking ADD INDEX ttr_iss_id(ttr_iss_id)";
+$stmts[] = "ALTER TABLE eventum_custom_filter ADD COLUMN cst_use_fulltext tinyint(1) DEFAULT 0 AFTER cst_keywords";
+
+foreach ($stmts as $stmt) {
+    $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
+    $res = $GLOBALS["db_api"]->dbh->query($stmt);
+    if (PEAR::isError($res)) {
+        echo "<pre>";var_dump($res);echo "</pre>";
+        exit;
+    }
+}
+
+?>
+done
