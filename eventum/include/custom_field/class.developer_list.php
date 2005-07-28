ec00d669f18cde21b2e1f501dc2fd4d4d3762e58
Bryan Alsdorf <balsdorf@gmail.com>
2005-07-28 15:39:36 +0000

    New BitKeeper file ``include/custom_field/class.developer_list.php''
    (Logical change 1.810)

---
+++ eventum/include/custom_field/class.developer_list.php
@@ -0,0 +1,47 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003, 2004, 2005 MySQL AB                              |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Bryan Alsdorf <bryan@mysql.com>                             |
+// +----------------------------------------------------------------------+
+//
+
+include_once(APP_INC_PATH . "class.user.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+
+/**
+ * Custom field backend to return list of users with a role of developer or above.
+ * 
+ * @author Bryan Alsdorf <bryan@mysql.com>
+ */
+class Developer_List_Custom_Field_Backend
+{
+    function getList($fld_id)
+    {
+        return User::getActiveAssocList(Auth::getCurrentProject(), User::getRoleID("Standard User"));
+    }
+}
+
+
+?>
