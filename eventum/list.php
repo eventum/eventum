fc09c2ab0d7024c2b6f79f83bcebd20e7726f04d
Bryan Alsdorf <balsdorf@gmail.com>
2005-08-19 02:24:17 +0000

    Only show releases with a date in the future on bulk update form.
    (Logical change 1.870)

--- eventum/list.php
+++ eventum/list.php
@@ -120,6 +120,7 @@ $tpl->assign("csts", Filter::getListing(true));
 $tpl->assign("filter_info", Filter::getFiltersInfo());
 $tpl->assign("categories", Category::getAssocList($prj_id));
 $tpl->assign("releases", Release::getAssocList($prj_id, true));
+$tpl->assign("available_releases", Release::getAssocList($prj_id));
 $tpl->assign("groups", $groups);
 $tpl->assign("custom_fields_display", $custom_fields_display);
 $tpl->assign("reporters", Project::getReporters($prj_id));
