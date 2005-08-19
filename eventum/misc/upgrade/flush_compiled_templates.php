95ba6f2766aca44af4778fb2d9952e6808ebb00d
Bryan Alsdorf <balsdorf@gmail.com>
2005-08-19 01:25:18 +0000

    New BitKeeper file ``misc/upgrade/flush_compiled_templates.php''
    (Logical change 1.869)

---
+++ eventum/misc/upgrade/flush_compiled_templates.php
@@ -0,0 +1,12 @@
+<?php
+include_once("../../config.inc.php");
+include_once(APP_INC_PATH . "class.misc.php");
+
+$compile_dir = APP_PATH . "templates_c";
+$templates = Misc::getFileList($compile_dir);
+foreach ($templates as $filename) {
+    unlink($compile_dir . '/' . $filename);
+}
+
+?>
+done
