ae78d861f470c25752b6705754fbaabf9686f2f1
Bryan Alsdorf <balsdorf@gmail.com>
2005-08-22 20:05:36 +0000

    New BitKeeper file ``js/dynamic_custom_field.js.php''
    (Logical change 1.871)

---
+++ eventum/js/dynamic_custom_field.js.php
@@ -0,0 +1,28 @@
+<?php
+include_once("../config.inc.php");
+include_once(APP_INC_PATH . "class.custom_field.php");
+include_once(APP_INC_PATH . "class.template.php");
+include_once(APP_INC_PATH . "db_access.php");
+
+Auth::checkAuthentication(APP_COOKIE);
+
+if (!empty($_REQUEST['iss_id'])) {
+    $fields = Custom_Field::getListByIssue(Auth::getCurrentProject(), $_REQUEST['iss_id']);
+} else {
+    $fields = Custom_Field::getListByProject(Auth::getCurrentProject(), $_REQUEST['form_type']);
+}
+
+$data = array();
+foreach ($fields as $field) {
+    $backend = Custom_Field::getBackend($field['fld_id']);
+    if ((is_object($backend)) && (is_subclass_of($backend, "Dynamic_Select_Custom_Field_Backend"))) {
+        $field['structured_data'] = $backend->getStructuredData();
+        $data[] = $field;
+    }
+}
+$tpl = new Template_API();
+$tpl->setTemplate("js/dynamic_custom_field.tpl.js");
+$tpl->assign("fields", $data);
+$tpl->displayTemplate();
+?>
+
