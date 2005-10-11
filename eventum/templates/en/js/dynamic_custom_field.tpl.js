4d1dca2bb4a750c5898e60994ddb13a728ce179b
Bryan Alsdorf <balsdorf@gmail.com>
2005-10-11 18:05:34 +0000

    Handle date fields better.
    (Logical change 1.894)

--- eventum/templates/en/js/dynamic_custom_field.tpl.js
+++ eventum/templates/en/js/dynamic_custom_field.tpl.js
@@ -4,6 +4,7 @@ var dynamic_options = new Array();
 i = dynamic_options.length;
 dynamic_options[i] = new Object();
 dynamic_options[i].target_field_id = {$field.fld_id};
+dynamic_options[i].fld_type = '{$field.fld_type}';
 dynamic_options[i].controlling_field_id = {$field.controlling_field_id};
 dynamic_options[i].controlling_field_name = '{$field.controlling_field_name}';
 dynamic_options[i].hide_when_no_options = '{$field.hide_when_no_options}';
@@ -47,11 +48,26 @@ function custom_field_init_dynamic_options(fld_id)
 {
     for (var i = 0; i < dynamic_options.length; i++) {
         if (dynamic_options[i].target_field_id == fld_id) {
-            // set alert on target field prompting them to choose controlling field first
-            target_field = getPageElement('custom_field_' + dynamic_options[i].target_field_id);
-            target_field.onmousedown = custom_field_prompt_choose_controller;
-            target_field.onkeypress = custom_field_prompt_choose_controller;
-
+            if (dynamic_options[i].fld_type == 'date') {
+                // date fields need special care
+                // set alert on target field prompting them to choose controlling field first
+                target_field = getPageElement('custom_field_' + dynamic_options[i].target_field_id + '_month');
+                target_field.onmousedown = custom_field_prompt_choose_controller;
+                target_field.onkeypress = custom_field_prompt_choose_controller;
+                
+                target_field = getPageElement('custom_field_' + dynamic_options[i].target_field_id + '_day');
+                target_field.onmousedown = custom_field_prompt_choose_controller;
+                target_field.onkeypress = custom_field_prompt_choose_controller;
+                
+                target_field = getPageElement('custom_field_' + dynamic_options[i].target_field_id + '_year');
+                target_field.onmousedown = custom_field_prompt_choose_controller;
+                target_field.onkeypress = custom_field_prompt_choose_controller;
+            } else {
+                // set alert on target field prompting them to choose controlling field first
+                target_field = getPageElement('custom_field_' + dynamic_options[i].target_field_id);
+                target_field.onmousedown = custom_field_prompt_choose_controller;
+                target_field.onkeypress = custom_field_prompt_choose_controller;
+            }
             // set event handler for controlling field
             controlling_field = getPageElement('custom_field_' + dynamic_options[i].controlling_field_id);
             controlling_field.onchange = custom_field_handle_controller_change;
@@ -84,51 +100,63 @@ function custom_field_set_new_options(controller, keep_target_value, target_fld_
     }
 
     for (var i = 0; i < details.length; i++) {
-        // see if this value has a set of options for the child field
-        target = getPageElement('custom_field_' + details[i].target_field_id);
-        if (keep_target_value) {
-            // get the current value
-            if (target.type == 'text') {
-                current_value = target.value;
-            } else {
-                current_value = target.options[target.selectedIndex].value;
+        // get the target/targets
+        var targets = new Array();
+        if (details[i].fld_type == 'date') {
+            targets[0] = getPageElement('custom_field_' + details[i].target_field_id + '_month');
+            targets[1] = getPageElement('custom_field_' + details[i].target_field_id + '_day');
+            targets[2] = getPageElement('custom_field_' + details[i].target_field_id + '_year');
+        } else {
+            targets[0] = target = getPageElement('custom_field_' + details[i].target_field_id);
+        }
+        
+        for (var targ_num = 0; targ_num < targets.length; targ_num++) {
+            target = targets[targ_num];    
+            // see if this value has a set of options for the child field
+            if (keep_target_value) {
+                // get the current value
+                if (target.type == 'text' || target.type == 'textarea') {
+                    current_value = target.value;
+                } else {
+                    current_value = target.options[target.selectedIndex].value;
+                }
             }
-        }
-        if (target.type != 'text') {
-            target.options.length = 1;
-        }
-        var show = false;
-        for (var j = 0; j < details[i].groups.length; j++) {
-            for (var k = 0; k < details[i].groups[j].keys.length; k++) {
-                if (details[i].groups[j].keys[k] == value) {
-                    show = true;
-                    for (var l = 0; l < details[i].groups[j].options.length; l++) {
-                        target.options[target.options.length] = details[i].groups[j].options[l];
-                    }
-                    target.onmousedown = '';
-                    target.onkeypress = '';
-                    if (keep_target_value) {
-                        if (target.type == 'text') {
-                            target.value = current_value;
-                        } else {
-                            selectOption(target.form, target.name, current_value);
+            if (target.type != 'text' && target.type != 'textarea' && details[i].fld_type != 'date') {
+                target.options.length = 1;
+            }
+            var show = false;
+            for (var j = 0; j < details[i].groups.length; j++) {
+                for (var k = 0; k < details[i].groups[j].keys.length; k++) {
+                    if (details[i].groups[j].keys[k] == value) {
+                        show = true;
+                        for (var l = 0; l < details[i].groups[j].options.length; l++) {
+                            target.options[target.options.length] = details[i].groups[j].options[l];
                         }
-                    } else {
-                        if (target.type == 'text') {
-                            target.value = '';
+                        target.onmousedown = '';
+                        target.onkeypress = '';
+                        if (keep_target_value) {
+                            if (target.type == 'text' || target.type == 'textarea') {
+                                target.value = current_value;
+                            } else {
+                                selectOption(target.form, target.name, current_value);
+                            }
                         } else {
-                            target.selectedIndex = 0;
+                            if (target.type == 'text' || target.type == 'textarea') {
+                                target.value = '';
+                            } else {
+                                target.selectedIndex = 0;
+                            }
                         }
                     }
                 }
             }
-        }
-
-        if (details[i].hide_when_no_options == 1) {
-            if (show == false) {
-                target.parentNode.parentNode.style.display = 'none';
-            } else {
-                target.parentNode.parentNode.style.display = getDisplayStyle();
+    
+            if (details[i].hide_when_no_options == 1) {
+                if (show == false) {
+                    target.parentNode.parentNode.style.display = 'none';
+                } else {
+                    target.parentNode.parentNode.style.display = getDisplayStyle();
+                }
             }
         }
     }
