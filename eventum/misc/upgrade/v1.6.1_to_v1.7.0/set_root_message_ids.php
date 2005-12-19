938972d7903f946835f38500f67039d438966812
Bryan Alsdorf <balsdorf@gmail.com>
2005-12-19 02:53:39 +0000

    Fix include path.
    (Logical change 1.931)

--- eventum/misc/upgrade/v1.6.1_to_v1.7.0/set_root_message_ids.php
+++ eventum/misc/upgrade/v1.6.1_to_v1.7.0/set_root_message_ids.php
@@ -2,7 +2,7 @@
 /*
  * Runonce script to set iss_root_message_id
  */
-include_once("../../config.inc.php");
+include_once("../../../config.inc.php");
 include_once(APP_INC_PATH . "db_access.php");
 include_once(APP_INC_PATH . "class.issue.php");
 
