46622e15e0e0e415f7a8c3315527093391f9ff56
Bryan Alsdorf <balsdorf@gmail.com>
2006-05-12 06:35:26 +0000

    Changed support_email table to not truncate long to and cc lists
    (Logical change 1.1013)

--- eventum/setup/changes.sql
+++ eventum/setup/changes.sql
@@ -650,3 +650,7 @@ ALTER TABLE eventum_issue_attachment ADD COLUMN iat_status enum('internal', 'pub
 ALTER TABLE eventum_issue_attachment ADD COLUMN iat_not_id int(11) unsigned DEFAULT NULL;
 
 ALTER TABLE eventum_note ADD COLUMN not_has_attachment tinyint(1) NOT NULL default 0;
+
+# May 12th
+ALTER TABLE eventum_support_email CHANGE COLUMN sup_to sup_to tinytext;
+ALTER TABLE eventum_support_email CHANGE COLUMN sup_cc sup_cc tinytext;
