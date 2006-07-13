759d5103231c233f0d2a4b1213bb64abc5474910
Bryan Alsdorf <balsdorf@gmail.com>
2006-07-13 22:07:46 +0000

    Added X-Eventum-Assignee header to emails
    (Logical change 1.1035)

--- eventum/include/class.mail.php
+++ eventum/include/class.mail.php
@@ -748,6 +748,9 @@ class Mail_API
                     $new_headers['X-Eventum-Level'] = $support_levels[Customer::getSupportLevelID($prj_id, $customer_id)];
                 }
             }
+            // add assignee header
+            $new_headers['X-Eventum-Assignee'] = join(',', User::getEmail(Issue::getAssignedUserIDs($issue_id)));
+
             $new_headers['X-Eventum-Category'] = Category::getTitle(Issue::getCategory($issue_id));
             $new_headers['X-Eventum-Project'] = Project::getName($prj_id);
         }
