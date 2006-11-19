9337cf66b3f21b7efb8e0552ae7a93b9ee58adc5
Elan Ruusamäe <glen@delfi.ee>
2006-11-19 21:30:32 +0000

    Update database changes.

--- eventum/misc/upgrade/v1.7.1_to_v2.0/database_changes.php
+++ eventum/misc/upgrade/v1.7.1_to_v2.0/database_changes.php
@@ -7,6 +7,7 @@ $stmts = array();
 $stmts[] = "ALTER TABLE eventum_support_email CHANGE COLUMN sup_to sup_to tinytext;";
 $stmts[] = "ALTER TABLE eventum_support_email CHANGE COLUMN sup_cc sup_cc tinytext;";
 $stmts[] = "ALTER TABLE eventum_user ADD COLUMN usr_lang varchar(5);";
+$stmts[] = "ALTER TABLE eventum_custom_field_option CHANGE COLUMN cfo_value cfo_value varchar(128) NOT NULL;";
 
 foreach ($stmts as $stmt) {
     $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
