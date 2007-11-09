3684d02b6cc507aa5c4ec1309edd3e5efc8b6582
Elan Ruusamäe <glen@delfi.ee>
2007-11-09 21:02:59 +0000

    - updated pear-Text_Diff to 0.3.1

--- eventum/include/pear/Text/Diff/Renderer.php
+++ eventum/include/pear/Text/Diff/Renderer.php
@@ -1,11 +1,16 @@
 <?php
 /**
+ * $Horde: framework/Text_Diff/Diff/Renderer.php,v 1.18 2007/09/25 22:10:36 chuck Exp $
+ *
  * A class to render Diffs in different formats.
  *
  * This class renders the diff in classic diff format. It is intended that
  * this class be customized via inheritance, to obtain fancier outputs.
  *
- * $Horde: framework/Text_Diff/Diff/Renderer.php,v 1.5 2004/10/13 09:30:20 jan Exp $
+ * Copyright 2004-2007 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
  *
  * @package Text_Diff
  */
@@ -41,6 +46,23 @@ class Text_Diff_Renderer {
     }
 
     /**
+     * Get any renderer parameters.
+     *
+     * @return array  All parameters of this renderer object.
+     */
+    function getParams()
+    {
+        $params = array();
+        foreach (get_object_vars($this) as $k => $v) {
+            if ($k[0] == '_') {
+                $params[substr($k, 1)] = $v;
+            }
+        }
+
+        return $params;
+    }
+
+    /**
      * Renders a diff.
      *
      * @param Text_Diff $diff  A Text_Diff object.
@@ -56,27 +78,43 @@ class Text_Diff_Renderer {
         $nlead = $this->_leading_context_lines;
         $ntrail = $this->_trailing_context_lines;
 
-        $this->_startDiff();
+        $output = $this->_startDiff();
 
-        foreach ($diff->getDiff() as $edit) {
+        $diffs = $diff->getDiff();
+        foreach ($diffs as $i => $edit) {
+            /* If these are unchanged (copied) lines, and we want to keep
+             * leading or trailing context lines, extract them from the copy
+             * block. */
             if (is_a($edit, 'Text_Diff_Op_copy')) {
+                /* Do we have any diff blocks yet? */
                 if (is_array($block)) {
-                    if (count($edit->orig) <= $nlead + $ntrail) {
+                    /* How many lines to keep as context from the copy
+                     * block. */
+                    $keep = $i == count($diffs) - 1 ? $ntrail : $nlead + $ntrail;
+                    if (count($edit->orig) <= $keep) {
+                        /* We have less lines in the block than we want for
+                         * context => keep the whole block. */
                         $block[] = $edit;
                     } else {
                         if ($ntrail) {
+                            /* Create a new block with as many lines as we need
+                             * for the trailing context. */
                             $context = array_slice($edit->orig, 0, $ntrail);
                             $block[] = &new Text_Diff_Op_copy($context);
                         }
-                        $this->_block($x0, $ntrail + $xi - $x0,
-                                      $y0, $ntrail + $yi - $y0,
-                                      $block);
+                        /* @todo */
+                        $output .= $this->_block($x0, $ntrail + $xi - $x0,
+                                                 $y0, $ntrail + $yi - $y0,
+                                                 $block);
                         $block = false;
                     }
                 }
+                /* Keep the copy block as the context for the next block. */
                 $context = $edit->orig;
             } else {
+                /* Don't we have any diff blocks yet? */
                 if (!is_array($block)) {
+                    /* Extract context lines from the preceding copy block. */
                     $context = array_slice($context, count($context) - $nlead);
                     $x0 = $xi - count($context);
                     $y0 = $yi - count($context);
@@ -97,53 +135,49 @@ class Text_Diff_Renderer {
         }
 
         if (is_array($block)) {
-            $this->_block($x0, $xi - $x0,
-                          $y0, $yi - $y0,
-                          $block);
+            $output .= $this->_block($x0, $xi - $x0,
+                                     $y0, $yi - $y0,
+                                     $block);
         }
 
-        return $this->_endDiff();
+        return $output . $this->_endDiff();
     }
 
     function _block($xbeg, $xlen, $ybeg, $ylen, &$edits)
     {
-        $this->_startBlock($this->_blockHeader($xbeg, $xlen, $ybeg, $ylen));
+        $output = $this->_startBlock($this->_blockHeader($xbeg, $xlen, $ybeg, $ylen));
+
         foreach ($edits as $edit) {
             switch (strtolower(get_class($edit))) {
             case 'text_diff_op_copy':
-                $this->_context($edit->orig);
+                $output .= $this->_context($edit->orig);
                 break;
 
             case 'text_diff_op_add':
-                $this->_added($edit->final);
+                $output .= $this->_added($edit->final);
                 break;
 
             case 'text_diff_op_delete':
-                $this->_deleted($edit->orig);
+                $output .= $this->_deleted($edit->orig);
                 break;
 
             case 'text_diff_op_change':
-                $this->_changed($edit->orig, $edit->final);
+                $output .= $this->_changed($edit->orig, $edit->final);
                 break;
-
-            default:
-                trigger_error("Unknown edit type", E_USER_ERROR);
             }
-
-            $this->_endBlock();
         }
+
+        return $output . $this->_endBlock();
     }
 
     function _startDiff()
     {
-        ob_start();
+        return '';
     }
 
     function _endDiff()
     {
-        $val = ob_get_contents();
-        ob_end_clean();
-        return $val;
+        return '';
     }
 
     function _blockHeader($xbeg, $xlen, $ybeg, $ylen)
@@ -155,45 +189,49 @@ class Text_Diff_Renderer {
             $ybeg .= ',' . ($ybeg + $ylen - 1);
         }
 
+        // this matches the GNU Diff behaviour
+        if ($xlen && !$ylen) {
+            $ybeg--;
+        } elseif (!$xlen) {
+            $xbeg--;
+        }
+
         return $xbeg . ($xlen ? ($ylen ? 'c' : 'd') : 'a') . $ybeg;
     }
 
     function _startBlock($header)
     {
-        echo $header . "\n";
+        return $header . "\n";
     }
 
     function _endBlock()
     {
+        return '';
     }
 
     function _lines($lines, $prefix = ' ')
     {
-        foreach ($lines as $line) {
-            echo "$prefix$line\n";
-        }
+        return $prefix . implode("\n$prefix", $lines) . "\n";
     }
 
     function _context($lines)
     {
-        $this->_lines($lines);
+        return $this->_lines($lines, '  ');
     }
 
     function _added($lines)
     {
-        $this->_lines($lines, '>');
+        return $this->_lines($lines, '> ');
     }
 
     function _deleted($lines)
     {
-        $this->_lines($lines, '<');
+        return $this->_lines($lines, '< ');
     }
 
     function _changed($orig, $final)
     {
-        $this->_deleted($orig);
-        echo "---\n";
-        $this->_added($final);
+        return $this->_deleted($orig) . "---\n" . $this->_added($final);
     }
 
 }
--- eventum/include/pear/Text/Diff/Renderer/unified.php
+++ eventum/include/pear/Text/Diff/Renderer/unified.php
@@ -1,11 +1,24 @@
 <?php
 /**
+ * $Horde: framework/Text_Diff/Diff/Renderer/unified.php,v 1.7 2007/10/28 04:58:36 chuck Exp $
+ *
  * "Unified" diff renderer.
  *
  * This class renders the diff in classic "unified diff" format.
  *
- * $Horde: framework/Text_Diff/Diff/Renderer/unified.php,v 1.3 2004/09/15 05:00:00 chuck Exp $
+ * Copyright 2004-2007 The Horde Project (http://www.horde.org/)
  *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author  Ciprian Popovici
+ * @package Text_Diff
+ */
+
+/** Text_Diff_Renderer */
+require_once 'Text/Diff/Renderer.php';
+
+/**
  * @package Text_Diff
  */
 class Text_Diff_Renderer_unified extends Text_Diff_Renderer {
@@ -31,20 +44,24 @@ class Text_Diff_Renderer_unified extends Text_Diff_Renderer {
         return "@@ -$xbeg +$ybeg @@";
     }
 
+    function _context($lines)
+    {
+        return $this->_lines($lines, ' ');
+    }
+
     function _added($lines)
     {
-        $this->_lines($lines, '+');
+        return $this->_lines($lines, '+');
     }
 
     function _deleted($lines)
     {
-        $this->_lines($lines, '-');
+        return $this->_lines($lines, '-');
     }
 
     function _changed($orig, $final)
     {
-        $this->_deleted($orig);
-        $this->_added($final);
+        return $this->_deleted($orig) . $this->_added($final);
     }
 
 }
