10f2d3209117caa37753124637072038f5381cfb
Bryan Alsdorf <balsdorf@gmail.com>
2007-11-14 02:41:47 +0000

    Fixed broken schema

--- eventum/misc/upgrade/v2.0_to_v2.1/database_changes.php
+++ eventum/misc/upgrade/v2.0_to_v2.1/database_changes.php
@@ -9,7 +9,7 @@ $stmts[] = "ALTER TABLE eventum_issue_custom_field ADD COLUMN icf_value_integer 
 $stmts[] = "ALTER TABLE eventum_issue_custom_field ADD COLUMN icf_value_date date NULL DEFAULT NULL";
 $stmts[] = "ALTER TABLE eventum_custom_field ADD COLUMN fld_close_form tinyint(1) NOT NULL DEFAULT 0";
 $stmts[] = "ALTER TABLE eventum_custom_field ADD COLUMN fld_close_form_required tinyint(1) NOT NULL DEFAULT 0";
-$stmts[] = "ALTER TABLE eventum_issue ADD COLUMN iss_customer_contract_id int(11) unsigned AFTER iss_customer_id";
+$stmts[] = "ALTER TABLE eventum_issue ADD COLUMN iss_customer_contract_id varchar(50) NULL AFTER iss_customer_contact_id";
 
 foreach ($stmts as $stmt) {
     $stmt = str_replace('eventum_', APP_TABLE_PREFIX, $stmt);
