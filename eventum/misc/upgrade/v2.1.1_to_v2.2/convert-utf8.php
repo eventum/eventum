648d277875f47e612c2509426747c4def3ef80fe
Elan Ruusamäe <glen@delfi.ee>
2008-05-30 06:23:38 +0000

    - convert database to utf8, based on queries from Bryan

---
+++ eventum/misc/upgrade/v2.1.1_to_v2.2/convert-utf8.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+// avoid setup redirecting us
+define('CONFIG_PATH', '/etc/webapps/eventum');
+
+if (!file_exists(CONFIG_PATH. '/config.php')) {
+	die("Can't find config.php from ". CONFIG_PATH . ". Did you forgot to copy config from old install?");
+}
+
+require_once '/usr/share/eventum/htdocs/init.php';
+require_once APP_INC_PATH . 'db_access.php';
+
+function apply_db_changes($stmts) {
+	foreach ($stmts as $stmt) {
+		$stmt = str_replace('%TABLE_PREFIX%', APP_TABLE_PREFIX, $stmt);
+		$res = $GLOBALS['db_api']->dbh->query($stmt);
+		if (PEAR::isError($res)) {
+			echo $res->getMessage(), ': ', $res->getDebugInfo(), "\n";
+			exit(1);
+		}
+	}
+}
+
+$changes = array();
+if (strtolower(APP_CHARSET) == 'utf-8' || strtolower(APP_CHARSET) == 'utf8') {
+	$collation = 'latin1_swedish_ci'; // XXX autodetect
+
+	// This command can generate the alter statement for all tables:
+	$query = 
+		"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES ".
+		"WHERE TABLE_SCHEMA = '".APP_SQL_DBNAME."' AND TABLE_COLLATION = '$collation'";
+	$res = $GLOBALS['db_api']->dbh->getAll($query, DB_FETCHMODE_ASSOC);
+	if (PEAR::isError($res)) {
+		echo $res->getMessage(), ': ', $res->getDebugInfo(), "\n";
+		exit(1);
+	}
+	foreach ($res as $col => $val) {
+		$changes[] = "ALTER TABLE {$val['TABLE_NAME']} CONVERT TO CHARACTER SET utf8";
+	}
+	// Alter database:
+	$changes[] = 'ALTER DATABASE `'.APP_SQL_DBNAME.'` DEFAULT CHARACTER SET utf8';
+}
+
+echo "Performing database changes (", count($changes), ") queries\n";
+apply_db_changes($changes);
+echo "Done!\n";
