d97b2b971d6277dbcff364c26867e31dd7762641
Elan Ruusamäe <glen@delfi.ee>
2008-06-03 13:00:36 +0000

    - merge better code from fix-charset.php

--- eventum/misc/upgrade/v2.1.1_to_v2.2/convert-utf8.php
+++ eventum/misc/upgrade/v2.1.1_to_v2.2/convert-utf8.php
@@ -11,35 +11,48 @@ if (!file_exists(CONFIG_PATH. '/config.php')) {
 require_once INSTALL_PATH . '/init.php';
 require_once APP_INC_PATH . 'db_access.php';
 
-function apply_db_changes($stmts) {
-	foreach ($stmts as $stmt) {
-		$stmt = str_replace('%TABLE_PREFIX%', APP_TABLE_PREFIX, $stmt);
-		$res = $GLOBALS['db_api']->dbh->query($stmt);
-		if (PEAR::isError($res)) {
-			echo $res->getMessage(), ': ', $res->getDebugInfo(), "\n";
-			exit(1);
-		}
-	}
-}
-
-$changes = array();
-if (strtolower(APP_CHARSET) == 'utf-8' || strtolower(APP_CHARSET) == 'utf8') {
-	$collation = 'latin1_swedish_ci'; // XXX autodetect
-
-	// This command can generate the alter statement for all tables:
-	$query = 
-		"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES ".
-		"WHERE TABLE_SCHEMA = '".APP_SQL_DBNAME."' AND TABLE_COLLATION = '$collation'";
+function db_getAll($query) {
+	$query = str_replace('%TABLE_PREFIX%', APP_TABLE_PREFIX, $query);
+	$query = str_replace('%DBNAME%', APP_SQL_DBNAME, $query);
 	$res = $GLOBALS['db_api']->dbh->getAll($query, DB_FETCHMODE_ASSOC);
 	if (PEAR::isError($res)) {
 		echo $res->getMessage(), ': ', $res->getDebugInfo(), "\n";
 		exit(1);
 	}
-	foreach ($res as $col => $val) {
-		$changes[] = "ALTER TABLE {$val['TABLE_NAME']} CONVERT TO CHARACTER SET utf8";
+	return $res;
+}
+
+function db_query($query) {
+	$query = str_replace('%TABLE_PREFIX%', APP_TABLE_PREFIX, $query);
+	$query = str_replace('%DBNAME%', APP_SQL_DBNAME, $query);
+	$res = $GLOBALS['db_api']->dbh->query($query);
+	if (PEAR::isError($res)) {
+		echo $res->getMessage(), ': ', $res->getDebugInfo(), "\n";
+		exit(1);
 	}
-	// Alter database:
-	$changes[] = 'ALTER DATABASE `'.APP_SQL_DBNAME.'` DEFAULT CHARACTER SET utf8';
+	return $res;
+}
+
+function apply_db_changes($stmts) {
+	foreach ($stmts as $stmt) {
+		db_query($stmt);
+	}
+}
+
+
+$changes = array();
+if (strtolower(APP_CHARSET) == 'utf-8' || strtolower(APP_CHARSET) == 'utf8') {
+	// convert tables
+	$res = db_getAll(
+		"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES ".
+		"WHERE TABLE_SCHEMA='%DBNAME%' AND TABLE_COLLATION NOT LIKE 'utf8\_%'"
+	);
+	foreach ($res as $idx => $row) {
+		$changes[] = "ALTER TABLE {$row['TABLE_NAME']} CONVERT TO CHARACTER SET utf8";
+	}
+
+	// convert database:
+	$changes[] = 'ALTER DATABASE `%DBNAME%` DEFAULT CHARACTER SET utf8';
 }
 
 echo "Performing database changes (", count($changes), ") queries\n";
