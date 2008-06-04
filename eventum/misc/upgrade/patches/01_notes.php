6fef86154da802e187c4b7d64a0f261bc6434645
Elan Ruusamäe <glen@delfi.ee>
2008-06-04 20:02:02 +0000

    - Combined alter table statements (bryan).

--- eventum/misc/upgrade/patches/01_notes.php
+++ eventum/misc/upgrade/patches/01_notes.php
@@ -2,10 +2,9 @@
 
 function db_patch_1() {
 	$stmts = array();
-	$stmts[] = "ALTER TABLE %TABLE_PREFIX%note CHANGE COLUMN not_blocked_message not_blocked_message longblob NULL";
+	$stmts[] = "ALTER TABLE %TABLE_PREFIX%note CHANGE COLUMN not_blocked_message not_full_message longblob NULL";
 	$stmts[] = "ALTER TABLE %TABLE_PREFIX%support_email_body CHANGE seb_full_email seb_full_email longblob NOT NULL";
 	$stmts[] = "ALTER TABLE %TABLE_PREFIX%mail_queue CHANGE COLUMN maq_body maq_body longblob NOT NULL";
-	$stmts[] = "ALTER TABLE %TABLE_PREFIX%note CHANGE COLUMN not_blocked_message not_full_message longblob NOT NULL";
 	$stmts[] = "ALTER TABLE %TABLE_PREFIX%note ADD COLUMN not_is_blocked tinyint(1) NOT NULL DEFAULT 0";
 	$stmts[] = "UPDATE %TABLE_PREFIX%note SET not_is_blocked = 1 WHERE not_full_message != ''";
 
