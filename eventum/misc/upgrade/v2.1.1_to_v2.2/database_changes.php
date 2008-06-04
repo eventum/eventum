6d5e999cdabf2a3c7a1b7fe7efd2eb0142b85289
Bryan Alsdorf <balsdorf@gmail.com>
2008-06-04 19:40:59 +0000

    Combined alter table statements.

--- eventum/misc/upgrade/v2.1.1_to_v2.2/database_changes.php
+++ eventum/misc/upgrade/v2.1.1_to_v2.2/database_changes.php
@@ -12,10 +12,9 @@ require_once APP_INC_PATH . 'db_access.php';
 
 $stmts = array();
 
-$stmts[] = "ALTER TABLE eventum_note CHANGE COLUMN not_blocked_message not_blocked_message longblob NULL";
+$stmts[] = "ALTER TABLE eventum_note CHANGE COLUMN not_blocked_message not_full_message longblob NULL";
 $stmts[] = "ALTER TABLE eventum_support_email_body CHANGE seb_full_email seb_full_email longblob NOT NULL";
 $stmts[] = "ALTER TABLE eventum_mail_queue CHANGE COLUMN maq_body maq_body longblob NOT NULL";
-$stmts[] = "ALTER TABLE eventum_note CHANGE COLUMN not_blocked_message not_full_message longblob NOT NULL";
 $stmts[] = "ALTER TABLE eventum_note ADD COLUMN not_is_blocked tinyint(1) NOT NULL DEFAULT 0";
 $stmts[] = "UPDATE eventum_note SET not_is_blocked = 1 WHERE not_full_message != ''";
 $stmts[] = "ALTER TABLE eventum_issue CHANGE COLUMN iss_res_id iss_res_id int(10) unsigned NULL DEFAULT NULL";
