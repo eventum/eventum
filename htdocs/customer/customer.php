77fd59207f7b05b69400be7c4be707b71ca61529
Bryan Alsdorf <balsdorf@gmail.com>
2010-03-24 17:27:53 -0500

    Added routing file so customer web files can be outside of webroot

---
+++ htdocs/customer/customer.php
@@ -0,0 +1,41 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 encoding=utf-8: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2010 Bryan Alsdorf                                     |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Bryan Alsdorf <balsdorf@gmail.com>                          |
+// +----------------------------------------------------------------------+
+
+/*
+ * This file is used to call various customer pages that are outside of the web
+ * root.
+ */
+require_once "../../init.php";
+
+$prj_id = Auth::getCurrentProject();
+$page = $_REQUEST['page'];
+
+if (!Customer::hasCustomerIntegration($prj_id)) {
+    echo "No customer integration for specified project.";exit;
+}
+
+require Customer::getPath($prj_id) . basename($page) . '.php';
