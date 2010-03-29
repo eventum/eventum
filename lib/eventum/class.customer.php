e320a4212e00ec86070ab71775d0e67d69c0960c
Bryan Alsdorf <balsdorf@gmail.com>
2010-03-29 15:00:09 -0500

    Really fix bug with this customer method

--- lib/eventum/class.customer.php
+++ lib/eventum/class.customer.php
@@ -991,7 +991,7 @@ class Customer
         if (method_exists($backend, 'getTemplatePath')) {
             return $backend->getTemplatePath();
         } else {
-            return 'customer/' . $backend->getName() . '/';
+            return 'customer/' . Customer::getBackendImplementationName($prj_id) . '/';
         }
     }
 
