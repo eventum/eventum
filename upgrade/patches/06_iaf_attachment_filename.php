8e877da12d1cbc5ed31e15e3dedc6540f6f7b2b2
Elan Ruusamäe <glen@delfi.ee>
2010-05-27 09:57:05 +0300

    - DB Fix: Decode attachment filenames from QuotedPrintable MIME encoding.

---
+++ upgrade/patches/06_iaf_attachment_filename.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Decode attachment filenames from QuotedPrintable MIME encoding.
+ */
+$res = db_getAll("SELECT iaf_id, iaf_filename FROM %TABLE_PREFIX%issue_attachment_file WHERE iaf_filename LIKE '%=?%'");
+if (PEAR::isError($res)) {
+    echo $res->getMessage(), ': ', $res->getDebugInfo(), "\n";
+    exit(1);
+}
+
+foreach ($res as $idx => $row) {
+	$iaf_filename = Mime_Helper::decodeQuotedPrintable($row['iaf_filename']);
+	echo $iaf_filename, "\n";
+	db_query("UPDATE %TABLE_PREFIX%issue_attachment_file ".
+		"SET iaf_filename='". DB_Helper::escapeString($iaf_filename). "' ".
+		"WHERE iaf_id=".$row['iaf_id']
+	);
+}
