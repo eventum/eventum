0f642a354eb6db2733dd4b4a8199aacea4105fec
Elan Ruusamäe <glen@delfi.ee>
2010-05-27 10:09:10 +0300

    - function exactly like upper init.php would behave (check filesize as well)

--- upgrade/init.php
+++ upgrade/init.php
@@ -1,12 +1,11 @@
 <?php
 // common init for upgrade scripst
-
 define('INSTALL_PATH', realpath(dirname(__FILE__) . '/..'));
 define('CONFIG_PATH', INSTALL_PATH.'/config');
 
 // avoid setup redirecting us
-if (!file_exists(CONFIG_PATH. '/config.php')) {
-	fwrite(STDERR, "\nCan't find config.php from ". CONFIG_PATH. ". Did you forgot to copy config from old install?\n");
+if (!file_exists(CONFIG_PATH. '/config.php') || !filesize(CONFIG_PATH. '/config.php')) {
+	fwrite(STDERR, "Can't find config.php in '". CONFIG_PATH. "'. Did you forgot to copy config from old install?\n");
 	exit(1);
 }
 
