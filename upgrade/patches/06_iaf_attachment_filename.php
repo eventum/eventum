a58623e313762493247272a408af87e54c9b4bcb
Elan Ruusamäe <glen@delfi.ee>
2010-05-27 09:57:47 +0300

    - remove debug

--- upgrade/patches/06_iaf_attachment_filename.php
+++ upgrade/patches/06_iaf_attachment_filename.php
@@ -10,7 +10,6 @@ if (PEAR::isError($res)) {
 
 foreach ($res as $idx => $row) {
 	$iaf_filename = Mime_Helper::decodeQuotedPrintable($row['iaf_filename']);
-	echo $iaf_filename, "\n";
 	db_query("UPDATE %TABLE_PREFIX%issue_attachment_file ".
 		"SET iaf_filename='". DB_Helper::escapeString($iaf_filename). "' ".
 		"WHERE iaf_id=".$row['iaf_id']
