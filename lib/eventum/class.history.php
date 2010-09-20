3e715688aa3f70936bb7adbfe5cf8be9155b2314
Elan Ruusamäe <glen@delfi.ee>
2010-09-20 14:03:05 +0300

    - more GET parameters for reports/weekly.php

--- lib/eventum/class.history.php
+++ lib/eventum/class.history.php
@@ -267,10 +267,10 @@ class History
                 "other"     =>  array()
             );
             if (count($res) > 0) {
-                if (isset($_POST['show_per_issue'])) {
+                if (isset($_REQUEST['show_per_issue'])) {
                     Time_Tracking::fillTimeSpentByIssueAndTime($res, $usr_id, $start, $end);
                 }
-                if (isset($_POST['separate_status_changed'])) {
+                if (isset($_REQUEST['separate_status_changed'])) {
                     self::fillStatusChangedOnlyIssues($res, $usr_id, $start, $end);
                 }
                 foreach ($res as $index => $row) {
@@ -280,7 +280,7 @@ class History
                     }
                     if (($separate_closed) && ($row['sta_is_closed'] == 1)) {
                         $data['closed'][] = $row;
-                    } elseif ((isset($_POST['separate_status_changed'])) && $row['only_stat_changed']) {
+                    } elseif ((isset($_REQUEST['separate_status_changed'])) && $row['only_stat_changed']) {
                         $data['status_changed'][] = $row;
                     } else {
                         $data['other'][] = $row;
