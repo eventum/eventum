7ed511e4d6a8930640ae9141a0e0a8e9be56920b
Bryan Alsdorf <balsdorf@gmail.com>
2010-11-15 15:35:55 -0600

    Put the contract id field in the customer backend template, not the main new.tpl.html page.

--- htdocs/new.php
+++ htdocs/new.php
@@ -155,7 +155,6 @@ if (Auth::getCurrentRole() == User::getRoleID('Customer')) {
     $tpl->assign(array(
         "customer_id" => $customer_id,
         "contact_id"  => User::getCustomerContactID($usr_id),
-        "contract_id" => $customer_details['contract_id'],
         "customer"    => $customer_details,
     ));
 }
