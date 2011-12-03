78440727b3911c37bf64ab4b5e472430aa9fee45
Elan Ruusamäe <glen@delfi.ee>
2011-12-03 19:08:55 +0200

    prepare for 2.3.2 release

--- init.php
+++ init.php
@@ -41,7 +41,7 @@ ini_set('memory_limit', '128M');
 ini_set('session.cache_limiter', 'nocache');
 
 define('APP_URL', 'https://launchpad.net/eventum/');
-define('APP_VERSION', '2.3.1');
+define('APP_VERSION', '2.3.2');
 
 // define base path
 define('APP_PATH', realpath(dirname(__FILE__)));
