675d60bf7cbf6aa0df7d9ed7cfc5ad71b06f571e
Bryan Alsdorf <balsdorf@gmail.com>
2012-04-30 23:28:24 -0500

    Added option to check if IRC bot process is still running

--- lib/eventum/class.lock.php
+++ lib/eventum/class.lock.php
@@ -35,12 +35,28 @@ class Lock
      *
      * @access  public
      * @param   string $name The name of this lock file
+     * @param   bool $check If we should check if the process exists in addition to check for a lock file
      * @return  boolean
      */
-    public static function acquire($name)
+    public static function acquire($name, $check = false)
     {
         $pid = self::getProcessID($name);
         if (!empty($pid)) {
+    	    // Test asks us to check if the process is still running
+            if ($check) {
+                $exists = true;
+                if (function_exists('posix_kill')) {
+                    $exists = posix_kill($pid, 0);
+                } else {
+                    $retval = 0;
+                    $out = array();
+                    $discard = exec('kill -s 0 ' . $pid, $out, $retval);
+                    $exists = $retval == 0;
+                }
+                if ($exists) {
+                    return false;
+                }
+            }
             return false;
         }
 
