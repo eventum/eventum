22bdcaa423bc94d57ac69a23ce879f56bdb87383
Elan Ruusamäe <glen@delfi.ee>
2013-05-08 22:30:22 +0300

    migrate each (non primary) project timetracking items to their timetracking categories

--- upgrade/patches/12_timetracking_project.sql
+++ upgrade/patches/12_timetracking_project.sql
@@ -10,7 +10,7 @@ select prj_id from eventum_project limit 1 into @prj_id;
 update %TABLE_PREFIX%time_tracking_category set ttc_prj_id=@prj_id;
 
 -- duplicate for others
-create /*temporary*/ table %TABLE_PREFIX%__migrate_ttc_prj (
+create /*temporary*/ table migrate_ttc_prj (
 	`ttc_id` int(11) NOT NULL AUTO_INCREMENT,
 	`orig_ttc_id` int(10) unsigned DEFAULT NULL,
 	`ttc_prj_id` int(11) unsigned NOT NULL DEFAULT '0',
@@ -21,18 +21,28 @@ create /*temporary*/ table %TABLE_PREFIX%__migrate_ttc_prj (
 
 -- adjust auto increment position
 select max(ttc_id) from %TABLE_PREFIX%time_tracking_category into @max_ttc_id;
-insert into %TABLE_PREFIX%__migrate_ttc_prj set ttc_id=@max_ttc_id;
+insert into migrate_ttc_prj set ttc_id=@max_ttc_id;
 
-insert into %TABLE_PREFIX%__migrate_ttc_prj
+insert into migrate_ttc_prj
 select 0 ttc_id, ttc_id orig_ttc_id, prj_id ttc_prj_id, ttc_title, ttc_created_date
 from %TABLE_PREFIX%time_tracking_category ttc, eventum_project prj
 where prj.prj_id!=@prj_id order by ttc_prj_id;
 
-delete from %TABLE_PREFIX%__migrate_ttc_prj where orig_ttc_id is null;
+delete from migrate_ttc_prj where orig_ttc_id is null;
 
 -- finally import it
 insert into %TABLE_PREFIX%time_tracking_category (ttc_id, ttc_prj_id, ttc_title, ttc_created_date)
 select ttc_id, ttc_prj_id, ttc_title, ttc_created_date
-from %TABLE_PREFIX%__migrate_ttc_prj;
+from migrate_ttc_prj;
 
--- TODO: migrate each project timetracking items to their proper ttc_id using orig_ttc_id;
+-- migrate each project timetracking items to their proper ttc_id using orig_ttc_id;
+create /*temporary*/ table migrate_issues_by_project
+select iss_id,iss_prj_id from %TABLE_PREFIX%issue where iss_prj_id!=@prj_id;
+
+create /*temporary*/ table migrate_ttr_mapping
+select ttr_ttc_id old_ttr_ttc_id,ttc_id new_ttr_ttc_id
+from %TABLE_PREFIX%time_tracking ttr, migrate_issues_by_project iss, migrate_ttc_prj ttc
+where ttr_iss_id=iss_id and orig_ttc_id=ttr_ttc_id and ttc_prj_id=iss_prj_id;
+
+update %TABLE_PREFIX%time_tracking ttr, migrate_ttr_mapping m
+set ttr_ttc_id=new_ttr_ttc_id where ttr_ttc_id=old_ttr_ttc_id;
