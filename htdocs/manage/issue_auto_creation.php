32ba8480b280f5d245505ec78c909fc92b545de0
Bryan Alsdorf <balsdorf@gmail.com>
2013-09-29 23:06:03 -0500

    Convert issue auto creation manage to new template system

--- htdocs/manage/issue_auto_creation.php
+++ htdocs/manage/issue_auto_creation.php
@@ -30,36 +30,31 @@
 require_once dirname(__FILE__) . '/../../init.php';
 
 $tpl = new Template_Helper();
-$tpl->setTemplate("manage/index.tpl.html");
+$tpl->setTemplate("manage/issue_auto_creation.tpl.html");
 
 Auth::checkAuthentication(APP_COOKIE);
 
-$tpl->assign("type", "issue_auto_creation");
 
 @$ema_id = $_POST["ema_id"] ? $_POST["ema_id"] : $_GET["ema_id"];
 
 $role_id = Auth::getCurrentRole();
-if (($role_id == User::getRoleID('administrator')) || ($role_id == User::getRoleID('manager'))) {
-    if ($role_id == User::getRoleID('administrator')) {
-        $tpl->assign("show_setup_links", true);
-    }
-
-    $prj_id = Email_Account::getProjectID($ema_id);
-
-    if (@$_POST["cat"] == "update") {
-        @Email_Account::updateIssueAutoCreation($ema_id, $_POST['issue_auto_creation'], $_POST['options']);
-    }
-    // load the form fields
-    $tpl->assign("info", Email_Account::getDetails($ema_id));
-    $tpl->assign("cats", Category::getAssocList($prj_id));
-    $tpl->assign("priorities", Priority::getList($prj_id));
-    $tpl->assign("users", Project::getUserAssocList($prj_id, 'active'));
-    $tpl->assign("options", Email_Account::getIssueAutoCreationOptions($ema_id));
-    $tpl->assign("ema_id", $ema_id);
-    $tpl->assign("prj_title", Project::getName($prj_id));
-    $tpl->assign("uses_customer_integration", CRM::hasCustomerIntegration($prj_id));
-} else {
-    $tpl->assign("show_not_allowed_msg", true);
+if ($role_id < User::getRoleID('administrator')) {
+    Misc::setMessage("Sorry, you are not allowed to access this page.", Misc::MSG_ERROR);
+    $tpl->displayTemplate();exit;
 }
 
+$prj_id = Email_Account::getProjectID($ema_id);
+
+if (@$_POST["cat"] == "update") {
+    @Email_Account::updateIssueAutoCreation($ema_id, $_POST['issue_auto_creation'], $_POST['options']);
+}
+// load the form fields
+$tpl->assign("info", Email_Account::getDetails($ema_id));
+$tpl->assign("cats", Category::getAssocList($prj_id));
+$tpl->assign("priorities", Priority::getList($prj_id));
+$tpl->assign("users", Project::getUserAssocList($prj_id, 'active'));
+$tpl->assign("options", Email_Account::getIssueAutoCreationOptions($ema_id));
+$tpl->assign("ema_id", $ema_id);
+$tpl->assign("prj_title", Project::getName($prj_id));
+$tpl->assign("uses_customer_integration", CRM::hasCustomerIntegration($prj_id));
 $tpl->displayTemplate();
