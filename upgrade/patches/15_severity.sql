b44befc67065502cdd5fb93bcc99cc378e5f812a
Bryan Alsdorf <bryan@montyprogram.com>
2013-12-04 16:07:36 -0600

    Create default severities

--- upgrade/patches/15_severity.sql
+++ upgrade/patches/15_severity.sql
@@ -8,6 +8,11 @@ CREATE TABLE `%TABLE_PREFIX%project_severity` (
   UNIQUE KEY sev_title (sev_title, sev_prj_id)
 ) ENGINE = MYISAM DEFAULT CHARSET=utf8;
 
+INSERT INTO `%TABLE_PREFIX%project_severity` VALUES (1,1,'S1 ','Total Production Outage',0);
+INSERT INTO `%TABLE_PREFIX%project_severity` VALUES (2,1,'S2','Serious Production Failure',1);
+INSERT INTO `%TABLE_PREFIX%project_severity` VALUES (3,1,'S3','Minor Failure',2);
+INSERT INTO `%TABLE_PREFIX%project_severity` VALUES (4,1,'S4','General Requests',3);
+
 ALTER TABLE `%TABLE_PREFIX%issue` ADD COLUMN iss_sev_id int(11) unsigned NOT NULL default 0 AFTER iss_pri_id;
 ALTER TABLE `%TABLE_PREFIX%custom_filter` ADD COLUMN cst_iss_sev_id int(10) unsigned NULL AFTER cst_iss_pri_id;
 
@@ -17,3 +22,4 @@ CREATE TABLE `%TABLE_PREFIX%reminder_severity` (
   rms_sev_id INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY(rms_id)
 ) ENGINE = MYISAM DEFAULT CHARSET=utf8;
+
