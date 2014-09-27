9c929b94c30d36da2e28850aad7fcaa0fa054ddb
Elan Ruusamäe <glen@delfi.ee>
2014-09-27 21:09:04 +0300

    phpcs: code style fixes

--- htdocs/setup/index.php
+++ htdocs/setup/index.php
@@ -339,34 +339,35 @@ function getErrorMessage($type, $message)
 
 function getTimezone()
 {
-	$ini = ini_get("date.timezone");
-	if ($ini) {
-		return $ini;
-	}
+    $ini = ini_get("date.timezone");
+    if ($ini) {
+        return $ini;
+    }
 
-	// if php.ini is unconfigured, this function is noisy
-	return @date_default_timezone_get();
+    // if php.ini is unconfigured, this function is noisy
+    return @date_default_timezone_get();
 }
 
 function getFirstWeekday()
 {
-	// this works on Linux
-	// http://stackoverflow.com/questions/727471/how-do-i-get-the-first-day-of-the-week-for-the-current-locale-php-l8n
-	$weekday = system('locale first_weekday');
-	if ($weekday) {
-		// Returns Monday=2, but we need 1 for Monday
-		// see http://man7.org/linux/man-pages/man5/locale.5.html
-		return --$weekday;
-	}
+    // this works on Linux
+    // http://stackoverflow.com/questions/727471/how-do-i-get-the-first-day-of-the-week-for-the-current-locale-php-l8n
+    $weekday = system('locale first_weekday');
+    if ($weekday) {
+        // Returns Monday=2, but we need 1 for Monday
+        // see http://man7.org/linux/man-pages/man5/locale.5.html
+        return --$weekday;
+    }
 
-	// This would work in PHP 5.5
-	if (class_exists('IntlCalendar')) {
-		$cal = IntlCalendar::createInstance();
-		return $cal->getFirstDayOfWeek() == IntlCalendar::DOW_MONDAY ? 1 : 0;
-	}
+    // This would work in PHP 5.5
+    if (class_exists('IntlCalendar')) {
+        $cal = IntlCalendar::createInstance();
 
-	// default to Monday as it's default for "World" in CLDR's supplemental data
-	return 1;
+        return $cal->getFirstDayOfWeek() == IntlCalendar::DOW_MONDAY ? 1 : 0;
+    }
+
+    // default to Monday as it's default for "World" in CLDR's supplemental data
+    return 1;
 }
 
 function getDatabaseList($conn)
