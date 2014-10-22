8a7c715f1844839aafe1def227e5190994dd9f0f
Elan Ruusamäe <glen@delfi.ee>
2014-10-22 11:13:06 +0300

    Abstract_Partner_Backend: document prototypes, add missing methods

--- lib/eventum/partner/class.abstract_partner_backend.php
+++ lib/eventum/partner/class.abstract_partner_backend.php
@@ -3,7 +3,7 @@
 // +----------------------------------------------------------------------+
 // | Eventum - Issue Tracking System                                      |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2011 Eventum Team              .                       |
+// | Copyright (c) 2011-2014 Eventum Team              .                  |
 // |                                                                      |
 // | This program is free software; you can redistribute it and/or modify |
 // | it under the terms of the GNU General Public License as published by |
@@ -23,42 +23,93 @@
 // | Boston, MA 02111-1307, USA.                                          |
 // +----------------------------------------------------------------------+
 // | Authors: Bryan Alsdorf <balsdorf@gmail.com>                          |
+// | Authors: Elan Ruusamäe <glen@delfi.ee>                               |
 // +----------------------------------------------------------------------+
-
 
 /**
  * Abstract parent class for partner business logic.
  */
 abstract class Abstract_Partner_Backend
 {
-    public function __construct()
-    {
-        // setup the backend
-    }
+    /**
+     * method to set up the backend
+     */
+    public function __construct() {}
 
-    public function getCode()
-    {
-        // return the code grabbed from this class name
-    }
+    /**
+     * return the code grabbed from this class name
+     * @return int
+     */
+    public function getCode() {}
 
+    /**
+     * @return string
+     */
     abstract public function getName();
 
+    /**
+     * @param int $iss_id
+     */
     public function issueAdded($iss_id) {}
 
+    /**
+     * @param int $iss_id
+     */
     public function issueRemoved($iss_id) {}
 
+    /**
+     * @param int $iss_id
+     * @param int $sup_id
+     */
     public function handleNewEmail($iss_id, $sup_id) {}
 
+    /**
+     * @param int $iss_id
+     * @param int $not_id
+     */
     public function handleNewNote($iss_id, $not_id) {}
 
+    /**
+     * @param int $iss_id
+     * @param int $usr_id
+     * @param array $old_details
+     * @param array $changes
+     */
     public function handleIssueChange($iss_id, $usr_id, $old_details, $changes) {}
 
+    /**
+     * @param int $iss_id
+     * @return string
+     */
     public function getIssueMessage($iss_id) {}
 
-    public static function canUserAccessFeature($usr_id, $feature)
-    {
-        return null;
-    }
+    /**
+     * @param int $usr_id
+     * @param string $feature
+     * @return bool
+     */
+    public static function canUserAccessFeature($usr_id, $feature) {}
 
+    /**
+     * @param int $issue_id
+     * @param int $usr_id
+     * @return bool
+     */
     public static function canEditIssue($issue_id, $usr_id) {}
+
+    /**
+     * @param int $usr_id
+     * @param string $section partners, drafts, files, time, notes, phone, history, notification_list, authorized_repliers
+     * @return bool
+     */
+    public static function canUserAccessIssueSection($usr_id, $section) {}
+
+    /**
+     * If the partner can edit the issue.
+     *
+     * @param integer   $issue_id
+     * @param integer   $usr_id
+     * @return bool
+     */
+    public static function canUpdateIssue($issue_id, $usr_id) {}
 }
