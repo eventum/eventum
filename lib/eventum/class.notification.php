5c263e62c8aa0ff01b8c029551eb7595e77bda8a
Elan Ruusamäe <glen@delfi.ee>
2014-11-12 18:47:36 +0200

    fixfsfaddr.sed

--- lib/eventum/class.notification.php
+++ lib/eventum/class.notification.php
@@ -21,8 +21,8 @@
 // | along with this program; if not, write to:                           |
 // |                                                                      |
 // | Free Software Foundation, Inc.                                       |
-// | 59 Temple Place - Suite 330                                          |
-// | Boston, MA 02111-1307, USA.                                          |
+// | 51 Franklin Street, Suite 330                                          |
+// | Boston, MA 02110-1301, USA.                                          |
 // +----------------------------------------------------------------------+
 // | Authors: João Prado Maia <jpm@mysql.com>                             |
 // | Authors: Elan Ruusamäe <glen@delfi.ee>                               |
