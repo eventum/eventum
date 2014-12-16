e3949f075418abb31396486ec4c84a27ae54e6d3
Elan Ruusamäe <glen@delfi.ee>
2014-12-16 21:48:49 +0200

    skip DbMaxAllowedPacketTest on travis

--- tests/DbMaxAllowedPacketTest.php
+++ tests/DbMaxAllowedPacketTest.php
@@ -4,15 +4,17 @@
  * Separate test to use different getInstance call
  */
 class DbMaxAllowedPacketTest extends PHPUnit_Framework_TestCase {
-    public function testPhp56() {
+    public function setUp()
+    {
+        if (getenv('TRAVIS')) {
+            $this->markTestSkipped("No DB tests in Travis");
+        }
+
         if (PHP_VERSION_ID >= 50600) {
             $this->markTestSkipped("PEAR::DB not compatible with php 5.6");
         }
     }
 
-    /**
-     * @depends testPhp56
-     */
     public function testGetMaxAllowedPacket() {
         // this should not fail if db is not reachable
         $res = DB_Helper::getMaxAllowedPacket();
