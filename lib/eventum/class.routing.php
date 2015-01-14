d1a05a2c9ea4115ebbaf5435879a114d106e5c5d
Bryan Alsdorf <bryan@mariadb.com>
2015-01-14 17:58:49 -0600

    Fix incorrect "last action type" when a user without an account sent an email

--- lib/eventum/class.routing.php
+++ lib/eventum/class.routing.php
@@ -265,7 +265,8 @@ class Routing
             if ((!empty($t['customer_id'])) && ($t['customer_id'] != 'NULL')) {
                 Issue::markAsUpdated($issue_id, 'customer action');
             } else {
-                if ((!empty($usr_id)) && (User::getRoleByUser($usr_id, $prj_id) > User::getRoleID('Customer'))) {
+                if ((!empty($usr_id)) && ($usr_id != APP_SYSTEM_USER_ID) &&
+                        (User::getRoleByUser($usr_id, $prj_id) > User::getRoleID('Customer'))) {
                     Issue::markAsUpdated($issue_id, 'staff response');
                 } else {
                     Issue::markAsUpdated($issue_id, 'user response');
