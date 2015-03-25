3e1813640a776586ccfbd1c229ab48fed230bd78
Elan Ruusamäe <glen@delfi.ee>
2015-03-25 20:06:20 +0200

    use shorthand for new Array and new Object

--- templates/js/dynamic_custom_field.tpl.js
+++ templates/js/dynamic_custom_field.tpl.js
@@ -1,23 +1,23 @@
-var dynamic_options = new Array();
+var dynamic_options = [];
 
 {foreach from=$fields item=field name=fields}
 i = dynamic_options.length;
-dynamic_options[i] = new Object();
+dynamic_options[i] = {};
 dynamic_options[i].target_field_id = {$field.fld_id};
 dynamic_options[i].fld_type = '{$field.fld_type}';
 dynamic_options[i].controlling_field_id = '{$field.controlling_field_id}';
 dynamic_options[i].controlling_field_name = '{$field.controlling_field_name}';
 dynamic_options[i].hide_when_no_options = '{$field.hide_when_no_options}';
 dynamic_options[i].lookup_method = '{$field.lookup_method}';
-dynamic_options[i].groups = new Array();
+dynamic_options[i].groups = [];
     {foreach from=$field.structured_data key=key item=group}
     j = dynamic_options[i].groups.length;
-    dynamic_options[i].groups[j] = new Object();
-    dynamic_options[i].groups[j].keys = new Array();
+    dynamic_options[i].groups[j] = {};
+    dynamic_options[i].groups[j].keys = [];
         {foreach from=$group.keys item=key}
         dynamic_options[i].groups[j].keys[dynamic_options[i].groups[j].keys.length] = '{$key}';
         {/foreach}
-    dynamic_options[i].groups[j].options = new Array();
+    dynamic_options[i].groups[j].options = [];
         {foreach from=$group.options item=option key=option_value}
         dynamic_options[i].groups[j].options[dynamic_options[i].groups[j].options.length] = new Option('{$option|escape:'javascript'}', '{$option_value|escape:'javascript'}');
         {/foreach}
@@ -26,7 +26,7 @@ dynamic_options[i].groups = new Array();
 {literal}
 function custom_field_get_details_by_controller(controller_id)
 {
-    var details = new Array();
+    var details = [];
     for (var i = 0; i < dynamic_options.length; i++) {
         if (dynamic_options[i].controlling_field_id == controller_id) {
             details[details.length] = dynamic_options[i];
@@ -83,14 +83,14 @@ function custom_field_set_new_options(controller, keep_target_value, target_fld_
 
     // find the object
     if (target_fld_id != undefined) {
-        details = new Array();
+        details = [];
         details[0] = custom_field_get_details_by_target(target_fld_id);
     } else {
         details = custom_field_get_details_by_controller(controller.attr('id'), target_fld_id);
     }
     for (var i = 0; i < details.length; i++) {
         // get the target/targets
-        var targets = new Array();
+        var targets = [];
         var target;
         targets[0] = target = $('#custom_field_' + details[i].target_field_id);
 
