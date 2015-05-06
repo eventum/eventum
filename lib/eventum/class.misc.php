c37de990abc102b0e76bdbe07ba395c3f1ab996b
Elan Ruusamäe <glen@delfi.ee>
2015-05-06 20:40:41 +0300

    move message token processor to misc class for now

--- lib/eventum/class.misc.php
+++ lib/eventum/class.misc.php
@@ -916,4 +916,37 @@ class Misc
 
         return $data->getCode() == $code;
     }
+
+    /**
+     * Processes a message according to PSR-3 rules
+     *
+     * It replaces {foo} with the value from $context['foo']
+     *
+     * @see \Monolog\Processor\PsrLogMessageProcessor()
+     * @link https://github.com/Seldaek/monolog/blob/master/src/Monolog/Processor/PsrLogMessageProcessor.php
+     * @param string $message
+     * @param  array $context
+     * @return string
+     */
+    public static function processTokens($message, array $context)
+    {
+        if (false === strpos($message, '{')) {
+            return $message;
+        }
+
+        $replacements = array();
+        foreach ($context as $key => $val) {
+            if (is_null($val) || is_scalar($val) || (is_object($val) && method_exists($val, '__toString'))) {
+                $replacements['{' . $key . '}'] = $val;
+            } elseif (is_object($val)) {
+                $replacements['{' . $key . '}'] = '[object ' . get_class($val) . ']';
+            } else {
+                $replacements['{' . $key . '}'] = '[' . gettype($val) . ']';
+            }
+        }
+
+        $message = strtr($message, $replacements);
+
+        return $message;
+    }
 }
--- tests/HistoryTest.php
+++ tests/HistoryTest.php
@@ -9,37 +9,8 @@ class HistoryTest extends PHPUnit_Framework_TestCase
             'status' => 'closed',
             'actor' => 'Random User',
         );
-        $message = $this->logProcessor($message, $context);
+        $message = Misc::processTokens($message, $context);
         $exp = "Issue updated to status 'closed' by Random User";
         $this->assertEquals($exp, $message);
-    }
-
-    /**
-     * @see \Monolog\Processor\PsrLogMessageProcessor()
-     * @link https://github.com/Seldaek/monolog/blob/master/src/Monolog/Processor/PsrLogMessageProcessor.php
-     * @param string $message
-     * @param  array $context
-     * @return string
-     */
-    private function logProcessor($message, array $context)
-    {
-        if (false === strpos($message, '{')) {
-            return $message;
-        }
-
-        $replacements = array();
-        foreach ($context as $key => $val) {
-            if (is_null($val) || is_scalar($val) || (is_object($val) && method_exists($val, '__toString'))) {
-                $replacements['{' . $key . '}'] = $val;
-            } elseif (is_object($val)) {
-                $replacements['{' . $key . '}'] = '[object ' . get_class($val) . ']';
-            } else {
-                $replacements['{' . $key . '}'] = '[' . gettype($val) . ']';
-            }
-        }
-
-        $message = strtr($message, $replacements);
-
-        return $message;
     }
 }
