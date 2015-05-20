154c6f730967778b977ac1721db35958a4163815
Bryan Alsdorf <bryan@mariadb.com>
2015-05-20 12:50:45 -0500

    Fix table name in upgrade patch

--- upgrade/patches/27_history_context.sql
+++ upgrade/patches/27_history_context.sql
@@ -4,7 +4,7 @@
 -- as we are doing full table rebuild anyway with this alter
 --
 
-alter table eventum_issue_history
+alter table {{%issue_history}}
     add his_context mediumtext not null after his_summary,
     modify `his_htt_id` tinyint(2) NOT NULL DEFAULT '0' after `his_usr_id`,
     modify `his_is_hidden` tinyint(1) NOT NULL DEFAULT '0' after his_htt_id
