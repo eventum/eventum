bd23b9a85b521f3bce63d4a31cef312f62cfeee8
Elan Ruusamäe <glen@delfi.ee>
2015-07-14 14:13:20 +0300

    simplify

--- upgrade/patches/31_fix_history.php
+++ upgrade/patches/31_fix_history.php
@@ -6,9 +6,8 @@
 $res = $db->getAll("select his_id,his_context from {{%issue_history}} where his_summary='Note routed from {user}' and his_context like '%from%:%'");
 
 foreach ($res as $idx => $row) {
-	$context = json_decode($row['his_context'],1);
-	$user = $context['from'];
-	$context['user'] = $user;
+	$context = json_decode($row['his_context'], 1);
+	$context['user'] = $context['from'];
 	unset($context['from']);
 
 	$context = json_encode($context);
