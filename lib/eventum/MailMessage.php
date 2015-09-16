7ccb3504ba08dfb417c855f2366d551f3913e8e7
Elan Ruusamäe <glen@delfi.ee>
2015-09-16 19:28:41 +0300

    add MailMessage class that uses imap resource for input and returns Zend Mail Message

---
+++ lib/eventum/MailMessage.php
@@ -0,0 +1,70 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 encoding=utf-8: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2015 Eventum Team.                                     |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 51 Franklin Street, Suite 330                                        |
+// | Boston, MA 02110-1301, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Elan Ruusamäe <glen@delfi.ee>                               |
+// +----------------------------------------------------------------------+
+
+use Zend\Mail\Storage\Message;
+
+class MailMessage extends Message
+{
+    /**
+     * Method to read email from imap extension and return Zend Mail Message object.
+     *
+     * This is bridge while migrating to Zend Mail package supporting reading from imap extension functions.
+     *
+     * @param resource $mbox
+     * @param integer $num
+     * @return MailMessage
+     */
+    public static function createFromImap($mbox, $num)
+    {
+        // check if the current message was already seen
+        list($overview) = imap_fetch_overview($mbox, $num);
+
+        $header = imap_fetchheader($mbox, $num);
+        $content = imap_body($mbox, $num);
+
+        // fill with "\Seen", "\Deleted", "\Answered", ... etc
+        $knownFlags = array(
+            'recent' => Zend\Mail\Storage::FLAG_RECENT,
+            'flagged' => Zend\Mail\Storage::FLAG_FLAGGED,
+            'answered' => Zend\Mail\Storage::FLAG_ANSWERED,
+            'deleted' => Zend\Mail\Storage::FLAG_DELETED,
+            'seen' => Zend\Mail\Storage::FLAG_SEEN,
+            'draft' => Zend\Mail\Storage::FLAG_DRAFT,
+        );
+        $flags = array();
+        foreach ($knownFlags as $flag => $value) {
+            if ($overview->$flag) {
+                $flags[] = $value;
+            }
+        }
+
+        $message = new self(array('headers' => $header, 'content' => $content, 'flags' => $flags));
+
+        return $message;
+    }
+}
