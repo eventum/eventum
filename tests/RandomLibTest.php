46a06322581851d8df757dd6f0d022ac32ead57e
Elan Ruusamäe <glen@delfi.ee>
2015-10-13 23:15:59 +0300

    password gen test

--- tests/RandomLibTest.php
+++ tests/RandomLibTest.php
@@ -8,4 +8,10 @@ class RandomLibTest extends PHPUnit_Framework_TestCase
         $this->assertNotNull($res);
         $this->assertEquals(32, Misc::countBytes($res));
     }
+
+    public function testPasswordGen() {
+        $hash = md5(Misc::generateRandom(32));
+        $password = substr($hash, 0, 12);
+        $this->assertEquals(12, Misc::countBytes($password));
+    }
 }
