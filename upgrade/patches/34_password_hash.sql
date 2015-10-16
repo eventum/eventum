0f7ef1962c3b1afe15fd616952db10c5c72e59f7
Elan Ruusamäe <glen@delfi.ee>
2015-10-16 19:48:58 +0300

    use recommended 255 as password hash length
    http://php.net/manual/en/function.password-hash.php
    
    PASSWORD_DEFAULT - Use the bcrypt algorithm (default as of PHP 5.5.0).
    
    Note that this constant is designed to change over time as new and
    stronger algorithms are added to PHP. For that reason, the length of the
    result from using this identifier can change over time. Therefore, it is
    recommended to store the result in a database column that can expand
    beyond 60 characters (255 characters would be a good choice).

--- upgrade/patches/34_password_hash.sql
+++ upgrade/patches/34_password_hash.sql
@@ -1,5 +1,5 @@
 ALTER TABLE {{%user}}
-  MODIFY COLUMN usr_password varchar(60) NOT NULL DEFAULT '';
+  MODIFY COLUMN usr_password varchar(255) NOT NULL DEFAULT '';
 
 # reset account passwords who have empty passwords
 update {{%user}} set usr_password='' where usr_password=md5('');
