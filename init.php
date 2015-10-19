41b63a19228a7f72b9c08695b6145d30afcbdfcf
Elan Ruusamäe <glen@delfi.ee>
2015-10-19 22:40:04 +0300

    simplify APP constants define with a closure

--- init.php
+++ init.php
@@ -44,9 +44,15 @@ define('APP_VERSION', '3.0.4-dev');
 
 // define base path
 define('APP_PATH', __DIR__);
-if (!defined('APP_CONFIG_PATH')) {
-    define('APP_CONFIG_PATH', APP_PATH . '/config');
-}
+
+$define = function($name, $value) {
+    if (defined($name)) {
+        return;
+    }
+    define($name, $value);
+};
+
+$define('APP_CONFIG_PATH', APP_PATH . '/config');
 
 // include local site config. may override any default
 require_once APP_CONFIG_PATH . '/config.php';
@@ -60,110 +66,47 @@ require_once APP_CONFIG_PATH . '/config.php';
  * APP_LOCAL_PATH/templates
  * APP_LOCAL_PATH/workflow
  */
-if (!defined('APP_LOCAL_PATH')) {
-    define('APP_LOCAL_PATH', APP_CONFIG_PATH);
-}
-
-if (!defined('APP_COOKIE')) {
-    define('APP_COOKIE', 'eventum');
-}
+$define('APP_LOCAL_PATH', APP_CONFIG_PATH);
+$define('APP_COOKIE', 'eventum');
 
 // define other paths
-if (!defined('APP_SETUP_FILE')) {
-    define('APP_SETUP_FILE', APP_CONFIG_PATH . '/setup.php');
-}
-
-if (!defined('APP_TPL_PATH')) {
-    define('APP_TPL_PATH', APP_PATH . '/templates');
-}
-
-if (!defined('APP_TPL_COMPILE_PATH')) {
-    define('APP_TPL_COMPILE_PATH', APP_PATH . '/templates_c');
-}
-
-if (!defined('APP_INC_PATH')) {
-    define('APP_INC_PATH', APP_PATH . '/lib/eventum');
-}
-
-if (!defined('APP_LOCKS_PATH')) {
-    define('APP_LOCKS_PATH', APP_PATH . '/locks');
-}
-
-if (!defined('APP_LOG_PATH')) {
-    define('APP_LOG_PATH', APP_PATH . '/logs');
-}
-
-if (!defined('APP_ERROR_LOG')) {
-    define('APP_ERROR_LOG', APP_LOG_PATH . '/errors.log');
-}
-
-if (!defined('APP_CLI_LOG')) {
-    define('APP_CLI_LOG', APP_LOG_PATH . '/cli.log');
-}
-
-if (!defined('APP_IRC_LOG')) {
-    define('APP_IRC_LOG', APP_LOG_PATH . '/irc_bot.log');
-}
-
-if (!defined('APP_LOGIN_LOG')) {
-    define('APP_LOGIN_LOG', APP_LOG_PATH . '/login_attempts.log');
-}
+$define('APP_SETUP_FILE', APP_CONFIG_PATH . '/setup.php');
+$define('APP_TPL_PATH', APP_PATH . '/templates');
+$define('APP_TPL_COMPILE_PATH', APP_PATH . '/templates_c');
+$define('APP_INC_PATH', APP_PATH . '/lib/eventum');
+$define('APP_LOCKS_PATH', APP_PATH . '/locks');
+$define('APP_LOG_PATH', APP_PATH . '/logs');
+$define('APP_ERROR_LOG', APP_LOG_PATH . '/errors.log');
+$define('APP_CLI_LOG', APP_LOG_PATH . '/cli.log');
+$define('APP_IRC_LOG', APP_LOG_PATH . '/irc_bot.log');
+$define('APP_LOGIN_LOG', APP_LOG_PATH . '/login_attempts.log');
 
 // define the user_id of system user
-if (!defined('APP_SYSTEM_USER_ID')) {
-    define('APP_SYSTEM_USER_ID', 1);
-}
+$define('APP_SYSTEM_USER_ID', 1);
 
 // email address of anonymous user.
 // if you want anonymous users getting access to your eventum.
-if (!defined('APP_ANON_USER')) {
-    define('APP_ANON_USER', '');
-}
+$define('APP_ANON_USER', '');
 
 // if full text searching is enabled
-if (!defined('APP_ENABLE_FULLTEXT')) {
-    define('APP_ENABLE_FULLTEXT', false);
-}
+$define('APP_ENABLE_FULLTEXT', false);
+$define('APP_FULLTEXT_SEARCH_CLASS', 'MySQL_Fulltext_Search');
 
-if (!defined('APP_FULLTEXT_SEARCH_CLASS')) {
-    define('APP_FULLTEXT_SEARCH_CLASS', 'MySQL_Fulltext_Search');
-}
+$define('APP_AUTH_BACKEND', 'Mysql_Auth_Backend');
 
-if (!defined('APP_AUTH_BACKEND')) {
-    define('APP_AUTH_BACKEND', 'Mysql_Auth_Backend');
-}
+$define('APP_AUTH_BACKEND_ALLOW_FALLBACK', false);
+$define('APP_DEFAULT_ASSIGNED_EMAILS', 1);
+$define('APP_DEFAULT_NEW_EMAILS', 0);
+$define('APP_DEFAULT_COPY_OF_OWN_ACTION', 0);
+$define('APP_RELATIVE_URL', '/');
+$define('APP_COOKIE_URL', APP_RELATIVE_URL);
+$define('APP_COOKIE_DOMAIN', null);
+$define('APP_HASH_TYPE', 'MD5');
+$define('APP_DEFAULT_LOCALE', 'en_US');
+$define('APP_CHARSET', 'UTF-8');
+$define('APP_DEFAULT_TIMEZONE', 'UTC');
+$define('APP_DEFAULT_WEEKDAY', 0);
 
-if (!defined('APP_AUTH_BACKEND_ALLOW_FALLBACK')) {
-    define('APP_AUTH_BACKEND_ALLOW_FALLBACK', false);
-}
-
-if (!defined('APP_DEFAULT_ASSIGNED_EMAILS')) {
-    define('APP_DEFAULT_ASSIGNED_EMAILS', 1);
-}
-if (!defined('APP_DEFAULT_NEW_EMAILS')) {
-    define('APP_DEFAULT_NEW_EMAILS', 0);
-}
-if (!defined('APP_DEFAULT_COPY_OF_OWN_ACTION')) {
-    define('APP_DEFAULT_COPY_OF_OWN_ACTION', 0);
-}
-if (!defined('APP_RELATIVE_URL')) {
-    define('APP_RELATIVE_URL', '/');
-}
-if (!defined('APP_COOKIE_URL')) {
-    define('APP_COOKIE_URL', APP_RELATIVE_URL);
-}
-if (!defined('APP_COOKIE_DOMAIN')) {
-    define('APP_COOKIE_DOMAIN', null);
-}
-if (!defined('APP_HASH_TYPE')) {
-    define('APP_HASH_TYPE', 'MD5');
-}
-if (!defined('APP_DEFAULT_LOCALE')) {
-    define('APP_DEFAULT_LOCALE', 'en_US');
-}
-if (!defined('APP_CHARSET')) {
-    define('APP_CHARSET', 'UTF-8');
-}
 if (!defined('APP_EMAIL_ENCODING')) {
     if (APP_CHARSET == 'UTF-8') {
         define('APP_EMAIL_ENCODING', '8bit');
@@ -171,30 +114,18 @@ if (!defined('APP_EMAIL_ENCODING')) {
         define('APP_EMAIL_ENCODING', '7bit');
     }
 }
-if (!defined('APP_DEFAULT_TIMEZONE')) {
-    define('APP_DEFAULT_TIMEZONE', 'UTC');
-}
-if (!defined('APP_DEFAULT_WEEKDAY')) {
-    define('APP_DEFAULT_WEEKDAY', 0);
-}
 
 // Number of failed attempts before Back-Off locking kicks in.
 // If set to false do not use Back-Off locking.
-if (!defined('APP_FAILED_LOGIN_BACKOFF_COUNT')) {
-	define('APP_FAILED_LOGIN_BACKOFF_COUNT', false);
-}
+$define('APP_FAILED_LOGIN_BACKOFF_COUNT', false);
 // How many minutes to lock account for during Back-Off
-if (!defined('APP_FAILED_LOGIN_BACKOFF_MINUTES')) {
-	define('APP_FAILED_LOGIN_BACKOFF_MINUTES', 15);
-}
+$define('APP_FAILED_LOGIN_BACKOFF_MINUTES', 15);
 
-define('APP_HIDE_CLOSED_STATS_COOKIE', 'eventum_hide_closed_stats');
+$define('APP_HIDE_CLOSED_STATS_COOKIE', 'eventum_hide_closed_stats');
 
 // if set, normal calls to eventum are redirected to a maintenance page while
 // requests to /manage/ still work
-if (!defined('APP_MAINTENANCE')) {
-    define('APP_MAINTENANCE', false);
-}
+$define('APP_MAINTENANCE', false);
 
 require_once APP_PATH . '/autoload.php';
 
@@ -227,23 +158,15 @@ if (APP_MAINTENANCE) {
 }
 
 // Default IRC category
-define("APP_EVENTUM_IRC_CATEGORY_DEFAULT", "default");
-
-if (!defined('APP_EVENTUM_IRC_CATEGORY_REMINDER')) {
-    define("APP_EVENTUM_IRC_CATEGORY_REMINDER", APP_EVENTUM_IRC_CATEGORY_DEFAULT);
-}
+$define("APP_EVENTUM_IRC_CATEGORY_DEFAULT", "default");
+$define("APP_EVENTUM_IRC_CATEGORY_REMINDER", APP_EVENTUM_IRC_CATEGORY_DEFAULT);
 
 // legacy constants, enable this block if you need time to migrate custom workflow, custom_field, customer, etc classes
 /*
 if (!defined('APP_DEFAULT_DB') || !defined('APP_TABLE_PREFIX')) {
     $dbconfig = DB_Helper::getConfig();
-    if (!defined('APP_DEFAULT_DB')) {
-        define('APP_DEFAULT_DB', $dbconfig['database']);
-    }
-
-    if (!defined('APP_TABLE_PREFIX')) {
-        define('APP_TABLE_PREFIX', $dbconfig['table_prefix']);
-    }
+    $define('APP_DEFAULT_DB', $dbconfig['database']);
+    $define('APP_TABLE_PREFIX', $dbconfig['table_prefix']);
     unset($dbconfig);
 }
 */
