88d05420092b6e09b09cf7a38646c471bb420b28
Elan Ruusamäe <glen@delfi.ee>
2015-10-25 23:45:37 +0200

    move commands to separate class
    isolating bot code and command actions

---
+++ lib/eventum/irc/AbstractBotCommands.php
@@ -0,0 +1,129 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 encoding=utf-8: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2015 Eventum Team.                                     |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 51 Franklin Street, Suite 330                                        |
+// | Boston, MA 02110-1301, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Elan Ruusamäe <glen@delfi.ee>                               |
+// +----------------------------------------------------------------------+
+
+/**
+ * Class containing helper methods for BotCommands class
+ */
+class AbstractBotCommands
+{
+    /** @var Eventum_Bot */
+    protected $bot;
+
+    /** @var Net_SmartIRC */
+    protected $irc;
+
+    /** @var int */
+    protected $default_category;
+
+    public function __construct(Eventum_Bot $bot, Net_SmartIRC $irc, array $config)
+    {
+        $this->bot = $bot;
+        $this->irc = $irc;
+        $this->default_category = $config['default_category'];
+    }
+
+    /**
+     * Method used to send a message to the given target.
+     *
+     * @param string $target The target for this message
+     * @param string|string[] $response The message to send
+     * @param integer $priority the priority level of the message
+     */
+    protected function sendResponse($target, $response, $priority = SMARTIRC_MEDIUM)
+    {
+        if (substr($target, 0, 1) != '#') {
+            $type = SMARTIRC_TYPE_QUERY;
+        } else {
+            $type = SMARTIRC_TYPE_CHANNEL;
+        }
+        $this->irc->message($type, $target, $response, $priority);
+    }
+
+    protected function isAuthenticated($data)
+    {
+        if (!$this->bot->isAuthenticated($data)) {
+            $this->sendResponse($data->nick, 'Error: You need to be authenticated to run this command.');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Register commands.
+     * All public final methods are registered. the method name is the prefix of the command
+     *
+     * @param Net_SmartIRC $irc
+     */
+    public function register(Net_SmartIRC $irc)
+    {
+        // register timer to handle events from database
+        $irc->registerTimehandler(3000, $this, 'notifyEvents');
+
+        // register all commands
+        $methods = $this->getMethods();
+        foreach ($methods as $methodName => $method) {
+            $commandName = $this->getCommandName($methodName);
+            $regex = "^!?{$commandName}";
+            $irc->registerActionhandler(SMARTIRC_TYPE_QUERY, $regex, $this, $methodName);
+        }
+    }
+
+    /**
+     * Get public final methods that will be used as bot commands
+     *
+     * @return ReflectionMethod[]
+     */
+    private function getMethods()
+    {
+        $methods = array();
+        $reflectionClass = new ReflectionClass($this);
+        foreach ($reflectionClass->getMethods() as $method) {
+            if (
+                $method->isPublic() // only public
+                && !$method->isStatic() // no static
+                && substr($method->getName(), 0, 2) != '__' // no magic
+                && $method->isFinal() // must be final
+            ) {
+                $methods[$method->getName()] = $method;
+            }
+        }
+
+        return $methods;
+    }
+
+    /**
+     * Transform camel case name to dash version
+     *
+     * @param  string $name
+     * @return string
+     */
+    private function getCommandName($name)
+    {
+        return strtolower(preg_replace('/([^A-Z-])([A-Z])/', '$1-$2', $name));
+    }
+}
--- lib/eventum/irc/Eventum_Bot.php
+++ lib/eventum/irc/Eventum_Bot.php
@@ -190,22 +190,17 @@ class Eventum_Bot
      */
     private function registerHandlers(Net_SmartIRC $irc)
     {
-        $irc->registerTimehandler(3000, $this, 'notifyEvents');
         $irc->registerTimehandler(1000, $this, 'signalDispatch');
 
         // methods that keep track of who is authenticated
-        $irc->registerActionhandler(SMARTIRC_TYPE_QUERY, '^!?list-auth', $this, 'listAuthenticatedUsers');
         $irc->registerActionhandler(SMARTIRC_TYPE_NICKCHANGE, '.*', $this, 'updateAuthenticatedUser');
         $irc->registerActionhandler(
             SMARTIRC_TYPE_KICK | SMARTIRC_TYPE_QUIT | SMARTIRC_TYPE_PART, '.*', $this, 'removeAuthenticatedUser'
         );
 
-        // real bot commands
-        $irc->registerActionhandler(SMARTIRC_TYPE_QUERY, '^!?help', $this, 'listAvailableCommands');
-        $irc->registerActionhandler(SMARTIRC_TYPE_QUERY, '^!?auth ', $this, 'authenticate');
-        $irc->registerActionhandler(SMARTIRC_TYPE_QUERY, '^!?clock', $this, 'clockUser');
-        $irc->registerActionhandler(SMARTIRC_TYPE_QUERY, '^!?list-clocked-in', $this, 'listClockedInUsers');
-        $irc->registerActionhandler(SMARTIRC_TYPE_QUERY, '^!?list-quarantined', $this, 'listQuarantinedIssues');
+        // register bot commands
+        $commands = new BotCommands($this, $irc, $this->config);
+        $commands->register($irc);
     }
 
     public function signalDispatch()
@@ -213,19 +208,32 @@ class Eventum_Bot
         pcntl_signal_dispatch();
     }
 
+    private function joinChannels(Net_SmartIRC $irc)
+    {
+        foreach ($this->channels as $prj_id => $options) {
+            foreach ($options as $chan => $categories) {
+                $irc->join($chan);
+            }
+        }
+    }
+
+    public function addUser($data, $email)
+    {
+        $this->auth[$data->nick] = $email;
+    }
+
+    public function getUsers()
+    {
+        return $this->auth;
+    }
+
     /**
-     * @param Net_SmartIRC $irc
      * @param Net_SmartIRC_data $data
      * @return bool
      */
-    private function isAuthenticated(Net_SmartIRC $irc, Net_SmartIRC_data $data)
+    public function isAuthenticated(Net_SmartIRC_data $data)
     {
-        if (in_array($data->nick, array_keys($this->auth))) {
-            return true;
-        }
-
-        $this->sendResponse($data->nick, 'Error: You need to be authenticated to run this command.');
-        return false;
+        return in_array($data->nick, array_keys($this->auth));
     }
 
     public function getEmailByNickname($nickname)
@@ -249,110 +257,12 @@ class Eventum_Bot
         return null;
     }
 
-    public function clockUser(Net_SmartIRC $irc, Net_SmartIRC_data $data)
-    {
-        if (!$this->isAuthenticated($irc, $data)) {
-            return;
-        }
-
-        switch (count($data->messageex)) {
-            case 1:
-                break;
-            case 2:
-                if (in_array($data->messageex[1], array('in', 'out'))) {
-                    break;
-                }
-                // fall through to an error
-            default:
-                $this->sendResponse($data->nick, 'Error: wrong parameter count for "CLOCK" command. Format is "!clock [in|out]".');
-                return;
-        }
-
-        $command = isset($data->messageex[1]) ? $data->messageex[1] : null;
-
-        // FIXME: handle if $email is empty
-        $email = $this->getEmailByNickname($data->nick);
-        $usr_id = User::getUserIDByEmail($email);
-
-        if ($command  == 'in') {
-            $res = User::clockIn($usr_id);
-        } elseif ($command  == 'out') {
-            $res = User::clockOut($usr_id);
-        } else {
-            if (User::isClockedIn($usr_id)) {
-                $msg = 'clocked in';
-            } else {
-                $msg = 'clocked out';
-            }
-            $this->sendResponse($data->nick, "You are currently $msg.");
-
-            return;
-        }
-
-        if ($res == 1) {
-            $this->sendResponse($data->nick, "Thank you, you are now clocked $command.");
-        } else {
-            $this->sendResponse($data->nick, "Error clocking $command.");
-        }
-    }
-
-    public function listClockedInUsers(Net_SmartIRC $irc, Net_SmartIRC_data $data)
-    {
-        if (!$this->isAuthenticated($irc, $data)) {
-            return;
-        }
-
-        $list = User::getClockedInList();
-        if (count($list) == 0) {
-            $this->sendResponse($data->nick, 'There are no clocked-in users as of now.');
-            return;
-        }
-
-        $this->sendResponse($data->nick, 'The following is the list of clocked-in users:');
-        foreach ($list as $name => $email) {
-            $this->sendResponse($data->nick, "$name: $email");
-        }
-    }
-
-    public function listQuarantinedIssues(Net_SmartIRC $irc, Net_SmartIRC_data $data)
-    {
-        if (!$this->isAuthenticated($irc, $data)) {
-            return;
-        }
-
-        $list = Issue::getQuarantinedIssueList();
-        $count = count($list);
-        if ($count == 0) {
-            $this->sendResponse($data->nick, 'There are no quarantined issues as of now.');
-            return;
-        }
-
-        $this->sendResponse($data->nick, "The following are the details of the {$count} quarantined issue(s):");
-        foreach ($list as $row) {
-            $url = APP_BASE_URL . 'view.php?id=' . $row['iss_id'];
-            $msg = sprintf(
-                'Issue #%d: %s, Assignment: %s, %s', $row['iss_id'], $row['iss_summary'],
-                $row['assigned_users'], $url
-            );
-            $this->sendResponse($data->nick, $msg);
-        }
-    }
-
-    public function listAvailableCommands(Net_SmartIRC $irc, Net_SmartIRC_data $data)
-    {
-        $commands = array(
-            'auth' => 'Format is "auth user@example.com password"',
-            'clock' => 'Format is "clock [in|out]"',
-            'list-clocked-in' => 'Format is "list-clocked-in"',
-            'list-quarantined' => 'Format is "list-quarantined"',
-        );
-
-        $this->sendResponse($data->nick, 'This is the list of available commands:');
-        foreach ($commands as $command => $description) {
-            $this->sendResponse($data->nick, "$command: $description");
-        }
-    }
-
+    /**
+     * Keep track of nicks for authenticated users
+     *
+     * @param Net_SmartIRC $irc
+     * @param Net_SmartIRC_data $data
+     */
     public function updateAuthenticatedUser(Net_SmartIRC $irc, Net_SmartIRC_data $data)
     {
         $old_nick = $data->nick;
@@ -363,54 +273,17 @@ class Eventum_Bot
         }
     }
 
+    /**
+     * Keep track of nicks for authenticated users
+     *
+     * @param Net_SmartIRC $irc
+     * @param Net_SmartIRC_data $data
+     */
     public function removeAuthenticatedUser(Net_SmartIRC $irc, Net_SmartIRC_data $data)
     {
         if (in_array($data->nick, array_keys($this->auth))) {
             unset($this->auth[$data->nick]);
         }
-    }
-
-    public function listAuthenticatedUsers(Net_SmartIRC $irc, Net_SmartIRC_data $data)
-    {
-        foreach ($this->auth as $nickname => $email) {
-            $this->sendResponse($data->nick, "$nickname => $email");
-        }
-    }
-
-    public function authenticate(Net_SmartIRC $irc, Net_SmartIRC_data $data)
-    {
-        if (count($data->messageex) != 3) {
-            $this->sendResponse($data->nick, 'Error: wrong parameter count for "AUTH" command. Format is "!auth user@example.com password".');
-
-            return;
-        }
-
-        $email = $data->messageex[1];
-        $password = $data->messageex[2];
-
-        // check if the email exists
-        if (!Auth::userExists($email)) {
-            $this->sendResponse($data->nick, 'Error: could not find a user account for the given email address "$email".');
-
-            return;
-        }
-
-        // check if the given password is correct
-        if (!Auth::isCorrectPassword($email, $password)) {
-            $this->sendResponse($data->nick, 'Error: The email address / password combination could not be found in the system.');
-
-            return;
-        }
-
-        // check if the user account is activated
-        if (!Auth::isActiveUser($email)) {
-            $this->sendResponse($data->nick, 'Error: Your user status is currently set as inactive. Please contact your local system administrator for further information.');
-
-            return;
-        }
-
-        $this->auth[$data->nick] = $email;
-        $this->sendResponse($data->nick, 'Thank you, you have been successfully authenticated.');
     }
 
     /**
@@ -420,7 +293,7 @@ class Eventum_Bot
      * @param   integer $prj_id The project ID
      * @return  array The list of channels
      */
-    private function getChannels($prj_id)
+    public function getChannels($prj_id)
     {
         if (isset($this->channels[$prj_id])) {
             return $this->channels[$prj_id];
@@ -435,7 +308,7 @@ class Eventum_Bot
      * @param   string $channel The name of the channel
      * @return  array The projects displayed in the channel
      */
-    private function getProjectsForChannel($channel)
+    public function getProjectsForChannel($channel)
     {
         $projects = array();
         foreach ($this->channels as $prj_id => $prj_channels) {
@@ -447,109 +320,5 @@ class Eventum_Bot
         }
 
         return $projects;
-    }
-
-    /**
-     * Method used as a callback to send notification events to the proper
-     * recipients.
-     *
-     * @param   Net_SmartIRC $irc The IRC connection handle
-     */
-    public function notifyEvents(Net_SmartIRC $irc)
-    {
-        // check the message table
-        $stmt
-            = "SELECT
-                    ino_id,
-                    ino_iss_id,
-                    ino_prj_id,
-                    ino_message,
-                    ino_target_usr_id,
-                    ino_category
-                 FROM
-                    {{%irc_notice}}
-                 LEFT JOIN
-                    {{%issue}}
-                 ON
-                    iss_id=ino_iss_id
-                 WHERE
-                    ino_status=?";
-        $res = DB_Helper::getInstance()->getAll($stmt, array('pending'));
-        foreach ($res as $row) {
-            if (empty($row['ino_category'])) {
-                $row['ino_category'] = $this->config['default_category'];
-            }
-
-            // check if this is a targeted message
-            if (!empty($row['ino_target_usr_id'])) {
-                $nick = $this->getNicknameByUser($row['ino_target_usr_id']);
-                if ($nick) {
-                    $this->sendResponse($nick, $row['ino_message']);
-                }
-                // FIXME: why mark it sent if user is not online?
-                $this->markEventSent($row['ino_id']);
-                continue;
-            }
-
-            $channels = $this->getChannels($row['ino_prj_id']);
-            if (!$channels) {
-                continue;
-            }
-            foreach ($channels as $channel => $categories) {
-                $message = $row['ino_message'];
-                if ($row['ino_iss_id'] > 0) {
-                    $message .= ' - ' . APP_BASE_URL . 'view.php?id=' . $row['ino_iss_id'];
-                } elseif (substr($row['ino_message'], 0, strlen('New Pending Email')) == 'New Pending Email') {
-                    $message .= ' - ' . APP_BASE_URL . 'emails.php';
-                }
-                if (count($this->getProjectsForChannel($channel)) > 1) {
-                    // if multiple projects display in the same channel, display project in message
-                    $message = '[' . Project::getName($row['ino_prj_id']) . '] ' . $message;
-                }
-                if (in_array($row['ino_category'], $categories)) {
-                    $this->sendResponse($channel, $message);
-                }
-            }
-            $this->markEventSent($row['ino_id']);
-        }
-    }
-
-    private function markEventSent($ino_id)
-    {
-        // mark message as sent
-        $stmt
-            = "UPDATE
-                    {{%irc_notice}}
-                 SET
-                    ino_status='sent'
-                 WHERE
-                    ino_id=?";
-        DB_Helper::getInstance()->query($stmt, array($ino_id));
-    }
-
-    /**
-     * Method used to send a message to the given target.
-     *
-     * @param string $target The target for this message
-     * @param string|string[] $response The message to send
-     * @param integer $priority the priority level of the message
-     */
-    public function sendResponse($target, $response, $priority = SMARTIRC_MEDIUM)
-    {
-        if (substr($target, 0, 1) != '#') {
-            $type = SMARTIRC_TYPE_QUERY;
-        } else {
-            $type = SMARTIRC_TYPE_CHANNEL;
-        }
-        $this->irc->message($type, $target, $response, $priority);
-    }
-
-    private function joinChannels(Net_SmartIRC $irc)
-    {
-        foreach ($this->channels as $prj_id => $options) {
-            foreach ($options as $chan => $categories) {
-                $irc->join($chan);
-            }
-        }
     }
 }
