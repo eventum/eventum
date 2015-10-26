59758319495f16626b63b58bdf936e375ca4ef04
Elan Ruusamäe <glen@delfi.ee>
2015-10-26 20:53:48 +0200

    migrate pids and locks

---
+++ upgrade/patches/39_var_paths.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * migrate pids and locks to new location
+ *
+ * https://github.com/eventum/eventum/pull/81
+ */
+
+$old_dir = APP_PATH . '/locks';
+$new_dir = APP_LOCKS_PATH;
+
+if (realpath($old_dir) == realpath($new_dir)) {
+    // user has decided to use old structure, nothing to migrate
+    return;
+}
+
+if (!Misc::isWritableDirectory($new_dir)) {
+    // shouldn't happen
+    throw new UnexpectedValueException("APP_LOCKS_PATH is not writable");
+}
+
+$files = Misc::getFileList($old_dir);
+if (!$files) {
+    // nothing to migrate
+    return;
+}
+
+echo "Migrating ", count($files), " files from $old_dir to $new_dir\n";
+foreach ($files as $file) {
+    $old_file = "$old_dir/$file";
+    $new_file = "$new_dir/$file";
+    $res = copy($old_file, $new_file);
+    if ($res == false) {
+        throw new BadMethodCallException("Could not copy $file");
+    }
+    // preserve timestamp
+    touch($new_file, filemtime($old_file));
+}
