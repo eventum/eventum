89ee53dc02561ded321e43ab4c25fb358154d3f0
Elan Ruusamäe <glen@delfi.ee>
2015-10-27 19:14:15 +0200

    skip* need to be static
    as they are invoked static as well

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,17 @@
 
 class TestCase extends PHPUnit_Framework_TestCase
 {
-    public function skipTravis($message = 'Disabled in Travis')
+    public static function skipTravis($message = 'Disabled in Travis')
     {
         if (getenv('TRAVIS')) {
-            $this->markTestSkipped($message);
+            self::markTestSkipped($message);
         }
     }
 
-    public function skipCi($message = 'Disabled in Travis/Jenkins')
+    public static function skipCi($message = 'Disabled in Travis/Jenkins')
     {
         if (getenv('TRAVIS') || getenv('JENKINS_HOME')) {
-            $this->markTestSkipped($message);
+            self::markTestSkipped($message);
         }
     }
 }
