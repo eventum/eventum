0a87e87560186e818fd304d205008e78b4c64f45
Elan Ruusamäe <glen@delfi.ee>
2015-10-31 13:31:46 +0200

    Add plain PDO adapter. experimental, but seems working

---
+++ lib/eventum/db/DbPdo.php
@@ -0,0 +1,191 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 encoding=utf-8: */
+// +----------------------------------------------------------------------+
+// | Eventum - Issue Tracking System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2015 Eventum Team.                                     |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+
+class DbPdo extends DbBasePdo implements DbInterface
+{
+    /** @var PDO */
+    private $db;
+
+    /**
+     * @param $config
+     */
+    public function __construct(array $config)
+    {
+        $dsn = $this->getDsn($config);
+
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        );
+        $this->db = new PDO($dsn, $config['username'], $config['password'], $options);
+
+        $this->tablePrefix = $config['table_prefix'];
+    }
+
+    public function getAll($query, $params = array(), $fetchmode = DbInterface::DB_FETCHMODE_ASSOC)
+    {
+        $this->convertFetchMode($fetchmode);
+
+        return $this->fetchAll($query, $params, $fetchmode);
+    }
+
+    public function fetchAssoc($query, $params = array(), $fetchmode = DbInterface::DB_FETCHMODE_DEFAULT)
+    {
+        $flags = PDO::FETCH_GROUP | PDO::FETCH_UNIQUE;
+        if ($fetchmode == DbInterface::DB_FETCHMODE_ASSOC) {
+            $flags |= PDO::FETCH_ASSOC;
+        } elseif ($fetchmode == DbInterface::DB_FETCHMODE_DEFAULT) {
+            $flags |= PDO::FETCH_NUM;
+        } else {
+            throw new UnexpectedValueException(__FUNCTION__ . ' unsupported fetchmode: ' . $fetchmode);
+        }
+
+        return $this->fetchAll($query, $params, $flags);
+    }
+
+    public function getPair($query, $params = array())
+    {
+        return $this->fetchAll($query, $params, PDO::FETCH_KEY_PAIR);
+    }
+
+    /**
+     * @deprecated use getColumn instead
+     */
+    public function getCol($query, $col = 0, $params = array())
+    {
+        if (is_array($col)) {
+            throw new LogicException('col passed as array, did you mean to use getColumn?');
+        }
+
+        if ($col !== 0) {
+            throw new UnexpectedValueException(__FUNCTION__ . ' - col != 0 not implemented');
+        }
+
+        return $this->getColumn($query, $params);
+    }
+
+    public function getColumn($query, $params = array())
+    {
+        return $this->fetchAll($query, $params, PDO::FETCH_COLUMN);
+    }
+
+    public function getOne($query, $params = array())
+    {
+        $query = $this->quoteSql($query);
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($params);
+
+        $res = $stmt->fetchColumn();
+
+        // emulate empty result
+        if ($res === false) {
+            return null;
+        }
+
+        return $res;
+    }
+
+    public function getRow($query, $params = array(), $fetchmode = DbInterface::DB_FETCHMODE_ASSOC)
+    {
+        $query = $this->quoteSql($query);
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($params);
+
+        $this->convertFetchMode($fetchmode);
+        return $stmt->fetch($fetchmode);
+    }
+
+    /**
+     * @deprecated this is broken by design, should use parameters instead
+     * @param string $str
+     * @return string
+     */
+    public function escapeSimple($str)
+    {
+        // doesn't do arrays
+        if (!is_scalar($str)) {
+            return null;
+        }
+
+        $str = $this->db->quote($str);
+
+        if ($str[0] == "'") {
+            return substr($str, 1, -1);
+        }
+
+        return $str;
+    }
+
+    public function query($query, $params = array())
+    {
+        $query = $this->quoteSql($query);
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($params);
+
+        return true;
+    }
+
+    public function quoteIdentifier($str)
+    {
+        return "`" . str_replace("`", "``", $str) . "`";
+    }
+
+    public function affectedRows()
+    {
+        throw new RuntimeException(__FUNCTION__ . ' not implemented');
+    }
+
+    /**
+     * Common method for API
+     */
+    private function fetchAll($query, $params, $fetchmode)
+    {
+        $query = $this->quoteSql($query);
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll($fetchmode);
+    }
+
+    private function quoteSql($sql)
+    {
+        return DB_Helper::quoteTableName($this, $this->tablePrefix, $sql);
+    }
+
+    private function convertFetchMode(&$fetchmode)
+    {
+        switch ($fetchmode) {
+            case DbInterface::DB_FETCHMODE_ASSOC:
+                $fetchmode = PDO::FETCH_ASSOC;
+                break;
+
+            case DbInterface::DB_FETCHMODE_DEFAULT:
+                $fetchmode = PDO::FETCH_NUM;
+                break;
+
+            default:
+                throw new UnexpectedValueException('Unsupported fetchmode');
+        }
+    }
+}
