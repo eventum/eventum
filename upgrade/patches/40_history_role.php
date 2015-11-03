a1f6f282162b350786bcd707ee0572f863d71f9b
Bryan Alsdorf <bryan@mariadb.com>
2015-11-02 20:20:41 -0600

    Use ROLE_VIEWER constant in upgrade patch for clarity

--- upgrade/patches/40_history_role.php
+++ upgrade/patches/40_history_role.php
@@ -3,7 +3,7 @@
 /**
  * Add minimum role to history table and update old history entries
  */
-$db->query('alter table {{%issue_history}} add `his_min_role` tinyint(1) NOT NULL DEFAULT 1');
+$db->query('alter table {{%issue_history}} add `his_min_role` tinyint(1) NOT NULL DEFAULT ?', array(User::ROLE_VIEWER));
 
 $res = $db->getAll("select htt_id, htt_role from {{%history_type}}");
 
