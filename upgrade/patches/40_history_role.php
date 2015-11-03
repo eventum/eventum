4d6dca99d5b8c0754b9eb8008781ad657fc9c209
Elan Ruusamäe <glen@delfi.ee>
2015-11-03 11:12:36 +0200

    add PR link

--- upgrade/patches/40_history_role.php
+++ upgrade/patches/40_history_role.php
@@ -1,8 +1,9 @@
 <?php
-
-/**
+/*
  * Add minimum role to history table and update old history entries
+ * https://github.com/eventum/eventum/pull/88
  */
+
 $db->query('alter table {{%issue_history}} add `his_min_role` tinyint(1) NOT NULL DEFAULT ?', array(User::ROLE_VIEWER));
 
 $res = $db->getAll("select htt_id, htt_role from {{%history_type}}");
