7eb5dc1822ba19f16eac6c4cb9001c1bb28d2c2e
Elan Ruusamäe <glen@delfi.ee>
2015-11-06 18:13:44 +0200

    ldap: lowercase emails

--- lib/eventum/auth/class.ldap_auth_backend.php
+++ lib/eventum/auth/class.ldap_auth_backend.php
@@ -165,7 +165,7 @@ class LDAP_Auth_Backend implements Auth_Backend_Interface
         $details = array(
             'uid' => $entry->get_value('uid'),
             'full_name' => $entry->get_value('cn'),
-            'emails' => $entry->get_value('mail', 'all'),
+            'emails' => Misc::lowercase($entry->get_value('mail', 'all')),
             'customer_id' => $entry->get_value($this->customer_id_attribute) ?: null,
             'contact_id' => $entry->get_value($this->contact_id_attribute) ?: null,
         );
