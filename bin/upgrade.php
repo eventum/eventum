c9f5f8e659eceae1634827ef6a4cd81c6f9dcef3
Elan Ruusamäe <glen@delfi.ee>
2015-11-11 00:22:54 +0200

    cosmetics and more cleanups

--- bin/upgrade.php
+++ bin/upgrade.php
@@ -10,8 +10,9 @@
 define('INSTALL_PATH', __DIR__ . '/..');
 define('CONFIG_PATH', INSTALL_PATH . '/config');
 
-// avoid setup redirecting us
-if (!file_exists(CONFIG_PATH . '/setup.php') || !filesize(CONFIG_PATH . '/setup.php') || !is_readable(CONFIG_PATH . '/setup.php')) {
+// avoid init.php redirecting us to setup if not configured yet
+$setup_path = CONFIG_PATH . '/setup.php';
+if (!file_exists($setup_path) || !filesize($setup_path) || !is_readable($setup_path)) {
     error_log("ERROR: Can't get setup.php in '" . CONFIG_PATH . "'");
     error_log('Did you forgot to copy config from old install? Is file readable?');
     exit(1);
@@ -23,9 +24,6 @@ try {
     $dbmigrate = new DbMigrate(INSTALL_PATH . '/upgrade');
     $dbmigrate->patch_database();
 } catch (Exception $e) {
-    if ($in_setup) {
-        throw $e;
-    }
     echo $e->getMessage(), "\n";
     exit(1);
 }
