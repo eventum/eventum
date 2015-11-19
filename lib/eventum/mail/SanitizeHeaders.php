84207ac6be1c697bb946c0d0c10bb404e683094c
Elan Ruusamäe <glen@delfi.ee>
2015-11-19 21:36:49 +0200

    phpcs: scan src

--- lib/eventum/mail/SanitizeHeaders.php
+++ lib/eventum/mail/SanitizeHeaders.php
@@ -24,6 +24,7 @@
 // | Boston, MA 02110-1301, USA.                                          |
 // +----------------------------------------------------------------------+
 
+
 namespace Eventum\Mail\Helper;
 
 use DomainException;
