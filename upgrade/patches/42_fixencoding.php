de1bf89a882b90622d6d5182166d9d7ef1c5c78a
Elan Ruusamäe <glen@delfi.ee>
2015-11-29 15:02:30 +0200

    add db patch to do fixEncoding in database
    later can drop Mime_Helper::fixEncoding calls from code when fetching
    data from database.
    
    currently sup_cc is still being inserted to db without decode

---
+++ upgrade/patches/42_fixencoding.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Update database fields with fixEncoding instead doing that runtime
+ */
+/** @var DbInterface $db */
+
+$logger = Logger::getInstance('db');
+
+$res = $db->getAll(
+    "SELECT sup_id,sup_subject,sup_from,sup_to,sup_cc FROM {{%support_email}} WHERE concat(sup_subject,sup_from,sup_to,sup_cc) LIKE '%=?%'"
+);
+
+$changed = 0;
+foreach ($res as $idx => $row) {
+    $params = array();
+    foreach ($row as $k => $v) {
+        $params[$k] = Mime_Helper::fixEncoding($v);
+    }
+
+    if ($row == $params) {
+        $logger->warning("sup_id={$row['sup_id']} no changes", array('sup_id' => $row['sup_id'], 'old' => $row));
+        continue;
+    }
+    $logger->info(
+        "updated sup_id={$row['sup_id']}", array('sup_id' => $row['sup_id'], 'old' => $row, 'new' => $params)
+    );
+
+    $params[] = $row['sup_id'];
+    $db->query('UPDATE {{%support_email}} SET ' . DB_Helper::buildSet($row) . ' WHERE sup_id=?', $params);
+    $changed++;
+}
+
+$count = count($res);
+$logger->info("Updated $changed out of $count entries");
