046c406502e075337eef3e01c485e28e8481892f
Elan Ruusamäe <glen@delfi.ee>
2015-12-01 19:39:40 +0200

    convert time_tracking.php to controller logic

--- htdocs/time_tracking.php
+++ htdocs/time_tracking.php
@@ -13,35 +13,5 @@
 
 require_once __DIR__ . '/../init.php';
 
-$tpl = new Template_Helper();
-$tpl->setTemplate('add_time_tracking.tpl.html');
-
-Auth::checkAuthentication('index.php?err=5', true);
-
-$issue_id = @$_POST['issue_id'] ? $_POST['issue_id'] : $_GET['iss_id'];
-
-if ((!Issue::canAccess($issue_id, Auth::getUserID())) || (Auth::getCurrentRole() <= User::ROLE_CUSTOMER)) {
-    $tpl = new Template_Helper();
-    $tpl->setTemplate('permission_denied.tpl.html');
-    $tpl->displayTemplate();
-    exit;
-}
-
-if (@$_POST['cat'] == 'add_time') {
-    $date = (array) $_POST['date'];
-    $ttc_id = (int) $_POST['category'];
-    $iss_id = (int) $_POST['issue_id'];
-    $time_spent = (int) $_POST['time_spent'];
-    $summary = (string) $_POST['summary'];
-    $res = Time_Tracking::addTimeEntry($iss_id, $ttc_id, $time_spent, $date, $summary);
-    $tpl->assign('time_add_result', $res);
-}
-
-$prj_id = Auth::getCurrentProject();
-$tpl->assign(array(
-    'issue_id'           => $issue_id,
-    'time_categories'    => Time_Tracking::getAssocCategories($prj_id),
-    'current_user_prefs' => Prefs::get(Auth::getUserID()),
-));
-
-$tpl->displayTemplate();
+$controller = new Eventum\Controller\TimeTrackingController();
+$controller->run();
