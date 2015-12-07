209bc2e42f68be8345da8c55bbbfff9d72cbb5d7
Elan Ruusamäe <glen@delfi.ee>
2015-12-08 00:14:03 +0200

    convert switch.php to controller logic

---
+++ src/Controller/SwitchController.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace Eventum\Controller;
+
+use Auth;
+use AuthCookie;
+use Misc;
+
+class SwitchController extends BaseController
+{
+    /** @var int */
+    private $prj_id;
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $request = $this->getRequest();
+
+        $this->prj_id = $request->request->get('current_project');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function canAccess()
+    {
+        Auth::checkAuthentication();
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultAction()
+    {
+        AuthCookie::setProjectCookie($this->prj_id);
+        Misc::setMessage(ev_gettext('The project has been switched'), Misc::MSG_INFO);
+
+        $url = $this->getRedirectUrl();
+        $this->redirect($url);
+    }
+
+    private function getRedirectUrl()
+    {
+        $request = $this->getRequest();
+        $url = $request->headers->get('referer');
+
+        // if url is 'view.php', use 'list.php',
+        // otherwise autoswitcher will switch back to the project where the issue was :)
+        if (!$url || stristr($url, 'view.php') !== false) {
+            $url = APP_RELATIVE_URL . 'list.php';
+        }
+
+        return $url;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function prepareTemplate()
+    {
+    }
+}
