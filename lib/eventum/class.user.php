eventum/eventum

commit 6a441f3c0b84abb50ae170021c24312e98ff863e

do not use Auth::hashPassword directly or modify usr_password field
use Auth::updatePassword to update password

lib/eventum/class.user.php
@@ -306,7 +306,6 @@ class User
                     {{%user}}
                  (
                     usr_created_date,
-                    usr_password,
                     usr_full_name,
                     usr_email,
                     usr_status
@@ -315,7 +314,6 @@ class User
             DB_Helper::getInstance()->query(
                 $stmt, array(
                     Date_Helper::getCurrentDateGMT(),
-                    Auth::hashPassword($_POST['passwd']),
                     $_POST['full_name'],
                     $_POST['email'],
                     'pending',
@@ -325,13 +323,16 @@ class User
             return -1;
         }
 
-        $new_usr_id = DB_Helper::get_last_insert_id();
+        $usr_id = DB_Helper::get_last_insert_id();
+
+        Auth::updatePassword($usr_id, $_POST['passwd']);
+
         // add the project associations!
         foreach ($projects as $prj_id) {
-            Project::associateUser($prj_id, $new_usr_id, $role);
+            Project::associateUser($prj_id, $usr_id, $role);
         }
 
-        Prefs::set($new_usr_id, Prefs::getDefaults($projects));
+        Prefs::set($usr_id, Prefs::getDefaults($projects));
 
         // send confirmation email to user
         $hash = md5($_POST['full_name'] . $_POST['email'] . Auth::privateKey());
@@ -1043,10 +1044,6 @@ class User
             $params['usr_grp_id'] = !empty($data['grp_id']) ? $data['grp_id'] : null;
         }
 
-        if (!empty($data['password'])) {
-            $params['usr_password'] = Auth::hashPassword($data['password']);
-        }
-
         if (isset($data['external_id'])) {
             $params['usr_external_id'] = $data['external_id'];
         }
@@ -1064,6 +1061,10 @@ class User
             DB_Helper::getInstance()->query($stmt, $params);
         } catch (DbException $e) {
             return -1;
+        }
+
+        if (!empty($data['password'])) {
+            Auth::updatePassword($usr_id, $data['password']);
         }
 
         if (isset($data['role'])) {
@@ -1157,7 +1158,6 @@ class User
             isset($user['customer_id']) ? $user['customer_id'] : null,
             isset($user['contact_id']) ? $user['contact_id'] : null,
             Date_Helper::getCurrentDateGMT(),
-            Auth::hashPassword($user['password']),
             $user['full_name'],
             $user['email'],
             !empty($user['grp_id']) ? $user['grp_id'] : null,
@@ -1170,22 +1170,13 @@ class User
                     usr_customer_id,
                     usr_customer_contact_id,
                     usr_created_date,
-                    usr_password,
                     usr_full_name,
                     usr_email,
                     usr_grp_id,
                     usr_external_id,
                     usr_par_code
                  ) VALUES (
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?
+                    ?, ?, ?, ?, ?, ?, ?, ?
                  )';
         try {
             DB_Helper::getInstance()->query($stmt, $params);
@@ -1193,23 +1184,26 @@ class User
             return -1;
         }
 
-        $new_usr_id = DB_Helper::get_last_insert_id();
+        $usr_id = DB_Helper::get_last_insert_id();
+
+        Auth::updatePassword($usr_id, $user['password']);
+
         // add the project associations!
         $projects = array();
         foreach ($user['role'] as $prj_id => $role) {
             if ($role < 1) {
                 continue;
             }
-            Project::associateUser($prj_id, $new_usr_id, $role);
+            Project::associateUser($prj_id, $usr_id, $role);
             $projects[] = $prj_id;
         }
 
-        Prefs::set($new_usr_id, Prefs::getDefaults($projects));
+        Prefs::set($usr_id, Prefs::getDefaults($projects));
 
         // send email to user
-        Notification::notifyNewUser($new_usr_id, $user['password']);
+        Notification::notifyNewUser($usr_id, $user['password']);
 
-        return $new_usr_id;
+        return $usr_id;
     }
 
     /**

commit 9d8a24dab3e0333f70a453ca05619325a2c9fc04

fix createVisitorAccount, broken since 3.0.4
query has 5 placeholders, but only 4 parameters
this was broken in 6a441f3

lib/eventum/class.user.php
@@ -290,7 +290,7 @@ class User
                     usr_full_name,
                     usr_email,
                     usr_status
-                 ) VALUES (?, ?, ?, ?, ?)';
+                 ) VALUES (?, ?, ?, ?)';
         try {
             DB_Helper::getInstance()->query(
                 $stmt, array(