2287646d48ffa2ab76092f47557812c4c0be39bc
Elan Ruusamäe <glen@delfi.ee>
2015-12-12 02:26:25 +0200

    disable gettext if cli ran from source tree

--- cli/eventum.php
+++ cli/eventum.php
@@ -12,6 +12,7 @@
  */
 
 define('APP_PATH', __DIR__ . '/..');
+define('APP_NO_GETTEXT', true);
 require_once APP_PATH . '/autoload.php';
 
 list($user_email, $user_password, $hostname, $port, $relative_url) = Command_Line::getEnvironmentSettings();
