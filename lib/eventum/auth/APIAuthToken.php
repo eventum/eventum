65e8e8e70c3c832697a2b13aab2c09b782a40d88
Elan Ruusamäe <glen@delfi.ee>
2016-01-07 00:40:14 +0200

    add 2016 to copyright years

--- lib/eventum/auth/APIAuthToken.php
+++ lib/eventum/auth/APIAuthToken.php
@@ -11,29 +11,6 @@
  * that were distributed with this source code.
  */
 
-// +----------------------------------------------------------------------+
-// | Eventum - Issue Tracking System                                      |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2015 Eventum Team.                                     |
-// |                                                                      |
-// | This program is free software; you can redistribute it and/or modify |
-// | it under the terms of the GNU General Public License as published by |
-// | the Free Software Foundation; either version 2 of the License, or    |
-// | (at your option) any later version.                                  |
-// |                                                                      |
-// | This program is distributed in the hope that it will be useful,      |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
-// | GNU General Public License for more details.                         |
-// |                                                                      |
-// | You should have received a copy of the GNU General Public License    |
-// | along with this program; if not, write to:                           |
-// |                                                                      |
-// | Free Software Foundation, Inc.                                       |
-// | 51 Franklin Street, Suite 330                                        |
-// | Boston, MA 02110-1301, USA.                                          |
-// +----------------------------------------------------------------------+
-
 class APIAuthToken
 {
     private static $default_alg = 'HS256';
