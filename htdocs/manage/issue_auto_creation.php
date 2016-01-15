ff249ecefa872ed365b90203a563e2f1ffd50e35
Elan Ruusamäe <glen@delfi.ee>
2016-01-15 22:55:20 +0200

    convert manage/issue_auto_creation.php to controller

--- htdocs/manage/issue_auto_creation.php
+++ htdocs/manage/issue_auto_creation.php
@@ -13,32 +13,5 @@
 
 require_once __DIR__ . '/../../init.php';
 
-$tpl = new Template_Helper();
-$tpl->setTemplate('manage/issue_auto_creation.tpl.html');
-
-Auth::checkAuthentication();
-
-@$ema_id = $_POST['ema_id'] ? $_POST['ema_id'] : $_GET['ema_id'];
-
-$role_id = Auth::getCurrentRole();
-if ($role_id < User::ROLE_REPORTER) {
-    Misc::setMessage('Sorry, you are not allowed to access this page.', Misc::MSG_ERROR);
-    $tpl->displayTemplate();
-    exit;
-}
-
-$prj_id = Email_Account::getProjectID($ema_id);
-
-if (@$_POST['cat'] == 'update') {
-    @Email_Account::updateIssueAutoCreation($ema_id, $_POST['issue_auto_creation'], $_POST['options']);
-}
-// load the form fields
-$tpl->assign('info', Email_Account::getDetails($ema_id));
-$tpl->assign('cats', Category::getAssocList($prj_id));
-$tpl->assign('priorities', Priority::getList($prj_id));
-$tpl->assign('users', Project::getUserAssocList($prj_id, 'active'));
-$tpl->assign('options', Email_Account::getIssueAutoCreationOptions($ema_id));
-$tpl->assign('ema_id', $ema_id);
-$tpl->assign('prj_title', Project::getName($prj_id));
-$tpl->assign('uses_customer_integration', CRM::hasCustomerIntegration($prj_id));
-$tpl->displayTemplate();
+$controller = new Eventum\Controller\Manage\IssueAutoCreationController();
+$controller->run();
