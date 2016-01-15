083a053b195a6a1a9738b55d633ac082c8b46bea
Elan Ruusamäe <glen@delfi.ee>
2016-01-15 21:57:29 +0200

    convert manage/email_accounts.php to controller

--- htdocs/manage/email_accounts.php
+++ htdocs/manage/email_accounts.php
@@ -13,40 +13,5 @@
 
 require_once __DIR__ . '/../../init.php';
 
-$tpl = new Template_Helper();
-$tpl->setTemplate('manage/email_accounts.tpl.html');
-
-Auth::checkAuthentication();
-
-$tpl->assign('all_projects', Project::getAll());
-
-$role_id = Auth::getCurrentRole();
-if ($role_id < User::ROLE_REPORTER) {
-    Misc::setMessage(ev_gettext('Sorry, you are not allowed to access this page.'), Misc::MSG_ERROR);
-    $tpl->displayTemplate();
-    exit;
-}
-
-if (@$_POST['cat'] == 'new') {
-    Misc::mapMessages(Email_Account::insert(), array(
-            1   =>  array(ev_gettext('Thank you, the email account was added successfully.'), Misc::MSG_INFO),
-            -1  =>  array(ev_gettext('An error occurred while trying to add the new account.'), Misc::MSG_ERROR),
-    ));
-} elseif (@$_POST['cat'] == 'update') {
-    Misc::mapMessages(Email_Account::update(), array(
-            1   =>  array(ev_gettext('Thank you, the email account was updated successfully.'), Misc::MSG_INFO),
-            -1  =>  array(ev_gettext('An error occurred while trying to update the account information.'), Misc::MSG_ERROR),
-    ));
-} elseif (@$_POST['cat'] == 'delete') {
-    Misc::mapMessages(Email_Account::remove(), array(
-            1   =>  array(ev_gettext('Thank you, the email account was deleted successfully.'), Misc::MSG_INFO),
-            -1  =>  array(ev_gettext('An error occurred while trying to delete the account information.'), Misc::MSG_ERROR),
-    ));
-}
-
-if (@$_GET['cat'] == 'edit') {
-    $tpl->assign('info', Email_Account::getDetails($_GET['id']));
-}
-$tpl->assign('list', Email_Account::getList());
-
-$tpl->displayTemplate();
+$controller = new Eventum\Controller\Manage\EmailAccountsController();
+$controller->run();
