748757b6918fd8c9599ac9bc09689dde65ed737d
Elan Ruusamäe <glen@delfi.ee>
2016-01-16 11:59:19 +0200

    convert manage/severities.php to controller

--- htdocs/manage/severities.php
+++ htdocs/manage/severities.php
@@ -13,44 +13,5 @@
 
 require_once __DIR__ . '/../../init.php';
 
-$tpl = new Template_Helper();
-$tpl->setTemplate('manage/severities.tpl.html');
-
-Auth::checkAuthentication();
-
-$role_id = Auth::getCurrentRole();
-if ($role_id < User::ROLE_MANAGER) {
-    Misc::setMessage('Sorry, you are not allowed to access this page.', Misc::MSG_ERROR);
-    $tpl->displayTemplate();
-    exit;
-}
-
-@$prj_id = $_POST['prj_id'] ? $_POST['prj_id'] : $_GET['prj_id'];
-$tpl->assign('project', Project::getDetails($prj_id));
-
-if (@$_POST['cat'] == 'new') {
-    $res = Severity::insert($prj_id, $_POST['title'], $_POST['description'], $_POST['rank']);
-    Misc::mapMessages($res, array(
-            1   =>  array('Thank you, the severity was added successfully.', Misc::MSG_INFO),
-            -1  =>  array('An error occurred while trying to add the severity.', Misc::MSG_ERROR),
-            -2  =>  array('Please enter the title for this new severity.', Misc::MSG_ERROR),
-    ));
-} elseif (@$_POST['cat'] == 'update') {
-    $res = Severity::update($_POST['id'], $_POST['title'], $_POST['description'], $_POST['rank']);
-    Misc::mapMessages($res, array(
-            1   =>  array('Thank you, the severity was added successfully.', Misc::MSG_INFO),
-            -1  =>  array('An error occurred while trying to add the severity.', Misc::MSG_ERROR),
-            -2  =>  array('Please enter the title for this new severity.', Misc::MSG_ERROR),
-    ));
-} elseif (@$_POST['cat'] == 'delete') {
-    Severity::remove($_POST['items']);
-}
-
-if (@$_GET['cat'] == 'edit') {
-    $tpl->assign('info', Severity::getDetails($_GET['id']));
-} elseif (@$_GET['cat'] == 'change_rank') {
-    Severity::changeRank($prj_id, $_GET['id'], $_GET['rank']);
-}
-$tpl->assign('list', Severity::getList($prj_id));
-
-$tpl->displayTemplate();
+$controller = new Eventum\Controller\Manage\SeveritiesController();
+$controller->run();
