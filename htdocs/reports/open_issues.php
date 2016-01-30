59bc2bc7ddeb0694ee6c14fd29d51518a676eae4
Elan Ruusamäe <glen@delfi.ee>
2016-01-30 13:11:24 +0200

    convert reports/open_issues.php to controller

--- htdocs/reports/open_issues.php
+++ htdocs/reports/open_issues.php
@@ -13,31 +13,5 @@
 
 require_once __DIR__ . '/../../init.php';
 
-$tpl = new Template_Helper();
-$tpl->setTemplate('reports/open_issues.tpl.html');
-
-Auth::checkAuthentication();
-
-if (!Access::canAccessReports(Auth::getUserID())) {
-    echo 'Invalid role';
-    exit;
-}
-
-$prj_id = Auth::getCurrentProject();
-
-if (!isset($_GET['cutoff_days'])) {
-    $cutoff_days = 7;
-} else {
-    $cutoff_days = $_GET['cutoff_days'];
-}
-
-if (empty($_GET['group_by_reporter'])) {
-    $group_by_reporter = false;
-} else {
-    $group_by_reporter = true;
-}
-$tpl->assign('cutoff_days', $cutoff_days);
-$res = Report::getOpenIssuesByUser($prj_id, $cutoff_days, $group_by_reporter);
-$tpl->assign('users', $res);
-
-$tpl->displayTemplate();
+$controller = new Eventum\Controller\Report\OpenIssuesController();
+$controller->run();
