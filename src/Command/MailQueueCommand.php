5dfb5aef59eeeb1678ea91240f82050316bf4b9a
Elan Ruusamäe <glen@delfi.ee>
2016-01-31 19:01:18 +0200

    bin/download mails: use common lock code

--- src/Command/MailQueueCommand.php
+++ src/Command/MailQueueCommand.php
@@ -13,33 +13,20 @@
 
 namespace Eventum\Command;
 
-use Lock;
 use Mail_Queue;
 
 class MailQueueCommand extends Command
 {
+    protected function configure()
+    {
+        $this->lock_name = 'process_mail_queue';
+    }
+
     /**
      * @inheritdoc
      */
     protected function execute()
     {
-        $config = $this->getParams();
-
-        // if requested, clear the lock
-        if ($config['fix-lock']) {
-            if (Lock::release('process_mail_queue')) {
-                echo "The lock file was removed successfully.\n";
-            }
-            exit(0);
-        }
-
-        if (!Lock::acquire('process_mail_queue')) {
-            $pid = Lock::getProcessID('process_mail_queue');
-            fwrite(STDERR, "ERROR: There is already a process (pid=$pid) of this script running.");
-            fwrite(STDERR, "If this is not accurate, you may fix it by running this script with '--fix-lock' as the only parameter.\n");
-            exit(1);
-        }
-
         // handle only pending emails
         $limit = 50;
         Mail_Queue::send('pending', $limit);
@@ -47,40 +34,5 @@ class MailQueueCommand extends Command
         // handle emails that we tried to send before, but an error happened...
         $limit = 50;
         Mail_Queue::send('error', $limit);
-
-        Lock::release('process_mail_queue');
-    }
-
-    /**
-     * Get parameters needed for this script.
-     *
-     * for CLI mode these are take from command line arguments
-     * for Web mode those are taken as named _GET parameters.
-     *
-     * @return  array   $config
-     */
-    private function getParams()
-    {
-        // defaults
-        $config = array(
-            'fix-lock' => false,
-        );
-
-        if ($this->SAPI_CLI) {
-            global $argc, $argv;
-            // --fix-lock may be only the last argument
-            if ($argv[$argc - 1] == '--fix-lock') {
-                // no other args are allowed
-                $config['fix-lock'] = true;
-            }
-        } else {
-            foreach (array_keys($config) as $key) {
-                if (isset($_GET[$key])) {
-                    $config[$key] = $_GET[$key];
-                }
-            }
-        }
-
-        return $config;
     }
 }
