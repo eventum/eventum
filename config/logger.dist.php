2e9d6e6294353bfea855c6952b358cbfb976e24d
Elan Ruusamäe <glen@delfi.ee>
2016-02-17 22:56:59 +0200

    use local vars for readability and reusability

--- config/logger.dist.php
+++ config/logger.dist.php
@@ -6,106 +6,111 @@
 #
 # Logger configuration for Eventum
 
+$formatters = array(
+    'default' => array(
+        'class' => 'Monolog\\Formatter\\LineFormatter',
+        'format' => "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n",
+    ),
+    'error_handler' => array(
+        'class' => 'Monolog\\Formatter\\LineFormatter',
+        'format' => '%channel%.%level_name%: %message% %context% %extra%',
+    ),
+);
+
+$handlers = array(
+    'app_log' => array(
+        'class' => 'Monolog\\Handler\\StreamHandler',
+        'level' => 'INFO',
+        'stream' => APP_LOG_PATH . '/eventum.log',
+        'filePermission' => 0640,
+    ),
+    'auth_log' => array(
+        'class' => 'Monolog\\Handler\\StreamHandler',
+        'level' => 'INFO',
+        'stream' => APP_LOG_PATH . '/auth.log',
+        'filePermission' => 0640,
+    ),
+    'cli_log' => array(
+        'class' => 'Monolog\\Handler\\StreamHandler',
+        'level' => 'INFO',
+        'stream' => APP_LOG_PATH . '/cli.log',
+        'filePermission' => 0640,
+    ),
+    'error_handler' => array(
+        'class' => 'Monolog\\Handler\\ErrorLogHandler',
+        'formatter' => 'error_handler',
+    ),
+);
+
+$processors = array(
+    'web_processor' => array(
+        'class' => 'Monolog\\Processor\\WebProcessor',
+    ),
+    'memory_processor' => array(
+        'class' => 'Monolog\\Processor\\MemoryUsageProcessor',
+    ),
+    'memory_peak_processor' => array(
+        'class' => 'Monolog\\Processor\\MemoryPeakUsageProcessor',
+    ),
+    'psr_log_processor' => array(
+        'class' => 'Monolog\\Processor\\PsrLogMessageProcessor',
+    ),
+    'introspection_processor' => array(
+        'class' => 'Monolog\\Processor\\IntrospectionProcessor',
+    ),
+    'eventum_app_info_processor' => array(
+        'class' => 'Eventum\\Monolog\\AppInfoProcessor',
+    ),
+);
+
+$loggers = array(
+    'app' => array(
+        'handlers' => array(
+            'app_log',
+            'error_handler',
+        ),
+        'processors' => array(
+            'web_processor',
+            'psr_log_processor',
+            'introspection_processor',
+            'memory_processor',
+            'memory_peak_processor',
+            'eventum_app_info_processor',
+        ),
+    ),
+    'auth' => array(
+        'handlers' => array(
+            'auth_log',
+            'error_handler',
+        ),
+        'processors' => array(
+            'web_processor',
+            'psr_log_processor',
+            'introspection_processor',
+            'memory_processor',
+            'memory_peak_processor',
+            'eventum_app_info_processor',
+        ),
+    ),
+    'cli' => array(
+        'handlers' => array(
+            'cli_log',
+            'error_handler',
+        ),
+        'processors' => array(
+            'web_processor',
+            'psr_log_processor',
+            'introspection_processor',
+            'memory_processor',
+            'memory_peak_processor',
+            'eventum_app_info_processor',
+        ),
+    ),
+);
+
 return array(
-    'formatters' => array(
-        'default' => array(
-            'class' => 'Monolog\\Formatter\\LineFormatter',
-            'format' => "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n",
-        ),
-        'error_handler' => array(
-            'class' => 'Monolog\\Formatter\\LineFormatter',
-            'format' => '%channel%.%level_name%: %message% %context% %extra%',
-        ),
-    ),
-    'handlers' => array(
-        'app_log' => array(
-            'class' => 'Monolog\\Handler\\StreamHandler',
-            'level' => 'INFO',
-            'formatter' => 'default',
-            'stream' => APP_LOG_PATH . '/eventum.log',
-            'filePermission' => 0640,
-        ),
-        'auth_log' => array(
-            'class' => 'Monolog\\Handler\\StreamHandler',
-            'level' => 'INFO',
-            'formatter' => 'default',
-            'stream' => APP_LOG_PATH . '/auth.log',
-            'filePermission' => 0640,
-        ),
-        'cli_log' => array(
-            'class' => 'Monolog\\Handler\\StreamHandler',
-            'level' => 'INFO',
-            'formatter' => 'default',
-            'stream' => APP_LOG_PATH . '/cli.log',
-            'filePermission' => 0640,
-        ),
-        'error_handler' => array(
-            'class' => 'Monolog\\Handler\\ErrorLogHandler',
-            'formatter' => 'error_handler',
-        ),
-    ),
-    'processors' => array(
-        'web_processor' => array(
-            'class' => 'Monolog\\Processor\\WebProcessor',
-        ),
-        'memory_processor' => array(
-            'class' => 'Monolog\\Processor\\MemoryUsageProcessor',
-        ),
-        'memory_peak_processor' => array(
-            'class' => 'Monolog\\Processor\\MemoryPeakUsageProcessor',
-        ),
-        'psr_log_processor' => array(
-            'class' => 'Monolog\\Processor\\PsrLogMessageProcessor',
-        ),
-        'introspection_processor' => array(
-            'class' => 'Monolog\\Processor\\IntrospectionProcessor',
-        ),
-        'eventum_app_info_processor' => array(
-            'class' => 'Eventum\\Monolog\\AppInfoProcessor',
-        ),
-    ),
-    'loggers' => array(
-        'app' => array(
-            'handlers' => array(
-                'app_log',
-                'error_handler',
-            ),
-            'processors' => array(
-                'web_processor',
-                'psr_log_processor',
-                'introspection_processor',
-                'memory_processor',
-                'memory_peak_processor',
-                'eventum_app_info_processor',
-            ),
-        ),
-        'auth' => array(
-            'handlers' => array(
-                'auth_log',
-                'error_handler',
-            ),
-            'processors' => array(
-                'web_processor',
-                'psr_log_processor',
-                'introspection_processor',
-                'memory_processor',
-                'memory_peak_processor',
-                'eventum_app_info_processor',
-            ),
-        ),
-        'cli' => array(
-            'handlers' => array(
-                'cli_log',
-                'error_handler',
-            ),
-            'processors' => array(
-                'web_processor',
-                'psr_log_processor',
-                'introspection_processor',
-                'memory_processor',
-                'memory_peak_processor',
-                'eventum_app_info_processor',
-            ),
-        ),
-    ),
+    'formatters' => $formatters,
+    'handlers' => $handlers,
+    'processors' => $processors,
+    'loggers' => $loggers,
 );
