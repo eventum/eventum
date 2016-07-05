7341ffa61807e1ed6b7e845000669d45a5355ec2
Elan Ruusamäe <glen@delfi.ee>
2016-07-05 12:32:03 +0300

    use Cest testing for Login actions

--- tests/acceptance/WelcomeCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** @var \Codeception\Scenario $scenario */
-$I = new AcceptanceTester($scenario);
-$I->wantTo('Ensure that frontpage has Login Form');
-$I->amOnPage('/');
-$I->see('Login:');
-$I->see('Password:');
