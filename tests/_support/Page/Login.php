27a9f5fbecb9be42949bb224baf61ddfb368332d
Elan Ruusamäe <glen@delfi.ee>
2016-07-05 14:56:19 +0300

    add Login Page
    $ codecept generate:pageobject Login
    
    http://codeception.com/docs/06-ReusingTestCode#PageObjects

---
+++ tests/_support/Page/Login.php
@@ -0,0 +1,24 @@
+<?php
+namespace Page;
+
+class Login
+{
+    // include url of current page
+    public static $URL = '';
+
+    /**
+     * Declare UI map for this page here. CSS or XPath allowed.
+     * public static $usernameField = '#username';
+     * public static $formSubmitButton = "#mainForm input[type=submit]";
+     */
+
+    /**
+     * Basic route example for your current URL
+     * You can append any additional parameter to URL
+     * and use it in tests like: Page\Edit::route('/123-post');
+     */
+    public static function route($param)
+    {
+        return static::$URL . $param;
+    }
+}
