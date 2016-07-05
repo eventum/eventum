3d250d14b6c27aea6b13d9abbdfee05db30aa52c
Elan Ruusamäe <glen@delfi.ee>
2016-07-05 15:00:26 +0300

    Implement Login Page for LoginCest

--- tests/acceptance/LoginCest.php
+++ tests/acceptance/LoginCest.php
@@ -6,23 +6,23 @@ class LoginCest
     {
     }
 
-    public function loginInvalidUser(AcceptanceTester $I)
+    public function loginInvalidUser(AcceptanceTester $I, \Page\Login $loginPage)
     {
         $I->am('User'); // actor's role
         $I->wantTo('Login Failure'); // feature to test
         $I->amGoingTo('Login to website'); // feature to test
 
-        $I->login('admin@example.com', 'qwerty');
+        $loginPage->login('admin@example.com', 'qwerty');
         $I->see('Error: The login / password combination could not be found in the system.');
     }
 
-    public function loginAdminUser(AcceptanceTester $I)
+    public function loginAdminUser(AcceptanceTester $I, \Page\Login $loginPage)
     {
         $I->am('Admin'); // actor's role
         $I->wantTo('Login Success'); // feature to test
         $I->amGoingTo('Login to website'); // feature to test
 
-        $I->login('admin@example.com', 'admin');
+        $loginPage->login('admin@example.com', 'admin');
         // after login should see "Create Issue"
         $I->see('Create Issue');
     }
