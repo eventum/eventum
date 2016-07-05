ad55c25661882555506781b7f9e8675704cfa596
Elan Ruusamäe <glen@delfi.ee>
2016-07-05 17:25:21 +0300

    add acceptance testing for eventum setup page

---
+++ tests/_support/Page/Setup.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+namespace Page;
+
+/**
+ * Page for Setup (Installation) actions
+ *
+ * @package Page
+ */
+class Setup
+{
+    /**
+     * @var \AcceptanceTester
+     */
+    protected $tester;
+
+    public function __construct(\AcceptanceTester $I)
+    {
+        $this->tester = $I;
+
+        $I->amOnPage('/setup/');
+    }
+
+    /**
+     * Should not see login form
+     * If we see login form, the application is already registered
+     */
+    public function dontSeeLogin()
+    {
+        $I = $this->tester;
+        $I->dontSee('Eventum - Login', '//h1');
+    }
+
+    public function shouldSeeInstallForm()
+    {
+        $I = $this->tester;
+        $I->see('Eventum Installation', '//h1');
+    }
+
+    /**
+     * Submit installation form
+     *
+     * @param array $params
+     * @return $this
+     */
+    public function setup($params)
+    {
+        $I = $this->tester;
+
+        $I->submitForm('#install_form', $params);
+        $I->see('Success!');
+
+        $I->see("Email Address: {$params['admin_user']} (literally)");
+        $I->see("Password: {$params['admin_password']}");
+
+        return $this;
+    }
+}
---
+++ tests/acceptance/SetupCest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+class SetupCest
+{
+    public function setup(AcceptanceTester $I, \Page\Setup $setupPage)
+    {
+        $I->am('Administrator');
+        $I->wantTo('Register Eventum');
+
+        $setupPage->dontSeeLogin();
+        $setupPage->shouldSeeInstallForm();
+        
+        $params = [
+            'cat' => 'install',
+            'hostname' => 'localhost',
+            'relative_url' => '/~glen/eventum-setup/',
+            'db_hostname' => 'localhost',
+            'db_name' => 'e',
+            'create_db' => 'yes',
+            'drop_tables' => 'yes',
+            'db_username' => 'mysql',
+            'db_password' => '',
+            'eventum_user' => '',
+            'eventum_password' => '',
+            'default_timezone' => 'Europe/Tallinn',
+            'default_weekday' => '1',
+            'setup' => [
+                'smtp' => [
+                    'from' => 'root@localhost',
+                    'host' => 'localhost',
+                    'port' => '25',
+                    'auth' => '0',
+                ],
+            ],
+            // Cest only, not supported by setup yet
+            'admin_user' => 'admin@example.com',
+            'admin_password' => 'admin',
+        ];
+        $setupPage->setup($params);
+    }
+}
