d570f34ee64999633c4c68d395808192cef55ffe
Bryan Alsdorf <bryan@mariadb.com>
2016-11-30 10:44:12 -0600

    Add getMovedIssueMapping to map data fields from old project to new project

--- lib/eventum/workflow/class.abstract_workflow_backend.php
+++ lib/eventum/workflow/class.abstract_workflow_backend.php
@@ -687,11 +687,21 @@ class Abstract_Workflow_Backend
      */
     public static function handleIssueMoved($prj_id, $issue_id, $new_prj_id)
     {
-        if (!self::hasWorkflowIntegration($prj_id)) {
-            return null;
-        }
-        $backend = self::_getBackend($prj_id);
+    }
 
-        $backend->handleIssueMoved($prj_id, $issue_id, $new_prj_id);
+
+    /**
+     * Returns fields to be updated when an issue is moved from one project to another.
+     *
+     * @param $prj_id
+     * @param $issue_id
+     * @param $mapping
+     * @param $new_prj_id
+     * @return array An array containing field names / values
+     * @since 3.1.6
+     */
+    public static function getMovedIssueMapping($prj_id, $issue_id, $mapping, $new_prj_id)
+    {
+        return $mapping;
     }
 }
