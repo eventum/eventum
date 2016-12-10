c6962f7637926d49b9f0292db45a8c5c38ccb4a3
Elan Ruusamäe <glen@delfi.ee>
2016-12-10 22:41:11 +0200

    patches/60_history_context_convert.php: remove unused query

--- upgrade/patches/60_history_context_convert.php
+++ upgrade/patches/60_history_context_convert.php
@@ -164,8 +164,6 @@ if (!$total) {
 /** @var Closure $log */
 $log("Total $total rows, this may take time. Please be patient.");
 
-$res = $db->query("select his_id,his_summary from {{%issue_history}} where his_context=''");
-
 foreach ($his_ids as $his_id) {
     $his_summary = $db->getOne('SELECT his_summary FROM {{%issue_history}} WHERE his_id=?', [$his_id]);
 
