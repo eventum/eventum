a7c906d185ebce4bf459767413bfaa1df1714a99
Elan Ruusamäe <glen@delfi.ee>
2017-01-12 19:32:06 +0200

    add  configuration proxy for sphix
    http://docs.phinx.org/en/latest/commands.html#configuration-file-parameter

---
+++ res/config/phinx.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+/**
+ * Configuration proxy for sphix
+ *
+ * @link http://docs.phinx.org/en/latest/commands.html#configuration-file-parameter
+ */
+
+$config = DB_Helper::getConfig();
+
+// TODO: use "connection" => $pdo_instance once PEAR DB support is dropped
+// http://docs.phinx.org/en/latest/commands.html#configuration-file-parameter
+
+// TODO: support unix sockets
+// http://docs.phinx.org/en/latest/configuration.html#socket-connections
+
+return [
+    'paths' => [
+        'migrations' => 'db/migrations'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_database' => $config['database'],
+        $config['database'] => [
+            'adapter' => 'mysql',
+            'host' => $config['hostname'],
+            'name' => $config['database'],
+            'user' => $config['username'],
+            'pass' => $config['password'],
+            'port' => $config['port']
+        ]
+    ]
+];
