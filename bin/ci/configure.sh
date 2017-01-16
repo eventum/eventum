10400d873cb16b282b76171d0b0473c7230dbebc
Elan Ruusamäe <glen@delfi.ee>
2017-01-16 10:18:28 +0200

    ci: move secure-http setting to script

--- bin/ci/configure.sh
+++ bin/ci/configure.sh
@@ -13,3 +13,6 @@ phpenv config-rm xdebug.ini ||
 # PHP 7.2 does not have mcrypt
 # Installation request for defuse/php-encryption ~1.2.1 -> satisfiable by defuse/php-encryption[v1.2.1].
 composer config platform.ext-mcrypt '0'
+
+# disable secure-http because sourceforge redirects to http:// urls
+composer config secure-http false
