b5db1c862be798d8126c9e545fadfa0be79263a6
Elan Ruusamäe <glen@pld-linux.org>
2017-05-09 21:11:48 +0300

    add tests hhvm todo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,6 +18,7 @@ use Eventum\Monolog\Logger;
 define('APP_PATH', realpath(__DIR__ . '/..'));
 define('APP_CONFIG_PATH', __DIR__);
 define('APP_SETUP_FILE', APP_CONFIG_PATH . '/_setup.php');
+// FIXME: HHVM: Warning: Constants may only evaluate to scalar values
 define('APP_ERROR_LOG', STDERR);
 define('APP_INC_PATH', APP_PATH . '/lib/eventum');
 define('APP_SYSTEM_USER_ID', 1);
