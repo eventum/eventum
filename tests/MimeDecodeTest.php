158e5e50029c16d964c23cc8b44a4359db1ef663
Elan Ruusamäe <glen@pld-linux.org>
2017-05-11 22:19:06 +0300

    add tests for Mime_Helper::decode() use cases

---
+++ tests/MimeDecodeTest.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace Eventum\Test;
+
+use Eventum\Mail\MailMessage;
+use Mime_Helper;
+
+/**
+ * Tests Mime_Decode so it could be dropped
+ *
+ * @see https://github.com/eventum/eventum/pull/256#issuecomment-300879398
+ * @see Mime_Helper::decode()
+ */
+class MimeDecodeTest extends TestCase
+{
+    public function testEmptyMessage()
+    {
+        // test empty message
+        $message = '';
+        $res = Mime_Helper::decode($message, false, true);
+
+        $this->assertMimeHelperResult($res);
+    }
+
+    /**
+     * Test usecase when Mime_Helper::decode is used only for headers array
+     */
+    public function testHeaders()
+    {
+        $message = $this->readfile(__DIR__ . '/data/LP901653.txt');
+        $res = Mime_Helper::decode($message, false, true);
+        $this->assertMimeHelperResult($res);
+
+        $ph = $res->headers;
+
+        $mail = MailMessage::createFromString($message);
+        $zh = $this->pearizeHeaders($mail->getHeadersArray());
+        // these headers were manually verified
+        unset($ph['from'], $zh['from']);
+        unset($ph['to'], $zh['to']);
+        unset($ph['content-type'], $zh['content-type']);
+
+        $this->assertEquals($zh, $ph);
+    }
+
+    /**
+     * Hack out inconsistencies:
+     *
+     * - pear/mime_decode decodes empty headers as false, zf as ''
+     * - pear/mime_decode lowercases headers array keys
+     * - zf preserves line continuations
+     * - MailMessage helper creates empty Cc: header
+     * - zf sanitizes recipient headers
+     */
+    private function pearizeHeaders($h)
+    {
+        $headers = [];
+        foreach ($h as $k => $v) {
+            // strip spaces, irrelevant for test
+            if (is_string($v)) {
+                $v = preg_replace('/\s+/', ' ', $v);
+            }
+            if ($v === '') {
+                $v = false;
+            }
+            $headers[strtolower($k)] = $v;
+        }
+
+        if ($headers['cc'] == false) {
+            unset($headers['cc']);
+        }
+
+        return $headers;
+    }
+
+    private function assertMimeHelperResult($res)
+    {
+        $this->assertInstanceOf('stdClass', $res);
+        $this->assertInternalType('array', $res->headers);
+        $this->assertObjectHasAttribute('headers', $res);
+        $this->assertObjectHasAttribute('ctype_primary', $res);
+        $this->assertObjectHasAttribute('ctype_secondary', $res);
+    }
+}
