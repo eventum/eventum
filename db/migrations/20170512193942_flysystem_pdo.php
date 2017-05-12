5f8dadc46fd9d59855c090f968c21d6c4aa0cf69
Elan Ruusamäe <glen@pld-linux.org>
2017-05-12 23:09:27 +0300

    fix autogenerated schema manually
    match what phlib/flysystem-pdo intended

--- db/migrations/20170512193942_flysystem_pdo.php
+++ db/migrations/20170512193942_flysystem_pdo.php
@@ -22,24 +22,24 @@ class FlysystemPdo extends AbstractMigration
 {
     public function change()
     {
-        $table = $this->table('flysystem_path');
+        $table = $this->table('flysystem_path', ['id' => false, 'primary_key' => 'path_id']);
         $table
             ->addColumn('path_id', 'integer', ['limit' => self::INT_MEDIUM, 'signed' => false])
             ->addColumn('type', 'enum', ['values' => ['dir', 'file']])
             ->addColumn('path', 'string', ['limit' => self::TEXT_TINY])
-            ->addColumn('mimetype', 'string', ['null' => true, 'limit' => self::TEXT_TINY])
+            ->addColumn('mimetype', 'string', ['null' => true, 'limit' => self::TEXT_TINY, 'encoding' => 'ascii'])
             ->addColumn('visibility', 'string', ['null' => true, 'default' => '', 'limit' => 25])
             ->addColumn('size', 'integer', ['null' => true, 'signed' => false])
             ->addColumn('is_compressed', 'integer', ['default' => '1', 'limit' => self::INT_TINY])
-            ->addColumn('update_ts', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP'])
-            ->create();
+            ->addColumn('update_ts', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP']);
+        $this->getPrimaryKey($table)->setIdentity(true);
+        $table->create();
 
-        $table = $this->table('flysystem_chunk');
+        $table = $this->table('flysystem_chunk', ['id' => false, 'primary_key' => ['path_id', 'chunk_no']]);
         $table
             ->addColumn('path_id', 'integer', ['limit' => self::INT_MEDIUM, 'signed' => false])
             ->addColumn('chunk_no', 'integer', ['limit' => self::INT_SMALL, 'signed' => false])
             ->addColumn('content', self::PHINX_TYPE_BLOB, ['length' => self::BLOB_MEDIUM])
-
             ->create();
     }
 }
