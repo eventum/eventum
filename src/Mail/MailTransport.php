cd521accab92c956da5d388ccc6be2831262ec8a
Elan Ruusamäe <glen@pld-linux.org>
2017-05-14 01:55:58 +0300

    mailtransport send takes only mailmessage
    cleanup, as other uses not present

--- src/Mail/MailTransport.php
+++ src/Mail/MailTransport.php
@@ -31,22 +31,12 @@ class MailTransport
      *              each RFC822 valid. This may contain recipients not
      *              specified in the headers, for Bcc:, resending
      *              messages, etc.
-     *
-     * @param MailMessage|array $headers The array of headers to send with the mail, in an
-     *              associative array, where the array key is the
-     *              header name (e.g., 'Subject'), and the array value
-     *              is the header value (e.g., 'test'). The header
-     *              produced from those values would be 'Subject:
-     *              test'.
-     *
-     * @param string $body the full text of the message body, including any
-     *               MIME parts, etc
-     *
+     * @param MailMessage $mail
      * @return mixed Returns true on success, or a exception class
      *               containing a descriptive error message on
      *               failure
      */
-    public function send($recipient, $headers, $body)
+    public function send($recipient, MailMessage $mail)
     {
         $transport = $this->getTransport();
 
@@ -54,18 +44,8 @@ class MailTransport
         $envelope->setTo($recipient);
         $transport->setEnvelope($envelope);
 
-        if ($headers instanceof MailMessage) {
-            $message = $headers;
-        } else {
-            if (!$body) {
-                throw new InvalidArgumentException('body is empty');
-            }
-            $message = MailMessage::createFromHeaderBody($headers, $body);
-        }
-
         try {
-
-            $transport->send($message->toMessage());
+            $transport->send($mail->toMessage());
             $res = true;
         } catch (\Exception $e) {
             $traceFile = $this->getTraceFile();
