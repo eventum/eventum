61d95b4ccd22fa5d17603b00cdec061eb4cad0be
Elan Ruusamäe <glen@delfi.ee>
2017-05-14 13:50:29 +0300

    improve snapshot release message in github

--- bin/ci/snapshot.sh
+++ bin/ci/snapshot.sh
@@ -3,16 +3,21 @@
 # travis will build release tarball and upload it to github release page
 # https://github.com/eventum/eventum/releases/tag/snapshot
 
-set -xe
+set -e
 
 git tag -d snapshot || :
 
+version=$(git describe --tags HEAD)
 branch=$(git rev-parse --abbrev-ref HEAD)
 commit=$(git rev-parse --short HEAD)
 message="snapshot from $branch branch
 
-created from $commit on $branch branch.
-uploaded by travis. use at your own risk"
+Created from $commit on $branch branch.
+Uploaded by Travis. Use at your own risk.
+
+If the snapshot tarball (eventum-${version#v}.tar.gz) is not appearing here,
+check Travis CI for errors: https://travis-ci.org/eventum/eventum
+"
 
 git tag -am "$message" snapshot HEAD
 git push -f git@github.com:eventum/eventum.git snapshot
