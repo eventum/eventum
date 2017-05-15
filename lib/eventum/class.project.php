eventum/eventum

commit 44993fff72489b7c2f6e336fc22d0a44d28b4c7a

Fix Eventum bug, for correct setup project roles

lib/eventum/class.project.php
@@ -417,7 +417,7 @@ class Project
         foreach ($_POST['users'] as $user) {
             if ($user == $_POST['lead_usr_id']) {
                 self::associateUser($_POST['id'], $user, User::ROLE_MANAGER);
-            } elseif (User::getRoleByUser($user, $_POST['id']) == '') {
+            } else {
                 // users who are now being associated with this project should be set to 'Standard User'
                 self::associateUser($_POST['id'], $user, User::ROLE_USER);
             }
@@ -476,9 +476,22 @@ class Project
             }
 
             return true;
-        }
 
-        return true;
+        } else {
+
+            $stmt = 'UPDATE {{%project_user}}
+                        SET pru_role = ?
+                    WHERE
+                        pru_prj_id = ? AND
+                        pru_usr_id = ?';
+            try {
+                DB_Helper::getInstance()->query($stmt, array($role, $prj_id, $usr_id));
+            } catch (DbException $e) {
+                return false;
+            }
+
+            return true;
+        }
     }
 
     /**

commit 7ae6d2563311fd321a4926e1f8aa9e68558f4444

Fix bug with user roles being reset when updating projects.
This bug was introduced in 44993ff

lib/eventum/class.project.php
@@ -452,16 +452,6 @@ class Project
 
             return true;
         }
-        $stmt = 'UPDATE {{%project_user}}
-                        SET pru_role = ?
-                    WHERE
-                        pru_prj_id = ? AND
-                        pru_usr_id = ?';
-        try {
-            DB_Helper::getInstance()->query($stmt, [$role, $prj_id, $usr_id]);
-        } catch (DatabaseException $e) {
-            return false;
-        }
 
         return true;
     }