d7d95ce5e92c5d5a7a915811547507672dee61e0
Elan Ruusamäe <glen@pld-linux.org>
2017-09-09 02:12:24 +0300

    travis: experiment updating tag message on build success

--- bin/ci/changelog.sh
+++ bin/ci/changelog.sh
@@ -2,8 +2,34 @@
 # Update Release notes using Chandler
 set -e
 
+# create fake changelog file for "snapshot" tag
+create_snapshot_changelog() {
+	# obtain current tag message
+	# https://stackoverflow.com/a/26132640/2314626
+	object=$(git rev-parse snapshot)
+
+	message=$(git cat-file -p $object)
+	message=$(echo "$message" | tail -n +6)
+	date=$(LC_ALL=C TZ=UTC date)
+	version=$(git describe --tags --abbrev=8 HEAD)
+
+	# Extra Travis env variables:
+	# https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
+
+	cat <<-EOF
+## $TRAVIS_TAG
+
+$message
+
+Built by #$TRAVIS_BUILD_NUMBER
+Build finished at $date
+https://travis-ci.org/eventum/eventum/builds/$TRAVIS_BUILD_ID
+
+	EOF
+}
+
 if [ "$TRAVIS_TAG" = "snapshot" ]; then
-	exit 0
+	create_snapshot_changelog > CHANGELOG.md
 fi
 
 gem install --no-ri --no-rdoc chandler
