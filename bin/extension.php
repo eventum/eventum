688c4407e5ea42fefc67c9cc165ad2a5b0746f5d
Elan Ruusamäe <glen@pld-linux.org>
2017-09-08 19:10:45 +0300

    add script to manage extensions from cli
    for now it's just for enabling

---
+++ bin/extension.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+require_once __DIR__ . '/../init.php';
+
+$app = new Eventum\Command\ExtensionCommand();
+$app->run();
