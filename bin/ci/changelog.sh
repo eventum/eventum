89c8e47c16ab67c265a7da311467ffb9709eff4c
Elan Ruusamäe <glen@pld-linux.org>
2017-09-09 05:14:52 +0300

    snapshot: make tarball as link

--- bin/ci/changelog.sh
+++ bin/ci/changelog.sh
@@ -6,13 +6,17 @@ set -e
 upload_snapshot_changelog() {
 	date=$(LC_ALL=C TZ=UTC date)
 	version=$(git describe --tags --abbrev=8 HEAD)
+	tarball=eventum-${version#v}.tar.gz
+	url=https://github.com/$TRAVIS_REPO_SLUG/releases/download/$TRAVIS_TAG/$tarball
 
 	# Info about Travis ENV variables:
 	# https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
 	notes=$(cat release_note.txt)
 	title=$(echo "$notes" | head -n 1)
+	notes=$(echo "$notes" | tail -n +3)
+	notes=$(echo "$notes" | sed -e "s;$tarball;[$tarball]($url);")
 	notes=$(cat <<-EOF
-$(echo "$notes" | tail -n +3)
+$notes
 
 Build [#$TRAVIS_BUILD_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID) finished at $date
 Release tarball built from [#$TRAVIS_JOB_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/jobs/$TRAVIS_JOB_ID)
