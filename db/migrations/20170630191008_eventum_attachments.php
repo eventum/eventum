94b77d4e8cb715cfa7343752669aa0abdfb4d65a
Elan Ruusamäe <glen@pld-linux.org>
2017-10-04 20:31:11 +0300

    attachments: migrate issue_attachment_file first
    this table is bigger than issue_attachment, meaning if disk space is short
    whole migration will be aborted, not completed partially.
    
    refs #300

--- db/migrations/20170630191008_eventum_attachments.php
+++ db/migrations/20170630191008_eventum_attachments.php
@@ -17,11 +17,11 @@ class EventumAttachments extends AbstractMigration
 {
     public function change()
     {
-        $this->table('issue_attachment')
-            ->addColumn('iat_min_role', 'integer', ['after' => 'iat_usr_id', 'length' => '1', 'signed' => false, 'null' => false, 'default' => 1])
-            ->update();
         $this->table('issue_attachment_file')
             ->addColumn('iaf_flysystem_path', 'string', ['length' => 255, 'null' => true])
+            ->update();
+        $this->table('issue_attachment')
+            ->addColumn('iat_min_role', 'integer', ['after' => 'iat_usr_id', 'length' => '1', 'signed' => false, 'null' => false, 'default' => 1])
             ->update();
     }
 }
