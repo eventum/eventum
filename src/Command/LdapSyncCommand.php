b37e7e31b7ee900ab0cb4f7dfbc190d44c7882af
Elan Ruusamäe <glen@pld-linux.org>
2017-10-10 22:16:35 +0300

    ldap: move code to methods with logical separation

--- src/Command/LdapSyncCommand.php
+++ src/Command/LdapSyncCommand.php
@@ -27,35 +27,37 @@ class LdapSyncCommand
     /** @var OutputInterface */
     private $output;
 
+    /** @var  LDAP_Auth_Backend */
+    private $ldap;
+
     public function execute(OutputInterface $output)
     {
         $this->assertLdapAuthEnabled();
         $this->output = $output;
+
+        $this->ldap = new LDAP_Auth_Backend();
         $this->ldapSync();
     }
 
     private function ldapSync()
     {
-        $ldap = new LDAP_Auth_Backend();
+        if ($dn = $this->ldap->active_dn) {
+            $this->updateUsers($this->findUsers($dn));
+        }
 
-        $findUsers = function ($dn) use ($ldap) {
-            $search = $ldap->getUserListing($dn);
+        if ($dn = $this->ldap->inactive_dn) {
+            $this->disableUsers($this->findUsers($dn));
+        }
+    }
 
-            while ($entry = $search->shiftEntry()) {
-                // skip entries with no email
-                $emails = $entry->get_value('mail', 'all');
-                if (!$emails) {
-                    $uid = $entry->getValue('uid');
-                    echo "skip (no email): $uid, $dn\n";
-                    continue;
-                }
-
-                yield $entry;
-            }
-        };
-
-        // process active users from ldap
-        foreach ($findUsers($ldap->active_dn) as $entry) {
+    /**
+     * add new users and update existing users
+     *
+     * @param \Net_LDAP2_Entry[] $users
+     */
+    private function updateUsers($users)
+    {
+        foreach ($users as $entry) {
             $uid = $entry->getValue('uid');
             $dn = $entry->dn();
 
@@ -63,39 +65,69 @@ class LdapSyncCommand
             // TODO: check if ldap enabled and eventum disabled activates accounts in eventum
             echo "checking: $uid, $dn\n";
             try {
-                $ldap->updateLocalUserFromBackend($uid);
+                $this->ldap->updateLocalUserFromBackend($uid);
             } catch (AuthException $e) {
                 // this likely logs that user doesn't exist and will not be created
                 error_log("XX: $uid: " . $e->getMessage());
             }
         }
+    }
 
-        // process inactive users from ldap
-        foreach ($findUsers($ldap->inactive_dn) as $entry) {
+    /**
+     * Process inactive users from ldap
+     *
+     * @param \Net_LDAP2_Entry[] $users
+     */
+    private function disableUsers($users)
+    {
+        foreach ($users as $entry) {
             $uid = $entry->getValue('uid');
             $dn = $entry->dn();
-            $active = $ldap->accountActive($uid);
+            $active = $this->ldap->accountActive($uid);
 
             // handle unmapped users
             if ($active === null) {
                 // try to find user
-                $remote = $ldap->getRemoteUserInfo($uid);
-                $usr_id = $ldap->getLocalUserId($uid, $remote['emails']);
+                $remote = $this->ldap->getRemoteUserInfo($uid);
+                $usr_id = $this->ldap->getLocalUserId($uid, $remote['emails']);
                 // first update user to setup "external_id" mapping
                 if ($usr_id) {
-                    $ldap->updateLocalUserFromBackend($uid);
+                    $this->ldap->updateLocalUserFromBackend($uid);
                     // fetch user again
-                    $active = $ldap->accountActive($uid);
+                    $active = $this->ldap->accountActive($uid);
                 }
             }
 
             if ($active === true) {
                 echo "disabling: $uid, $dn\n";
-                $res = $ldap->disableAccount($uid, $dn);
+                $res = $this->ldap->disableAccount($uid);
                 if ($res !== true) {
                     throw new RuntimeException("Account disable for $uid ($dn) failed");
                 }
             }
+        }
+    }
+
+    /**
+     * Find users under specified DN.
+     *
+     * @param string $dn
+     * @return \Generator|\Net_LDAP2_Entry[]
+     */
+    private function findUsers($dn)
+    {
+        $search = $this->ldap->getUserListing($dn);
+
+        while ($entry = $search->shiftEntry()) {
+            // skip entries with no email
+            $emails = $entry->get_value('mail', 'all');
+            if (!$emails) {
+                $uid = $entry->getValue('uid');
+                echo "skip (no email): $uid, $dn\n";
+                continue;
+            }
+
+            yield $entry;
         }
     }
 
