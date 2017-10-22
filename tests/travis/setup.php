90d31b7b2a49a72187067c6764e965489a35b9f3
Elan Ruusamäe <glen@pld-linux.org>
2017-10-22 12:36:44 +0300

    add config for travis

---
+++ tests/travis/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+return [
+  'database' => [
+    'driver' => 'mysql',
+    'hostname' => 'localhost',
+    'database' => 'e_test',
+    'username' => 'root',
+    'password' => '',
+    'port' => 3306,
+  ],
+];
