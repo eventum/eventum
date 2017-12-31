8e774d29457ba48182f656100aee09baa0de151b
Elan Ruusamäe <glen@pld-linux.org>
2017-12-31 10:33:58 +0200

    User::getUserIDByEmail takes plain email only as input, not formatted email header
      [Eventum\Db\DatabaseException (HY000)]
      SQLSTATE[HY000]: General error: 1267 Illegal mix of collations (latin1_swed
      ish_ci,IMPLICIT) and (utf8_general_ci,COERCIBLE) for operation '='

--- lib/eventum/class.support.php
+++ lib/eventum/class.support.php
@@ -913,8 +913,7 @@ class Support
     {
         $closing = isset($email_options['closing']) ? $email_options['closing'] : false;
 
-        // get usr_id from FROM header
-        $usr_id = User::getUserIDByEmail($mail->from);
+        $usr_id = User::getUserIDByEmail($mail->getSender());
 
         if (!empty($usr_id) && empty($email_options['customer_id'])) {
             $email_options['customer_id'] = User::getCustomerID($usr_id);
