0643f20ce1bc1d460e65185dd3929e94c44a614e
Elan Ruusamäe <glen@pld-linux.org>
2018-04-13 19:50:26 +0300

    fix inversed markdown check in activatelinks #291

--- lib/eventum/class.misc.php
+++ lib/eventum/class.misc.php
@@ -420,7 +420,7 @@ class Misc
     public static function activateLinks($text, $class = 'link')
     {
         // process issue link separatly since it has to do something special
-        if (!Link_Filter::markdownEnabled()) {
+        if (Link_Filter::markdownEnabled()) {
             // conflicts with markdown
             return $text;
         }
