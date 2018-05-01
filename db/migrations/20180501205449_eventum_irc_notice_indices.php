3f2b191293a4c7a1a9de186803c86f2b4bdbae3a
Elan Ruusamäe <glen@pld-linux.org>
2018-05-02 00:03:48 +0300

    add index to irc_notice table with project_id
    the query made now uses project id in join

---
+++ db/migrations/20180501205449_eventum_irc_notice_indices.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+use Eventum\Db\AbstractMigration;
+
+class EventumIrcNoticeIndices extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('irc_notice')
+            ->removeIndex(['ino_status'])
+            ->addIndex(['ino_status', 'ino_prj_id'])
+            ->update();
+    }
+}
