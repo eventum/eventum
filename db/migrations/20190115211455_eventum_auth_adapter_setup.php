7c01ab937bf91293a544f0178b9c86212cff1263
Elan Ruusamäe <glen@pld-linux.org>
2019-01-15 23:15:19 +0200

    add migration to move auth adapters to setup

---
+++ db/migrations/20190115211455_eventum_auth_adapter_setup.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+use Eventum\Auth\Adapter;
+use Eventum\Db\AbstractMigration;
+
+class EventumAuthAdapterSetup extends AbstractMigration
+{
+    const DEFAULT_ADAPTER = Adapter\MysqlAdapter::class;
+    const CLASS_MAPPING = [
+        'mysql_auth_backend' => Adapter\MysqlAdapter::class,
+        'ldap_auth_backend' => Adapter\LdapAdapter::class,
+        'cas_auth_backend' => Adapter\CasAdapter::class,
+    ];
+
+    public function up()
+    {
+        $this->setupAuthAdapter();
+    }
+
+    private function setupAuthAdapter()
+    {
+        $setup = Setup::get();
+        $className = $this->getClassName();
+        $reflection = new ReflectionClass($className);
+        $setup['auth'][$reflection->getName()] = $reflection->getFileName();
+        Setup::save();
+    }
+
+    private function getClassName()
+    {
+        $class = self::DEFAULT_ADAPTER;
+        if (!defined('APP_AUTH_BACKEND')) {
+            return $class;
+        }
+
+        $class = strtolower(APP_AUTH_BACKEND);
+
+        return self::CLASS_MAPPING[$class] ?? self::DEFAULT_ADAPTER;
+    }
+}
