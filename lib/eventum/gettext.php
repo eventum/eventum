aedb9e4469e7257dd5cfae40323ca321eff7b9b0
Elan Ruusamäe <glen@pld-linux.org>
2019-02-27 17:49:42 +0200

    gettext: always initialize _gettext symbols

--- lib/eventum/gettext.php
+++ lib/eventum/gettext.php
@@ -11,11 +11,6 @@
  * that were distributed with this source code.
  */
 
-// if gettext disabled, return early
-if (!defined('APP_PATH')) {
-    return;
-}
-
 // if there is no gettext support built into PHP, or we are running in language compatibility mode include PHP-gettext
 if (!function_exists('gettext') || (defined('APP_GETTEXT_MODE') && APP_GETTEXT_MODE === 'php')) {
     /** @noinspection PhpIncludeInspection */
@@ -86,7 +81,8 @@ if (!function_exists('gettext') || (defined('APP_GETTEXT_MODE') && APP_GETTEXT_M
     }
 }
 
-// this won't change over the request. so set it once and permanently
-_bindtextdomain('eventum', APP_PATH . '/localization/');
-_bind_textdomain_codeset('eventum', APP_CHARSET);
-_textdomain('eventum');
+if (defined('APP_PATH')) {
+    _bindtextdomain('eventum', APP_PATH . '/localization/');
+    _bind_textdomain_codeset('eventum', APP_CHARSET);
+    _textdomain('eventum');
+}
