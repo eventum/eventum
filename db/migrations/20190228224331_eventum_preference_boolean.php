616b70d7eb8071842a8fa9d6191c06099a227b88
Elan Ruusamäe <glen@pld-linux.org>
2019-03-01 00:50:44 +0200

    user_preference: remove null from refresh rate fields

--- db/migrations/20190228224331_eventum_preference_boolean.php
+++ db/migrations/20190228224331_eventum_preference_boolean.php
@@ -18,6 +18,8 @@ class EventumPreferenceBoolean extends AbstractMigration
     public function up(): void
     {
         $this->table('user_preference')
+            ->changeColumn('upr_list_refresh_rate', 'integer', ['limit' => self::INT_MEDIUM, 'signed' => false, 'default' => 5])
+            ->changeColumn('upr_email_refresh_rate', 'integer', ['limit' => self::INT_MEDIUM, 'signed' => false, 'default' => 5])
             ->changeColumn('upr_auto_append_email_sig', 'boolean', ['default' => false])
             ->changeColumn('upr_auto_append_note_sig', 'boolean', ['default' => false])
             ->changeColumn('upr_auto_close_popup_window', 'boolean', ['default' => true])
