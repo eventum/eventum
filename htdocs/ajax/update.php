5ea4136c6a7c2a3d62f4d54b2f4bfd161670b3d2
Elan Ruusamäe <glen@pld-linux.org>
2019-03-03 14:14:02 +0200

    htdocs/ajax/update.php: move logic to UpdateController

--- htdocs/ajax/update.php
+++ htdocs/ajax/update.php
@@ -13,53 +13,5 @@
 
 require_once __DIR__ . '/../../init.php';
 
-// check login
-Auth::checkAuthentication();
-
-$field_name = filter_var(!empty($_POST['field_name']) ? $_POST['field_name'] : null, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$issue_id = !empty($_POST['issue_id']) ? (int) $_POST['issue_id'] : null;
-
-// check if correct issue id was sent
-if (!$issue_id || !Issue::exists($issue_id)) {
-    die('Invalid issue_id');
-}
-
-$usr_id = Auth::getUserID();
-
-// check if user role is above "Standard User"
-if (User::getRoleByUser($usr_id, Issue::getProjectID($issue_id)) < User::ROLE_USER) {
-    die('Forbidden');
-}
-
-// check if user can acess the issue
-if (!Issue::canAccess($issue_id, $usr_id)) {
-    die('Forbidden');
-}
-
-switch ($field_name) {
-    case 'expected_resolution_date':
-        $day = Misc::escapeInteger($_POST['day']);
-        $month = Misc::escapeInteger($_POST['month']);
-        $year = Misc::escapeInteger($_POST['year']);
-
-        if ($day == 0 && $month == 1 && $year == 0) {
-            // clear button
-            $date = null;
-        } else {
-            $date = sprintf('%04d-%02d-%02d', $year, $month, $day);
-        }
-
-        $res = Issue::setExpectedResolutionDate($issue_id, $date);
-        if ($res === -1) {
-            die('Update failed');
-        }
-
-        if ($date !== null) {
-            echo Date_Helper::getSimpleDate($date, false);
-        }
-    break;
-
-    default:
-        die("Object type '$field_name' not supported");
-    break;
-}
+$controller = new Eventum\Controller\Ajax\UpdateController();
+$controller->run();
