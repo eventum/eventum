eventum/eventum

commit d28df0295aa3b6617fe6f13081849510388c58cf

htdocs/ajax/upload.php: move logic to UploadController

src/Controller/Ajax/UploadController.php
@@ -1,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace Eventum\Controller\Ajax;
+
+use AuthCookie;
+use Eventum\Attachment\AttachmentManager;
+use Eventum\Controller\BaseController;
+use InvalidArgumentException;
+use Throwable;
+
+class UploadController extends BaseController
+{
+    /** @var string */
+    protected $tpl_name;
+    /** @var string */
+    private $file;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $request = $this->getRequest();
+
+        $this->file = $request->query->get('file');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function canAccess(): bool
+    {
+        // check if logged in. if not, just give error
+        if (!AuthCookie::hasAuthCookie()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function defaultAction(): void
+    {
+        try {
+            $res = $this->uploadAction();
+        } catch (Throwable $e) {
+            $code = $e->getCode();
+            $res = [
+                'error' => $code ? $code : -1,
+                'message' => $e->getMessage(),
+            ];
+            $this->logger->error($e);
+        }
+
+        header('Content-Type: application/json; charset=UTF-8');
+        echo json_encode($res);
+    }
+
+    /**
+     * FIXME: no identity logged who added the file.
+     */
+    protected function uploadAction(): array
+    {
+        if ($this->file) {
+            // TRANSLATORS: this is technical error and should not be displayed to end users
+            throw new InvalidArgumentException(ev_gettext('No file argument'));
+        }
+
+        if (!isset($_FILES[$this->file])) {
+            throw new InvalidArgumentException(ev_gettext('No files uploaded'));
+        }
+
+        $iaf_ids = AttachmentManager::addFiles($_FILES[$this->file]);
+
+        return [
+            'error' => 0,
+            'iaf_id' => $iaf_ids,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function prepareTemplate(): void
+    {
+        exit(0);
+    }
+}

htdocs/ajax/upload.php
@@ -11,42 +11,7 @@
  * that were distributed with this source code.
  */
 
-use Eventum\Attachment\AttachmentManager;
-use Eventum\Monolog\Logger;
-
 require_once __DIR__ . '/../../init.php';
 
-// handle ajax upload
-// FIXME: no identity logged who added the file.
-try {
-    // check if logged in. if not, just give error
-    if (!AuthCookie::hasAuthCookie()) {
-        throw new BadFunctionCallException(ev_gettext('Must be logged in'));
-    }
-
-    if (!isset($_GET['file'])) {
-        // TRANSLATORS: this is technical error and should not be displayed to end users
-        throw new InvalidArgumentException(ev_gettext('No file argument'));
-    }
-
-    $file = (string) $_GET['file'];
-    if (!isset($_FILES[$file])) {
-        throw new InvalidArgumentException(ev_gettext('No files uploaded'));
-    }
-
-    $iaf_ids = AttachmentManager::addFiles($_FILES[$file]);
-    $res = [
-        'error' => 0,
-        'iaf_id' => $iaf_ids,
-    ];
-} catch (Exception $e) {
-    $code = $e->getCode();
-    $res = [
-        'error' => $code ? $code : -1,
-        'message' => $e->getMessage(),
-    ];
-    Logger::app()->error($e);
-}
-
-header('Content-Type: application/json; charset=UTF-8');
-echo json_encode($res);
+$controller = new Eventum\Controller\Ajax\UploadController();
+$controller->run();

commit 076f169458937ffc48083018d5ba7f3f3237db6b

fix ajax upload. #466
broken in d28df029

src/Controller/Ajax/UploadController.php
@@ -66,7 +66,7 @@ class UploadController extends AjaxBaseController
      */
     protected function uploadAction(): array
     {
-        if ($this->file) {
+        if (!$this->file) {
             // TRANSLATORS: this is technical error and should not be displayed to end users
             throw new InvalidArgumentException(ev_gettext('No file argument'));
         }