8005235d91693e70ee9b27f358e7973969df258e
Elan Ruusamäe <glen@pld-linux.org>
2019-04-06 01:55:12 +0300

    add status to issue seeder

--- db/seeds/IssueSeeder.php
+++ db/seeds/IssueSeeder.php
@@ -16,21 +16,41 @@ use Phinx\Seed\AbstractSeed;
 class IssueSeeder extends AbstractSeed
 {
     public const ISSUE_1 = 1;
+    public const ISSUE_2 = 2;
+    public const STATUS_DISCOVERY = 1;
 
     public function run(): void
     {
         $issues = [
             self::ISSUE_1 => [
                 'Issue Summary',
+                self::STATUS_DISCOVERY,
+            ],
+            self::ISSUE_2 => [
+                'Issue Summary',
+                self::STATUS_DISCOVERY,
+            ],
+            13 => [
+                'Issue Summary',
+                self::STATUS_DISCOVERY,
+            ],
+            14 => [
+                'Issue Summary',
+                self::STATUS_DISCOVERY,
+            ],
+            15 => [
+                'Issue Summary',
+                self::STATUS_DISCOVERY,
             ],
         ];
 
         $table = $this->table('issue');
-        foreach ($issues as $issue_id => [$summary]) {
+        foreach ($issues as $issue_id => [$summary, $iss_sta_id]) {
             $row = [
-                'iss_id' => 1,
+                'iss_id' => $issue_id,
                 'iss_prj_id' => ProjectSeeder::DEFAULT_PROJECT_ID,
-                'iss_summary' => $summary,
+                'iss_sta_id' => $iss_sta_id,
+                'iss_summary' => "$summary #{$issue_id}",
             ];
             $table->insert($row);
         }
