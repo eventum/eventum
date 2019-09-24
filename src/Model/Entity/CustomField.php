83c847a260e1c2b47800948b3abf6f4e3cdb50c7
Elan Ruusamäe <glen@pld-linux.org>
2019-09-24 22:40:17 +0300

    Increase size of custom_field title/description to 255
    Set also maxlength html attribute for better UX

--- src/Model/Entity/CustomField.php
+++ src/Model/Entity/CustomField.php
@@ -83,13 +83,13 @@ class CustomField
 
     /**
      * @var string
-     * @ORM\Column(name="fld_title", type="string", length=32, nullable=false)
+     * @ORM\Column(name="fld_title", type="string", length=255, nullable=false)
      */
     private $title;
 
     /**
      * @var string
-     * @ORM\Column(name="fld_description", type="string", length=64, nullable=true)
+     * @ORM\Column(name="fld_description", type="string", length=255, nullable=true)
      */
     private $description;
 
