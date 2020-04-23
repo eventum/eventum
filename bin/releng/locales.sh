ccbf97e60e0b3cd9ef1a73cbb8a7436c98d3c32d
Elan Ruusamäe <glen@pld-linux.org>
2020-04-23 07:41:17 +0300

    Drop unsupported languages (fo,ht,ur) from xenial

--- bin/releng/locales.sh
+++ bin/releng/locales.sh
@@ -4,7 +4,7 @@ set -xe
 # install all locales used by eventum
 sudo apt-get update
 sudo apt-get --reinstall install -qq \
-	language-pack-{br,ca,cs,da,de,eo,es,et,fi,fo,fr,he,ht,hu,id,it,ja,ko,lt,lv,nl,oc,pl,pt,ru,si,sv,ta,th,tr,uk,ur,vi,zh-hans}
+	language-pack-{br,ca,cs,da,de,eo,es,et,fi,fr,he,hu,id,it,ja,ko,lt,lv,nl,oc,pl,pt,ru,si,sv,ta,th,tr,uk,vi,zh-hans}
 
 # display some info from system
 dpkg --list | grep language-pack
