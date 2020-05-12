b4290376e8d26c88ea0a11546aaa7bbf3af1563c
Elan Ruusamäe <glen@pld-linux.org>
2020-05-12 07:01:38 +0300

    Extract Validation to a class

--- res/assets/scripts/bootstrap.js
+++ res/assets/scripts/bootstrap.js
@@ -12,7 +12,9 @@
 import { ExpandableCell } from "./ExpandableCell.js";
 import { CustomField } from "./CustomField.js";
 import { GrowingFileField } from "./GrowingFileField.js";
+import { Validation } from "./Validation.js";
 
 window.ExpandableCell = new ExpandableCell();
 window.CustomField = new CustomField();
 window.GrowingFileField = new GrowingFileField();
+window.Validation = new Validation();
