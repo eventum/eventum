43a4def9c31a1683776cff6a92b9abd4e7b60b7e
Elan Ruusamäe <glen@pld-linux.org>
2020-05-19 08:24:55 +0300

    Add ConsoleTrait for writeln

--- src/Console/Command/Command.php
+++ src/Console/Command/Command.php
@@ -13,26 +13,15 @@
 
 namespace Eventum\Console\Command;
 
+use Eventum\Console\ConsoleTrait;
 use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class Command
 {
+    use ConsoleTrait;
+
     // compact constants for writeln
     protected const DEBUG = OutputInterface::VERBOSITY_DEBUG;
     protected const VERBOSE = OutputInterface::VERBOSITY_VERBOSE;
     protected const VERY_VERBOSE = OutputInterface::VERBOSITY_VERY_VERBOSE;
-
-    /** @var OutputInterface */
-    protected $output;
-
-    /**
-     * Writes a message to the output and adds a newline at the end.
-     *
-     * @param string|array $messages The message as an array of lines of a single string
-     * @param int $options A bitmask of options (one of the OUTPUT or VERBOSITY constants), 0 is considered the same as self::OUTPUT_NORMAL | self::VERBOSITY_NORMAL
-     */
-    protected function writeln($messages, $options = 0): void
-    {
-        $this->output->writeln($messages, $options);
-    }
 }
---
+++ src/Console/ConsoleTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace Eventum\Console;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+trait ConsoleTrait
+{
+    /** @var OutputInterface */
+    protected $output;
+
+    /**
+     * Writes a message to the output and adds a newline at the end.
+     *
+     * @param string|array $messages The message as an array of lines of a single string
+     * @param int $options A bitmask of options (one of the OUTPUT or VERBOSITY constants), 0 is considered the same as self::OUTPUT_NORMAL | self::VERBOSITY_NORMAL
+     */
+    protected function writeln($messages, $options = 0): void
+    {
+        $this->output->writeln($messages, $options);
+    }
+}
