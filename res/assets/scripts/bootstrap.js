c45ad26180fedb57f19a6e9c6ddb9278f77bba9e
Elan Ruusamäe <glen@pld-linux.org>
2020-06-06 11:32:59 +0300

    Add page js: new_issue

--- res/assets/scripts/bootstrap.js
+++ res/assets/scripts/bootstrap.js
@@ -21,6 +21,7 @@ import IssueView from "./pages/IssueView.js";
 import IssueUpdate from "./pages/IssueUpdate.js";
 import CloseIssue from "./pages/CloseIssue.js";
 import AdvSearch from "./pages/AdvSearch.js";
+import NewIssue from "./pages/NewIssue.js";
 
 window.Eventum = new Eventum();
 window.ExpandableCell = new ExpandableCell();
@@ -36,3 +37,4 @@ window.issue_view = new IssueView();
 window.issue_update = new IssueUpdate();
 window.close_issue = new CloseIssue();
 window.adv_search = new AdvSearch();
+window.new_issue = new NewIssue();
