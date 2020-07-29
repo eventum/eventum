4e3482f7ca26633d047055be403dcd384c70d9a8
Elan Ruusamäe <glen@pld-linux.org>
2020-07-29 23:42:03 +0300

    Register clockworks bundle

--- res/bundles.php
+++ res/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Clockwork\Support\Symfony\ClockworkBundle::class => ['dev' => true],
 ];
