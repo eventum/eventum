eventum/eventum

commit 682a6009197d6a03f3910d961183de737da04789

Wrap closure around handlers for "this." context to be incorrect

res/assets/scripts/app.js
@@ -46,7 +46,9 @@ $(document).ready(function () {
 
     window.onbeforeunload = Eventum.handleClose;
 
-    $('form.validate').submit(Validation.callback);
+    $('form.validate').submit(function(e) {
+        Validation.callback(e);
+    });
 
     ExpandableCell.ready();
 
@@ -54,7 +56,9 @@ $(document).ready(function () {
         $(this).find("form").submit();
     });
 
-    $('#change_clock_status').click(Eventum.changeClockStatus);
+    $('#change_clock_status').click(function() {
+        return Eventum.changeClockStatus();
+    });
 
     $(".date_picker").datepicker({
         dateFormat: "yy-mm-dd",

res/assets/scripts/pages/CloseIssue.js
@@ -14,8 +14,12 @@ export default class {
         const page = this;
 
         page.toggleNotificationList();
-        $('input[name=notification_list]').change(page.toggleNotificationList);
-        $('input[name=add_email_signature]').change(page.toggleEmailSignature);
+        $('input[name=notification_list]').change(function() {
+            page.toggleNotificationList();
+        });
+        $('input[name=add_email_signature]').change(function() {
+            page.toggleEmailSignature();
+        });
 
         $('form[name=close_form]').submit(function () {
             return Validation.checkFormSubmission($('form[name=close_form]'), page.validateForm);

res/assets/scripts/pages/CustomFieldOptions.js
@@ -11,10 +11,14 @@
 
 export default class {
     ready() {
+        let page = this;
+
         $('#sortable').sortable();
         this.bind_actions();
 
-        $('#add_option').click(this.add_option);
+        $('#add_option').click(function() {
+            page.add_option();
+        });
         this.add_option();
     }
 

res/assets/scripts/pages/IssueUpdate.js
@@ -35,11 +35,19 @@ export default class {
 
         Eventum.setupShowSelections($('#assignments'));
 
-        $('.open_history').click(issue_view.openHistory);
-        $('.open_nl').click(issue_view.openNotificationList);
-        $('.open_ar').click(issue_view.openAuthorizedReplier);
+        $('.open_history').click(function() {
+            return issue_view.openHistory();
+        });
+        $('.open_nl').click(function() {
+            return issue_view.openNotificationList();
+        });
+        $('.open_ar').click(function() {
+            return issue_view.openAuthorizedReplier();
+        });
 
-        $('#severity').bind('change', page.display_severity_description).change();
+        $('#severity').bind('change', function() {
+            page.display_severity_description();
+        }).change();
     }
 
     validateForm() {

res/assets/scripts/pages/IssueView.js
@@ -42,31 +42,71 @@ export default class {
         });
 
         $('#issue_description_link')
-            .click(page.toggle_issue_description)
-            .ready(page.display_description_collapse_message);
+            .click(function() {
+                page.toggle_issue_description();
+            })
+            .ready(function() {
+                page.display_description_collapse_message();
+            });
 
         /* Main Issue actions */
-        $('.open_history').click(page.openHistory);
-        $('.open_nl').click(page.openNotificationList);
-        $('.open_ar').click(page.openAuthorizedReplier);
-        $('.self_assign').click(page.selfAssign);
-        $('.unassign').click(page.unassign);
-        $('.self_notification').click(page.selfNotification);
-        $('.self_authorized_replier').click(page.signupAsAuthorizedReplier);
-        $('.change_status').click(page.changeIssueStatus);
-        $('.change_access').click(page.changeAccess);
-
-        $('.remove_quarantine').click(page.removeQuarantine);
-        $('.clear_duplicate').click(page.clearDuplicateStatus);
-        $('.reply_issue').click(page.replyIssue);
-        $('.reply_issue_note').click(page.replyIssueNote);
-        $('.reply_email').click(page.reply);
-        $('.reply_email_note').click(page.replyAsNote);
-        $('.edit_incident_redemption').click(page.editIncidentRedemption);
-        $('a.edit_time_entry').click(page.editTimeEntry);
-        $('a.delete_time_entry').click(page.deleteTimeEntry);
-        $('.add_time_entry').click(page.addTimeEntry);
-
+        $('.open_history').click(function() {
+            return page.openHistory();
+        });
+        $('.open_nl').click(function() {
+            return page.openNotificationList();
+        });
+        $('.open_ar').click(function() {
+            return page.openAuthorizedReplier();
+        });
+        $('.self_assign').click(function() {
+            page.selfAssign();
+        });
+        $('.unassign').click(function() {
+            page.unassign();
+        });
+        $('.self_notification').click(function() {
+            page.selfNotification();
+        });
+        $('.self_authorized_replier').click(function() {
+            page.signupAsAuthorizedReplier();
+        });
+        $('.change_status').click(function(e) {
+            page.changeIssueStatus(e);
+        });
+        $('.change_access').click(function() {
+            return page.changeAccess();
+        });
+        $('.remove_quarantine').click(function() {
+            page.removeQuarantine();
+        });
+        $('.clear_duplicate').click(function() {
+            page.clearDuplicateStatus();
+        });
+        $('.reply_issue').click(function() {
+            page.replyIssue();
+        });
+        $('.reply_issue_note').click(function() {
+            page.replyIssueNote();
+        });
+        $('.reply_email').click(function() {
+            page.reply();
+        });
+        $('.reply_email_note').click(function() {
+            page.replyAsNote();
+        });
+        $('.edit_incident_redemption').click(function() {
+            page.editIncidentRedemption();
+        });
+        $('a.edit_time_entry').click(function(e) {
+            return page.editTimeEntry(e);
+        });
+        $('a.delete_time_entry').click(function(e) {
+            return page.deleteTimeEntry(e);
+        });
+        $('.add_time_entry').click(function() {
+            page.addTimeEntry();
+        });
         $('.mark_duplicate').click(function () {
             window.location.href = 'close.php?cat=duplicate&id=' + page.get_issue_id();
         });
@@ -81,9 +121,15 @@ export default class {
         $('#toggle_attachments').click(function () {
             page.toggle_issue_section('attachments');
         });
-        $('#upload_file').click(page.upload_file);
-        $('#attachments .delete_attachment').click(page.delete_attachment);
-        $('#attachments .delete_file').click(page.delete_file);
+        $('#upload_file').click(function(e) {
+            page.upload_file(e);
+        });
+        $('#attachments .delete_attachment').click(function() {
+            return page.delete_attachment();
+        });
+        $('#attachments .delete_file').click(function(e) {
+            return page.delete_file(e);
+        });
     }
 
     get_issue_id() {

res/assets/scripts/pages/ListIssues.js
@@ -36,41 +36,61 @@ export default class {
         $('#toggle_bulk_update').click(function () {
             Eventum.toggle_section_visibility('bulk_update');
         });
-
-        $('#reset_bulk_update').click(page.reset_bulk_update);
-        $('#bulk_update_button').click(page.bulk_update);
-        $('#clear_filters').click(page.clearFilters);
-        $('#hide_closed').click(page.hideClosed);
-        $('#show_all_projects').click(page.showAllProjects);
-        $('#page_size').change(page.resizePager);
-        $('#resize_page').click(page.resizePager);
-        $('#custom_filter').change(page.runCustomFilter);
-
+        $('#reset_bulk_update').click(function() {
+            page.reset_bulk_update();
+        });
+        $('#bulk_update_button').click(function() {
+            return page.bulk_update();
+        });
+        $('#clear_filters').click(function() {
+            page.clearFilters();
+        });
+        $('#hide_closed').click(function() {
+            page.hideClosed();
+        });
+        $('#show_all_projects').click(function() {
+            page.showAllProjects();
+        });
+        $('#page_size').change(function() {
+            page.resizePager();
+        });
+        $('#resize_page').click(function() {
+            page.resizePager();
+        });
+        $('#custom_filter').change(function() {
+            return page.runCustomFilter();
+        });
         $('.select_all').click(function () {
             Eventum.toggleCheckAll('item[]');
         });
 
         Eventum.getField('first').click(function () {
-            page.setPage(0);
+            return page.setPage(0);
         });
         Eventum.getField('previous').click(function () {
-            page.setPage(page.page - 1);
+            return page.setPage(page.page - 1);
         });
         Eventum.getField('next').click(function () {
-            page.setPage(page.current_page + 1);
+            return page.setPage(page.current_page + 1);
         });
         Eventum.getField('last').click(function () {
-            page.setPage(page.last_page);
+            return page.setPage(page.last_page);
         });
-        Eventum.getField('go').click(page.goPage);
+        Eventum.getField('go').click(function() {
+            return page.goPage();
+        });
         Eventum.getField('page').keydown(function (e) {
             if (e.which === 13) {
                 page.goPage();
             }
         });
 
-        $('#export_csv').click(page.downloadCSV);
-        $('.custom_field').click(page.updateCustomFields);
+        $('#export_csv').click(function() {
+            return page.downloadCSV();
+        });
+        $('.custom_field').click(function(e) {
+            return page.updateCustomFields(e);
+        });
 
         page.disableFields();
 

res/assets/scripts/pages/NewIssue.js
@@ -20,7 +20,9 @@ export default class {
             return Validation.checkFormSubmission($form, page.validateForm)
         });
 
-        $('#severity').bind('change', page.display_severity_description).change();
+        $('#severity').bind('change', function () {
+            page.display_severity_description();
+        }).change();
         product.ready();
     };
 

res/assets/scripts/pages/Preferences.js
@@ -11,15 +11,23 @@
 
 export default class {
     ready() {
+        const page = this;
+
         $('#show_revoked').click(function () {
             $('body#preferences .api_token .revoked').show();
         });
-
-        $('form#api_token_form').submit(this.confirmRegenerateToken);
-
-        $('form.update_name_form').submit(this.validateName);
-        $('form.update_email_form').submit(this.validateEmail);
-        $('form.update_password_form').submit(this.validatePassword);
+        $('form#api_token_form').submit(function() {
+            return page.confirmRegenerateToken();
+        });
+        $('form.update_name_form').submit(function() {
+            return page.validateName();
+        });
+        $('form.update_email_form').submit(function() {
+            return page.validateEmail();
+        });
+        $('form.update_password_form').submit(function() {
+            return page.validatePassword();
+        });
         $('input.api_token').focus(function () {
             const $this = $(this);
             $this.select();

commit 70da085bceb0f56858bd99f7c4eaf1177004c33f

Restore return value of form.validate submit action. #884
Regression introduced in 682a60091

res/assets/scripts/app.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
     window.onbeforeunload = Eventum.handleClose;
 
     $('form.validate').submit(function(e) {
-        Validation.callback(e);
+        return Validation.callback(e);
     });
 
     ExpandableCell.ready();