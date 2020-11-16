b2c8fbefaee709570776e275890246b8c520c46a
Elan Ruusamäe <glen@pld-linux.org>
2020-11-16 09:15:35 +0200

    Deprecate EventManager::dispatch method

--- src/EventDispatcher/EventManager.php
+++ src/EventDispatcher/EventManager.php
@@ -49,6 +49,6 @@ class EventManager
     {
         trigger_deprecation('eventum/eventum', '3.10.2', 'Method "%s::%s" is deprecated, use ServiceContainer::dispatch', __CLASS__, __METHOD__);
 
-        return ServiceContainer::getEventDispatcher()->dispatch($event ?? new Event(), $eventName);
+        return ServiceContainer::dispatch($eventName, $event);
     }
 }
