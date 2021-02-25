4dcdaedb89d2a724808e71f6be0bf3a52b5060b7
Elan Ruusamäe <glen@pld-linux.org>
2021-02-25 11:57:28 +0200

    Sync textarea state with original textarea on changes
    Inspired by:
    - https://gist.github.com/mbrehin/05c0d41a7e50eef7f95711e237502c85

--- res/assets/scripts/markdown/ProseMirrorView.js
+++ res/assets/scripts/markdown/ProseMirrorView.js
@@ -5,11 +5,20 @@ import { exampleSetup } from "prosemirror-example-setup";
 
 export class ProseMirrorView {
     constructor(source, target, content) {
+        this.area = source;
         this.view = new EditorView(target, {
             state: EditorState.create({
                 doc: defaultMarkdownParser.parse(content),
                 plugins: exampleSetup({ schema }),
-            })
+            }),
+            dispatchTransaction: (tr) => {
+                const { state } = this.view.state.applyTransaction(tr);
+                this.view.updateState(state);
+                // Update textarea only if content has changed
+                if (tr.docChanged) {
+                    this.area.value = defaultMarkdownSerializer.serialize(tr.doc);
+                }
+            },
         });
     }
 
