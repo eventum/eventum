0ec3b6bd00f956486a9dd3b40acf59fd789de2b2
Elan Ruusamäe <glen@pld-linux.org>
2021-03-06 19:52:03 +0200

    Move auth key check to auth class rather setup only

--- htdocs/setup/index.php
+++ htdocs/setup/index.php
@@ -34,8 +34,4 @@ set_time_limit(0);
 // set default timezone to utc to avoid default timezone not set warnings
 date_default_timezone_set(@date_default_timezone_get());
 
-if (!file_exists($privateKeyFile = Setup::getPrivateKeyPath()) || !filesize($privateKeyFile)) {
-    Auth::generatePrivateKey();
-}
-
 Kernel::handleRequest();
