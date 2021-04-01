70077fa2f092768810eb2282e0fb8ca5359e5ba9
Elan Ruusamäe <glen@pld-linux.org>
2021-04-02 00:34:17 +0300

    Ensure input/output are present in BaseCommand

--- src/Console/Command/BaseCommand.php
+++ src/Console/Command/BaseCommand.php
@@ -14,14 +14,26 @@
 namespace Eventum\Console\Command;
 
 use Eventum\Console\ConsoleTrait;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class BaseCommand
 {
     use ConsoleTrait;
 
-    // compact constants for writeln
     protected const DEBUG = OutputInterface::VERBOSITY_DEBUG;
     protected const VERBOSE = OutputInterface::VERBOSITY_VERBOSE;
     protected const VERY_VERBOSE = OutputInterface::VERBOSITY_VERY_VERBOSE;
+
+    /** @var InputInterface */
+    protected $input;
+
+    /** @var OutputInterface */
+    protected $output;
+
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->output = $output;
+        $this->input = $input;
+    }
 }
