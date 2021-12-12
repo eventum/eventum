f0ea4e061a8806505efc59478e729b72275e40ae
Elan Ruusamäe <glen@delfi.ee>
2021-12-12 23:49:36 +0200

    Add script to setup "replace" section from "extra.replace"

---
+++ bin/releng/composer-replace.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+# setup "replace" section in composer.json
+replace=$(jq .extra.replace composer.json)
+composer=$(mktemp composerXXXXXX)
+jq --arg replace "$replace" '.replace = ($replace|fromjson)' composer.json > $composer
+mv -f $composer composer.json
+
+# now remove the packages
+packages=$(jq -r '.extra.replace|keys[]' composer.json)
+set -x
+composer remove $packages --update-no-dev --ansi
