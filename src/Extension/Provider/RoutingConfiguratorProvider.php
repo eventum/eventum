3de2da1a446dbe7c7bf00e346461995912a64f3f
Elan Ruusamäe <glen@pld-linux.org>
2021-12-16 00:49:11 +0200

    Add new RoutingConfiguratorProvider provider (replaces RouteProvider)

---
+++ src/Extension/Provider/RoutingConfiguratorProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Eventum (Issue Tracking System) package.
+ *
+ * @copyright (c) Eventum Team
+ * @license GNU General Public License, version 2 or later (GPL-2+)
+ *
+ * For the full copyright and license information,
+ * please see the COPYING and AUTHORS files
+ * that were distributed with this source code.
+ */
+
+namespace Eventum\Extension\Provider;
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+interface RoutingConfiguratorProvider extends ExtensionProvider
+{
+    /**
+     * @since 3.11.0
+     */
+    public function configureRoutes(RoutingConfigurator $routes): void;
+}
