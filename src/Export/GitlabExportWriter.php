d63e5d151734edae98fe14854229139fdaccad8e
Elan Ruusamäe <glen@pld-linux.org>
2022-03-09 22:48:48 +0200

    Simplify writeProject with defaults

--- src/Export/GitlabExportWriter.php
+++ src/Export/GitlabExportWriter.php
@@ -46,45 +46,9 @@ class GitlabExportWriter
 
     private function writeProject(): void
     {
-        $this->writeJsonFile('tree/project.json', [
-            'description' => '',
-            'visibility_level' => 10,
-            'archived' => false,
-            'shared_runners_enabled' => true,
-            'build_coverage_regex' => null,
-            'build_allow_git_fetch' => true,
-            'build_timeout' => 3600,
-            'pending_delete' => false,
-            'public_builds' => true,
-            'last_repository_check_failed' => null,
-            'only_allow_merge_if_pipeline_succeeds' => false,
-            'has_external_issue_tracker' => false,
-            'request_access_enabled' => false,
-            'has_external_wiki' => false,
-            'only_allow_merge_if_all_discussions_are_resolved' => false,
-            'printing_merge_request_link_enabled' => true,
-            'auto_cancel_pending_pipelines' => 'enabled',
-            'ci_config_path' => null,
-            'delete_error' => null,
-            'merge_requests_rebase_enabled' => false,
-            'merge_requests_ff_only_enabled' => false,
-            'resolve_outdated_diff_discussions' => false,
-            'jobs_cache_index' => 1,
-            'pages_https_only' => false,
-            'external_authorization_classification_label' => null,
-            'disable_overriding_approvers_per_merge_request' => false,
-            'issues_template' => null,
-            'merge_requests_author_approval' => true,
-            'merge_requests_disable_committers_approval' => false,
-            'merge_requests_template' => '',
-            'require_password_to_approve' => false,
-            'reset_approvals_on_push' => true,
-            'service_desk_enabled' => true,
-            'approvals_before_merge' => 0,
-            'remove_source_branch_after_merge' => true,
-            'suggestion_commit_message' => '',
-            'autoclose_referenced_issues' => false,
-        ]);
+        $data = $this->readJsonFile('project.json');
+        $data['description'] = 'Issues exported from Eventum';
+        $this->writeJsonFile('tree/project.json', $data);
     }
 
     private function writeFile(string $fileName, string $content): void
